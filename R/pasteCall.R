3c375fee57da6880314bbeacb0737a0a11d5aade
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-11-19 17:39:09 +0000

    simpler pasteCall
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@576 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/pasteCall.R
+++ R/pasteCall.R
@@ -1,15 +1,6 @@
 `pasteCall` <- function (call, prefix = "Call:") 
 {
-    objcall <- deparse(call)
-    if (length(objcall) > 1) {
-        call.str <- ""
-        for (i in seq(along = objcall)) {
-            call.str <- paste(call.str, objcall[i], sep = " ")
-        }
-    }
-    else {
-        call.str <- objcall
-    }
+    call.str <- paste(deparse(call), collapse = " ")
     call.str <- paste(prefix, call.str, "\n", sep = " ")
     return(call.str)
 }
