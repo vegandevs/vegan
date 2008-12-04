74d846b8d1ca2ede702b7e45bb2bd60e7d83ec6e
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-12-04 10:08:52 +0000

    plot.rad gains argument log='y' so that log scales can be set on axes
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@608 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/plot.rad.R
+++ R/plot.rad.R
@@ -1,8 +1,8 @@
 "plot.rad" <-
-    function(x, xlab="Rank", ylab="Abundance",  ...)
+    function(x, xlab="Rank", ylab="Abundance", log = "y", ...)
 {
     rnk <- seq(along=x)
-    plot(rnk, x, log="y", xlab=xlab, ylab=ylab, ...)
+    plot(rnk, x, log=log, xlab=xlab, ylab=ylab, ...)
     out <- list(species = cbind(rnk, x))
     class(out) <- "ordiplot"
     invisible(out)
