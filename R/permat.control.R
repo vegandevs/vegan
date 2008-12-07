95cc1d9f3c6ec989d2898722615996fe1b90dc59
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-12-07 19:08:53 +0000

    permat update continued
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@621 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/permat.control.R
+++ R/permat.control.R
@@ -1,6 +1,6 @@
 `permat.control` <-
-function(ptype="full", mtype="count", method="quasiswap", fixedmar="both", shuffle="ind", reg=NULL, hab=NULL, burnin=10000, thin=1000)
+function(ptype="full", mtype="count", method="quasiswap", fixedmar="both", shuffle="ind", strata=NULL, burnin=10000, thin=1000)
 {
-list(ptype=ptype, mtype=mtype, method=method, fixedmar=fixedmar, shuffle=shuffle, reg=reg, hab=hab, burnin=burnin, thin=thin)
+list(ptype=ptype, mtype=mtype, method=method, fixedmar=fixedmar, shuffle=shuffle, strata=strata, burnin=burnin, thin=thin)
 }
 
