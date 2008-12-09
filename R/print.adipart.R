10d8be185485505efa819286fe4e19094ab62881
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-12-09 06:53:18 +0000

    excess space removed
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@629 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/print.adipart.R
+++ R/print.adipart.R
@@ -4,7 +4,7 @@ function(x, ...)
     cat("adipart with", ncol(x$oecosimu$simulated), "simulations\n")
     att <- attributes(x)
     att$names <- att$class <- att$n.levels <- att$terms <- att$model <- NULL
-    cat(" with", paste(names(att), att, collapse=", "))
+    cat("with", paste(names(att), att, collapse=", "))
 
     cat("\n\n")
     cl <- class(x)
