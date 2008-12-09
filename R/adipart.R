f063823336f1612a414a8d9f848f72d98a386e48
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-12-09 02:42:55 +0000

    adipart with formula, and 10 times faster
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@627 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/adipart.R
+++ R/adipart.R
@@ -1,35 +1,49 @@
 adipart <-
-function(matr, strata, index=c("richness", "shannon", "simpson"),
+function(formula, data, index=c("richness", "shannon", "simpson"),
     weights=c("unif", "prop"), relative = FALSE, nsimul=99, control, ...)
 {
+    ## evaluate formula
+    lhs <- formula[[2]]
+    if (missing(data))
+        data <- parent.frame()
+    lhs <- as.matrix(eval(lhs, data))
+    formula[[2]] <- NULL
+    rhs <- model.frame(formula, data, drop.unused.levels = TRUE)
+    tlab <- attr(attr(rhs, "terms"), "term.labels")
+    nlevs <- length(tlab)
 
-    ## internal function, maybe later gets out
-    nested.factor <-
-    function(x) {
-        x <- data.frame(x)
-        nc <- NCOL(x)
-        if (nc < 2)
-            stop("number of columns must at least 2")
-        nr <- NROW(x)
-        l1 <- sapply(x, function(z) length(unique(z)))
-        if (!any(sapply(2:nc, function(z) l1[z] <= l1[z-1])))
-            stop("number of levels are inapropriate")
-        rval <- list()
-        rval[[1]] <- as.factor(x[,nc])
-        rval[[1]] <- rval[[1]][drop = TRUE]
-        ncol <- nc - 1
-        for (i in 2:nc) {
-            rval[[i]] <- interaction(x[,ncol], rval[[(i-1)]], drop=TRUE)
-            ncol <- ncol - 1
-        }
-        rval <- as.data.frame(rval[rev(1:length(rval))])
-        colnames(rval) <- paste("x", 1:nc, sep="")
-        l2 <- sapply(rval, function(z) length(unique(z)))
-        if (any(l1 != l2))
-            warning("levels are not perfectly nested")
-        rval
+    ## part check proper design of the model frame
+    noint <- attr(attr(attr(rhs, "terms"), "factors"), "dimnames")[[1]]
+    int <- attr(attr(attr(rhs, "terms"), "factors"), "dimnames")[[2]]
+    if (!identical(noint, int))
+        stop("interactions are not allowed in formula")
+    if (!all(attr(attr(rhs, "terms"), "dataClasses") == "factor"))
+        stop("all right hand side variables in formula must be factors")
+    l1 <- sapply(rhs, function(z) length(unique(z)))
+    if (!any(sapply(2:nlevs, function(z) l1[z] <= l1[z-1])))
+        stop("number of levels are inapropriate, check sequence")
+    rval <- list()
+    rval[[1]] <- as.factor(rhs[,nlevs])
+    rval[[1]] <- rval[[1]][drop = TRUE]
+    nCol <- nlevs - 1
+    for (i in 2:nlevs) {
+        rval[[i]] <- interaction(rhs[,nCol], rval[[(i-1)]], drop=TRUE)
+        nCol <- nCol - 1
+    }
+    rval <- as.data.frame(rval[rev(1:length(rval))])
+    l2 <- sapply(rval, function(z) length(unique(z)))
+    if (any(l1 != l2))
+        warning("levels are not perfectly nested")
+
+    ## aggregate response matrix
+    fullgamma <-if (nlevels(rhs[,nlevs]) == 1)
+        TRUE else FALSE
+    ftmp <- vector("list", nlevs)
+    for (i in 1:nlevs) {
+        ftmp[[i]] <- as.formula(paste("~", tlab[i], "- 1"))
     }
 
+    ## evaluate other arguments
     index <- match.arg(index)
     weights <- match.arg(weights)
     if (missing(control))
@@ -41,30 +55,36 @@ function(matr, strata, index=c("richness", "shannon", "simpson"),
             divfun <- function(x) diversity(x, index = "shannon", MARGIN = 1, ...)},
         "simpson" = {
             divfun <- function(x) diversity(x, index = "simpson", MARGIN = 1)})
-    strata <- nested.factor(strata)
-    nl <- NCOL(strata)
-#    seed <- trunc(runif(1, 1000, 9999))
+    sumMatr <- sum(lhs)
+
     ## this is the function passed to oecosimu
     wdivfun <- function(x) {
-        tmp <- lapply(1:nl, function(i) aggregate(x, list(strata[,i]), sum)[,-1])
+        if (fullgamma) {
+            tmp <- lapply(1:(nlevs-1), function(i) t(model.matrix(ftmp[[i]], rhs)) %*% x)
+            tmp[[nlevs]] <- matrix(colSums(lhs), nrow = 1, ncol = ncol(lhs))
+        } else {
+            tmp <- lapply(1:nlevs, function(i) t(model.matrix(ftmp[[i]], rhs)) %*% x)
+        }
         ## weights will change in oecosimu thus need to be recalculated
         if (weights == "prop")
-            wt <- lapply(1:nl, function(i) apply(tmp[[i]], 1, function(z) sum(z) / sum(matr)))
-            else wt <- lapply(1:nl, function(i) rep(1 / NROW(tmp[[i]]), NROW(tmp[[i]])))
-        a <- sapply(1:nl, function(i) sum(divfun(tmp[[i]]) * wt[[i]]))
+            wt <- lapply(1:nlevs, function(i) apply(tmp[[i]], 1, function(z) sum(z) / sumMatr))
+            else wt <- lapply(1:nlevs, function(i) rep(1 / NROW(tmp[[i]]), NROW(tmp[[i]])))
+        a <- sapply(1:nlevs, function(i) sum(divfun(tmp[[i]]) * wt[[i]]))
         if (relative)
             a <- a / a[length(a)]
-        b <- sapply(2:nl, function(i) a[i] - a[(i-1)])
+        b <- sapply(2:nlevs, function(i) a[i] - a[(i-1)])
         c(a, b)
     }
-    sim <- oecosimu(matr, wdivfun, method = "permat", nsimul=nsimul,
+    sim <- oecosimu(lhs, wdivfun, method = "permat", nsimul=nsimul,
         burnin=control$burnin, thin=control$thin, control=control)
-    nam <- c(paste("alpha", 1:(nl-1), sep="."), "gamma",
-        paste("beta", 1:(nl-1), sep="."))
+    nam <- c(paste("alpha", 1:(nlevs-1), sep="."), "gamma",
+        paste("beta", 1:(nlevs-1), sep="."))
     names(sim$statistic) <- attr(sim$oecosimu$statistic, "names") <- nam
     attr(sim, "index") <- index
     attr(sim, "weights") <- weights
-    attr(sim, "n.levels") <- nl
+    attr(sim, "n.levels") <- nlevs
+    attr(sim, "terms") <- tlab
+    attr(sim, "model") <- rhs
     class(sim) <- c("adipart", "list")
     sim
 }
