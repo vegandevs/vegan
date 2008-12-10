65d78da9371282666d2fe58b6d1def1604abd761
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-12-10 04:45:34 +0000

    beauty salon for R functions
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@631 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/plot.permat.R
+++ R/plot.permat.R
@@ -1,11 +1,17 @@
 ## S3 plot method for permat
 `plot.permat` <-
-function(x, ylab="Bray-Curtis dissimilarity", xlab="Runs", col=c(2,4), lty=c(1,2), plot=TRUE, text=TRUE, ...)
+function(x, ylab, xlab, col, lty, plot=TRUE, text=TRUE, ...)
 {
+    if (missing(ylab))
+        ylab <- "Bray-Curtis dissimilarity"
+    if (missing(xlab))
+        xlab <- "Runs"
+    if (missing(col))
+        col <- c(2,4)
+    if (missing(lty))
+        lty <- c(1,2)
     n <- attr(x, "times")
     bray <- numeric(n)
-#    for (i in 1:n) bray[i] <- sum(abs(x$orig-x$perm[[i]]))/sum(x$orig+x$perm[[i]])
-#    bray <- sapply(x$perm, function(z) sum(abs(x$orig - z)) / sum(x$orig + z))
     bray <- summary(x)$bray
     if (plot) {
         plot(bray,type="n",ylab=ylab,xlab=xlab, ...)
