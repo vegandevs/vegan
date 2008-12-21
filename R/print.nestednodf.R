eb6b149396e566f8d2f61e588a9111899f5cb566
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-12-21 08:00:52 +0000

    print.nestednodf to match generic, and help page explains what print() returns
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@650 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/print.nestednodf.R
+++ R/print.nestednodf.R
@@ -1,9 +1,9 @@
 `print.nestednodf` <-
-    function(x)
+    function(x, ...)
 {    
-    cat("N columns:", format(x$statistic["N.columns"]), "\n")
-    cat("N rows:", format(x$statistic["N.rows"]), "\n")
-    cat("NODF:", format(x$statistic["NODF"]), "\n")
-    cat("Matrix fill:", format(x$fill), "\n")
+    cat("N columns  :", format(x$statistic["N.columns"], ...), "\n")
+    cat("N rows     :", format(x$statistic["N.rows"], ...), "\n")
+    cat("NODF       :", format(x$statistic["NODF"], ...), "\n")
+    cat("Matrix fill:", format(x$fill, ...), "\n")
     invisible(x)
 }
