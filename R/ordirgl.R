ec939e9eeab817f9296248ef9ad8b2ca0a06b9de
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-01-28 16:37:53 +0000

    removed workarounds for rgl versions pre 0.65 in ordirgl & orgltext
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@677 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/ordirgl.R
+++ R/ordirgl.R
@@ -4,7 +4,6 @@
 {
     if (!require(rgl)) 
         stop("Requires package 'rgl'")
-    oldpak <- compareVersion(packageDescription("rgl", field="Version"), "0.65") == -1
     x <- scores(object, display = display, choices = choices, 
                 ...)
     if (ncol(x) < 3) 
@@ -15,18 +14,17 @@
     else if (type == "t") {
         if (missing(text)) 
             text <- rownames(x)
-        rgl.texts(x[, 1], x[, 2], x[, 3], text, ...,
-                  if (oldpak) justify = "center" else adj = 0.5)
+        rgl.texts(x[, 1], x[, 2], x[, 3], text, adj = 0.5, ...)
     }
     rgl.lines(range(x[, 1]), c(0, 0), c(0, 0), col = ax.col)
     rgl.lines(c(0, 0), range(x[, 2]), c(0, 0), col = ax.col)
     rgl.lines(c(0, 0), c(0, 0), range(x[, 3]), col = ax.col)
     rgl.texts(1.1 * max(x[, 1]), 0, 0, colnames(x)[1], col = ax.col, 
-              if (oldpak) justify = "center" else adj = 0.5)
+              adj = 0.5)
     rgl.texts(0, 1.1 * max(x[, 2]), 0, colnames(x)[2], col = ax.col, 
-              if (oldpak) justify = "center" else adj = 0.5)
+              adj = 0.5)
     rgl.texts(0, 0, 1.1 * max(x[, 3]), colnames(x)[3], col = ax.col, 
-              if (oldpak) justify = "center" else adj = 0.5)
+              adj = 0.5)
     if (!missing(envfit) || !is.null(object$CCA)) {
         if (!missing(envfit)) 
             object <- envfit
@@ -35,7 +33,7 @@
         if (!is.null(cn) && !any(is.na(cn))) {
             bp <- bp[!(rownames(bp) %in% rownames(cn)), , drop = FALSE]
             rgl.texts(cn[, 1], cn[, 2], cn[, 3], rownames(cn), 
-                      col = arr.col, if (oldpak) justify = "center" else adj = 0.5)
+                      col = arr.col, adj = 0.5)
             rgl.points(cn[, 1], cn[, 2], cn[, 3], size = 5, col = arr.col)
         }
         if (!is.null(bp) && nrow(bp) > 0) {
@@ -50,7 +48,7 @@
                                                             bp[i, 3]), col = arr.col)
                 rgl.texts(1.1 * bp[i, 1], 1.1 * bp[i, 2], 1.1 * 
                           bp[i, 3], rownames(bp)[i], col = arr.col,
-                          if (oldpak) justify = "center" else adj = 0.5)
+                          adj = 0.5)
             }
         }
     }
--- R/orgltext.R
+++ R/orgltext.R
@@ -2,12 +2,10 @@
     function (object, text, display = "sites", choices = 1:3, justify = "center",  
               adj = 0.5, ...) 
 {
-    oldpak <- compareVersion(packageDescription("rgl", field="Version"), "0.65") == -1
     x <- scores(object, display = display, choices = choices, 
                 ...)
     if (missing(text)) 
         text <- rownames(x)
-    rgl.texts(x[, 1], x[, 2], x[, 3], text,
-              if (oldpak) justify = justify else adj = adj,  ...)
+    rgl.texts(x[, 1], x[, 2], x[, 3], text, adj = adj,  ...)
     invisible()
 }
