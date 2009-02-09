2227f307aeb6f08060be8660c3eb39e7579565f5
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-02-09 06:50:24 +0000

    contribdiv function based on Lu et al 2007 added, documentation needs more work
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@681 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/contribdiv.R
@@ -0,0 +1,51 @@
+## Contribution diversity
+## Lu, H.P., H.H. Wagner and X.Y. Chen (2007). 
+## A contribution diversity approach to evaluate species diversity. 
+## Basic and Applied Ecology 8: 1 -12.
+contribdiv <-
+function(comm, index = c("richness", "simpson"), relative = FALSE, scaled = TRUE, drop.zero = FALSE)
+{
+
+    index <- match.arg(index)
+
+    x <- comm[rowSums(comm) > 0, colSums(comm) > 0]
+    n <- nrow(x)
+    S <- ncol(x)
+
+    if (index == "richness") {
+        n.i <- colSums(x > 0)
+        S.k <- rowSums(x > 0)
+        alpha <- S.k / n
+        beta <- apply(x, 1, function(z) sum((n - n.i[z > 0]) / (n * n.i[z > 0])))
+        denom <- 1
+    } else {
+        P.ik <- decostand(x, "total")
+        P.i <- apply(P.ik, 2, function(z) sum(z) / n)
+        P.i2 <- matrix(P.i, n, S, byrow=TRUE)
+        alpha <- diversity(x, "simpson")
+        beta <- rowSums(P.ik * (P.ik - P.i2))
+        denom <- n
+    }
+    gamma <- alpha + beta
+    D <- sum(beta) / sum(gamma)
+    if (relative) {
+        denom <- if (scaled)
+            {denom * sum(gamma)} else 1
+        alpha <- (alpha - mean(alpha)) / denom
+        beta <- (beta - mean(beta)) / denom
+        gamma <- (gamma - mean(gamma)) / denom
+    }
+    rval <- data.frame(alpha = alpha, beta = beta, gamma = gamma)
+    if (!drop.zero && nrow(comm) != n) {
+        nas <- rep(NA, nrow(comm))
+        rval2 <- data.frame(alpha = nas, beta = nas, gamma = nas)
+        rval2[rowSums(comm) > 0, ] <- rval
+        rval <- rval2
+    }
+    attr(rval, "diff.coef") <- D
+    attr(rval, "index") <- index
+    attr(rval, "relative") <- relative
+    attr(rval, "scaled") <- scaled
+    class(rval) <- c("contribdiv", "data.frame")
+    return(rval)
+}
---
+++ R/plot.contribdiv.R
@@ -0,0 +1,18 @@
+plot.contribdiv <-
+function(x, sub, xlab, ylab, ylim, col, ...) {
+    y <- x[,c(1,3)]
+    if (missing(ylab))
+        ylab <- paste("Diversity components (", attr(x, "index"), ")", sep = "")
+    if (missing(xlab))
+        xlab <- "Sites"
+    if (missing(sub))
+        sub <- paste("Differentiation coefficient = ", round(attr(x, "diff.coef"),3), sep = "")
+    if (missing(ylim))
+        ylim <- c(0, max(y))
+    if (missing(col))
+        col <- c("lightgrey", "darkgrey")
+    matplot(y, type = "n", sub=sub,xlab=xlab,ylab=ylab,...)
+    polygon(c(1,1:nrow(y),nrow(y)), c(0,y$gamma,0), col=col[1])
+    polygon(c(1,1:nrow(y),nrow(y)), c(0,y$alpha,0), col=col[2])
+    invisible(x)
+}
