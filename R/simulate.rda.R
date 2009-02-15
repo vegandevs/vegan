a02eb9e94bd4e9fcc60e206237364ec8b91327c8
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-02-15 12:05:20 +0000

    added simulate.rda
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@684 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/simulate.rda.R
@@ -0,0 +1,40 @@
+`simulate.rda` <-
+    function(object, nsim = 1, seed = NULL, ...) 
+{
+    ## First check cases that won't work (yet?)
+    if (!is.null(object$pCCA))
+        stop("not yet implemented for partial models")
+    ## Handle RNG: code directly from stats::simulate.lm
+    if (!exists(".Random.seed", envir = .GlobalEnv, inherits = FALSE)) 
+        runif(1)
+    if (is.null(seed)) 
+        RNGstate <- get(".Random.seed", envir = .GlobalEnv)
+    else {
+        R.seed <- get(".Random.seed", envir = .GlobalEnv)
+        set.seed(seed)
+        RNGstate <- structure(seed, kind = as.list(RNGkind()))
+        on.exit(assign(".Random.seed", R.seed, envir = .GlobalEnv))
+    }
+    ## Proper simulation: very similar for simulate.lm, but produces a
+    ## response matrix.
+    if (nsim > 1)
+        .NotYetUsed("nsim")
+    ftd <- fitted(object)
+    ans <- as.data.frame(ftd + matrix(rnorm(length(ftd), 
+        sd = outer(rep(1,nrow(ftd)), sd(object$CA$Xbar))), 
+        nrow = nrow(ftd)))
+    attr(ans, "seed") <- RNGstate
+    ans
+}
+
+`simulate.cca` <-
+    function(object, nsim = 1, seed = NULL, ...)
+{
+    .NotYetImplemented()
+}
+
+`simulate.capscale` <-
+    function(object, nsim = 1, seed = NULL, ...)
+{
+    .NotYetImplemented()
+}
