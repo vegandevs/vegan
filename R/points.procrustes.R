806856e5916dab503d36ac4d950866035dc30570
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-02-19 17:36:34 +0000

    added text.procrustes()
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@689 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/points.procrustes.R
+++ R/points.procrustes.R
@@ -1,8 +1,19 @@
-"points.procrustes" <-
+`points.procrustes` <-
     function(x, display = c("target","rotated"), ...)
 {
     display <- match.arg(display)
     x <- if (display == "target") x$X else x$Yrot
     points(x, ...)
+    invisible()
+}
+
+`text.procrustes` <-
+    function(x, display = c("target","rotated"), labels, ...)
+{
+    display <- match.arg(display)
+    x <- if (display == "target") x$X else x$Yrot
+    if (missing(labels))
+        labels <- rownames(x)
+    text(x, labels = labels, ...)
     invisible()
 }
