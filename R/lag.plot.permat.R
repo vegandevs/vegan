4fe1f377b58aafed9075291583418e6eb18e4d46
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-02-24 19:14:55 +0000

    Box-Pierce test added to lag.plot.permat
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@704 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/lag.plot.permat.R
+++ R/lag.plot.permat.R
@@ -1,17 +1,33 @@
 `lag.plot.permat` <-
-function(x, type = "bray", ...)
+function(x, type = "bray", sub=NULL, ...)
 {
     type <- match.arg(type, c("bray", "chisq"))
     ## this duality is required, because ylab can not be specified for lag.plot
     switch(type,
         "bray" = {
             out <- Bray.Curtis.Dissimilarities <- summary(x)[[type]]
-            lag.plot(Bray.Curtis.Dissimilarities, ...)
+            if (is.null(sub)) {
+                Bt <- Box.test(out)[c(1,2,3)]
+                names(Bt)[c(1,2)] <- c("X-squared", "df")
+                Bt[[1]] <- round(Bt[[1]], 3)
+                Bt[[3]] <- if (Bt[[3]] < 0.0001)
+                    "< 0.0001" else round(Bt[[3]], 3)
+                sub <- paste("Box-Pierce test: ", paste(names(Bt), Bt, collapse=", "), collapse="")
+            }
+            lag.plot(Bray.Curtis.Dissimilarities, sub=sub, ...)
+            invisible(out)
         },
         "chisq" = {
             out <- Chi.Squared.Values <- summary(x)[[type]]
-            lag.plot(Chi.Squared.Values, ...)
+            if (is.null(sub)) {
+                Bt <- Box.test(out)[c(1,2,3)]
+                names(Bt)[c(1,2)] <- c("X-squared", "df")
+                Bt[[1]] <- round(Bt[[1]], 3)
+                Bt[[3]] <- if (Bt[[3]] < 0.0001)
+                    "< 0.0001" else round(Bt[[3]], 3)
+                sub <- paste("Box-Pierce test: ", paste(names(Bt), Bt, collapse=", "), collapse="")
+            }
+            lag.plot(Chi.Squared.Values, sub=sub, ...)
+            invisible(out)
         })
-#    lag.plot(summary(x)[[match.arg(type, c("bray", "chisq"))]], ...)
-    invisible(out)
 }
