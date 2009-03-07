2f4eababa88395d51510d8cef50ccb67a596943f
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-03-07 19:03:37 +0000

    cleanup after check
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@724 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/print.diagnose.permat.R
+++
@@ -1,8 +0,0 @@
-print.diagnose.permat <-
-function(x, ...) {
-    cat("First order autoregressive model\n")
-    print(x$arima)
-    cat("\nTest of independence\n")
-    print(x$box.ts)
-    print(x$box.resid)
-}
