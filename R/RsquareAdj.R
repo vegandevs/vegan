70c8c0b6101de5e1884ac600227b5613d2e98841
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-04-21 09:54:57 +0000

    Made RsquareAdj generic, with methods for 'rda', 'cca' (NA), 'lm' and 'glm' objects (on P Legendre's request)
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@809 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/RsquareAdj.R
+++ R/RsquareAdj.R
@@ -1,9 +1,51 @@
-"RsquareAdj" <-
-function(x, n, m, ...)
+`RsquareAdj` <-
+    function(x, ...)
+{
+    UseMethod("RsquareAdj")
+}
+
+
+`RsquareAdj.default` <-
+    function(x, n, m, ...)
 {
     if (m >= (n-1))
         NA
     else
         1 - (1-x)*(n-1)/(n-m-1)
+}
+
+## Use this with rda() results
+`RsquareAdj.rda` <-
+    function(x, ...)
+{
+    R2 <- x$CCA$tot.chi/x$tot.chi
+    m <- x$CCA$rank
+    n <- 1 + m + x$CA$rank + ifelse(is.null(x$pCCA), 0, x$pCCA$rank)
+    radj <- RsquareAdj(R2, n, m)
+    list(r.squared = R2, adj.r.squared = radj)
+}
+
+## cca result: no RsquareAdj
+RsquareAdj.cca <-
+    function(x, ...)
+{
+    NA
+}
+
+## Linear model: take the result from the summary
+RsquareAdj.lm <-
+  function(x, ...)
+{
+    summary(x)[c("r.squared", "adj.r.squared")]
+}
+
+## Generalized linear model: R2-adj only with Gaussian model
+RsquareAdj.glm <-
+    function(x, ...)
+{
+    if (family(x)$family == "gaussian")
+        summary.lm(x)[c("r.squared", "adj.r.squared")]
+    else
+        NA
 }
 
