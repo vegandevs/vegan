a8522112c92705b78e7f1cfb0ea6110c01bad1a2
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-04-23 13:31:16 +0000

    added a Lattice variant of ordiarrows for ordixyplot
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@813 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/panel.ordi.R
+++ R/panel.ordi.R
@@ -1,18 +1,36 @@
-`panel.ordi` <-
-    function(x, y, biplot, type = type,  ...)
+panel.ordi <-
+function(x, y, biplot, type = type,  ...)
 {
     panel.xyplot(x, y, type = type,  ...)
     tp <- trellis.par.get()
+    sp <- tp$superpose.symbol
+    ps <- tp$plot.symbol
     if ("biplot" %in% type && !is.null(biplot$arrows)) {
         panel.arrows(0, 0, biplot$arrows[,2], biplot$arrows[,1],
-                     col=tp$superpose.symbol$col, ...)
+                     col=sp$col, ...)
     }
     if ("biplot" %in% type && !is.null(biplot$centres)) {
         panel.xyplot(biplot$centres[,2], biplot$centres[,1],
-                     col = tp$plot.symbol$col, 
-                     pch = "+", cex = 3*tp$plot.symbol$cex, lwd=2,
+                     col = ps$col, 
+                     pch = "+", cex = 3*tps$cex, lwd=2,
                      ...)
+    }
+    if ("arrows" %in% type) {
+        panel.superpose(x, y, panel.groups= "panel.ordiarrows",  ...)
     }
     panel.abline(h=0, lty = 3)
     panel.abline(v=0, lty = 3)
+}
+
+## needed for "arrows" %in% type
+panel.ordiarrows <-
+function(x, y, subscripts, 
+         ends = "last", type = "open", length = 0.25, angle = 30, ...) 
+{
+    n <- length(x)
+    panel.arrows(x[-n], y[-n], x[-1], y[-1], ends = ends, type = "open",
+                 length = length, angle = angle, code = code, 
+                 col = trellis.par.get("superpose.line")$col,
+                 lwd = trellis.par.get("superpose.line")$lwd, 
+                 ) 
 }
