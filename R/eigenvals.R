50123e03df275313687e37b6f7377c60f2c6344d
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-05-31 14:44:50 +0000

    eigenvals got class and a summary method
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@843 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/eigenvals.R
+++ R/eigenvals.R
@@ -11,44 +11,71 @@
 {
     ## svd and eigen return unspecified 'list', see if this could be
     ## either of them
+    out <- NA
     if (is.list(x)) {
         ## eigen
         if (length(x) == 2 && all(names(x) %in% c("values", "vectors")))
-            return(x$values)
+            out <- x$values
         ## svd: return squares of singular values
         if (length(x) == 3 && all(names(x) %in% c("d", "u", "v")))
-            return(x$d^2)
-        else
-            stop("No eigenvalues found")
+            out <- x$d^2
     }
-    else
-        stop("No eigenvalues found")
+    class(out) <- "eigenvals"
+    out
 }
 
 ## squares of sdev 
 `eigenvals.prcomp` <-
     function(x)
 {
-    x$sdev^2
+    out <- x$sdev^2
+    names(out) <- colnames(x$rotation)
+    class(out) <- "eigenvals"
+    out
 }
 
 ## squares of sdev
 `eigenvals.princomp` <-
     function(x)
 {
-    x$sdev^2
+    out <- x$sdev^2
+    class(out) <- "eigenvals"
+    out
 }
 
 ## concatenate constrained and unconstrained eigenvalues in cca, rda
 ## and capscale (vegan) -- ignore pCCA component
 `eigenvals.cca` <- function(x)
 {
-   c(x$CCA$eig, x$CA$eig) 
+   out <- c(x$CCA$eig, x$CA$eig)
+   class(out) <- c("eigenvals")
+   out
 }
 
 ## wcmdscale (in vegan)
 `eigenvals.wcmdscale` <-
     function(x)
 {
-    x$eig
+    out <- x$eig
+    class(out) <-"eigenvals"
+    out
+}
+
+`print.eigenvals` <-
+    function(x, ...)
+{
+    print(unclass(x), ...)
+    invisible(x)
+}
+
+`summary.eigenvals` <-
+    function(object, ...)
+{
+    vars <- object/sum(object)
+    importance <- rbind(`Eigenvalue` = object,
+                        `Proportion Explained` = round(vars, 5),
+                        `Cumulative Proportion`= round(cumsum(vars), 5))
+    out <- list(importance = importance)
+    class(out) <- c("summary.eigenvals", "summary.prcomp")
+    out
 }
