64917c818c937388cb833a56a69922f3b1074913
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-07-04 06:58:07 +0000

    wcmdscale: emacs didn't save the last tiny change, but here it comes
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@885 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/wcmdscale.R
+++ R/wcmdscale.R
@@ -34,7 +34,7 @@ function(d, k, eig = FALSE, add = FALSE, x.ret = FALSE, w)
     ## If 'k' not given, find it as the number of positive
     ## eigenvalues, and also save negative eigenvalues
     negaxes <- NULL
-    if (missing(k) || k > sum(e$values > ZERO)) {
+    if (missing(k) || k > sum(e$) {
         k <- sum(e$values > ZERO)
         if (any(e$values < 0))
             negaxes <- points[, e$values < 0, drop = FALSE]
