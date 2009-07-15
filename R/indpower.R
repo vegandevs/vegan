0ff047427265d849ce8e35db3253e142982a2441
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-07-15 18:59:40 +0000

    indpower: new function
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@894 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/indpower.R
@@ -0,0 +1,21 @@
+indpower <-
+function(x, type=0)
+{
+    x <- as.matrix(ifelse(x > 0, 1, 0))
+    if (NCOL(x) < 2)
+        stop("provide at least 2 columns for 'x'")
+    if (!(type %in% 0:2))
+        stop("'type' must be in c(0, 1, 2)")
+    n <- nrow(x)
+    j <- t(x) %*% x
+    ip1 <- sweep(j, 1, diag(j), "/")
+    ip2 <- 1 - sweep(-sweep(j, 2, diag(j), "-"), 1, n - diag(j), "/")
+    ip <- sqrt(ip1 * ip2)
+    out <- switch(as.character(type),
+        "0" = ip,
+        "1" = ip1,
+        "2" = ip2)
+    colnames(out) <- paste("i", colnames(out), sep=".")
+    rownames(out) <- paste("t", rownames(out), sep=".")
+    out
+}
