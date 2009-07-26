21939883cfa0f782fa0b33c08a234eaea8d2ad53
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-07-26 14:57:40 +0000

    metaMDS accepts now user supplied dissimilarities
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@905 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/metaMDS.R
+++ R/metaMDS.R
@@ -4,11 +4,22 @@
               plot = FALSE, previous.best, old.wa = FALSE, ...) 
 {
     commname <- deparse(substitute(comm))
-    if (inherits(comm, "dist"))
-        stop("metaMDS does not accept dist objects, but needs original data frame")
-    dis <- metaMDSdist(comm, distance = distance, autotransform = autotransform, 
-                       noshare = noshare, trace = trace, commname = commname, 
-                       ...)
+    if (inherits(comm, "dist")) {
+        dis <- comm
+        if (is.null(attr(dis, "method")))
+            attr(dis, "method") <- "user supplied"
+        wascores <- FALSE
+    } else if (length(dim(comm) == 2) && ncol(comm) == nrow(comm) &&
+                all(comm == t(comm))) {
+        dis <- as.dist(m)
+        attr(dis, "method") <- "user supplied"
+        wascores <- FALSE
+    } else {
+        dis <- metaMDSdist(comm, distance = distance,
+                           autotransform = autotransform, 
+                           noshare = noshare, trace = trace,
+                           commname = commname, ...)
+    }
     if (missing(previous.best)) 
         previous.best <- NULL
     out <- metaMDSiter(dis, k = k, trymax = trymax, trace = trace, 
@@ -27,6 +38,8 @@
     out$points <- points
     out$species <- wa
     out$call <- match.call()
+    if (is.null(out$data))
+        out$data <- commname
     class(out) <- "metaMDS"
     out
 }
