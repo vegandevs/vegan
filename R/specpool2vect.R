789322abeaf01532b51e76441bce15eb85bb5504
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-08-12 16:23:46 +0000

    simpler specpool2vect (internal beauty)
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@918 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/specpool2vect.R
+++ R/specpool2vect.R
@@ -3,7 +3,5 @@ function(X, index = c("Jack.1","Jack.2", "Chao", "Boot", "Species"))
 {
     pool <- attr(X, "pool")
     index <- match.arg(index)
-    sel <- paste("X", index, sep = "$")
-    sel <- eval(parse(text=sel))
-    sel[pool]
+    X[[index]][pool]
 }
