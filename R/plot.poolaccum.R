be013571519e59df190b11e11576aa7f23f7015f
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-08-12 16:00:20 +0000

    poolaccum: plot methods and more docuentation
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@917 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/plot.poolaccum.R
@@ -0,0 +1,14 @@
+`plot.poolaccum` <-
+    function(x, alpha = 0.05, type = c("l","g"), ...)
+{
+    require(lattice) || stop("Needs package 'lattice'")
+    m <- summary(x, alpha = alpha, ...)
+    n <- nrow(m[[1]])
+    Size <- as.vector(sapply(m, function(x) c(x[,1], x[,1], rev(x[,1]))))
+    Richness <- as.vector(sapply(m, function(x) c(x[,2], x[,3], rev(x[,4]))) )
+    indnames <- as.character(sapply(m, function(x) colnames(x[,2, drop=FALSE])))
+    Index <- factor(rep(indnames, each = 3 * n), levels = indnames)
+    lintype <- rep(c(rep("aver", n), rep("envel", 2*n)), length=length(Size))
+    xyplot(Richness ~  Size | Index, as.table = TRUE, groups = lintype,
+           type = type, ...)
+}
