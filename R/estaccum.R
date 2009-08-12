b29e81fa8f0e6e8ff079ae2a38a4fec64cca6743
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-08-12 17:43:45 +0000

    estaccum: new function similar to poolaccum but using estimateR
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@920 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/estaccum.R
@@ -0,0 +1,21 @@
+##" Individual based accumulation model. Similar to poolaccum but uses
+##estimateR. Inherits from "poolaccum" class and uses its methods.
+`estaccumR` <-
+    function(x, permutations = 100)
+{
+    n <- nrow(x)
+    N <- seq_len(n)
+    S <- chao <- ace <- matrix(0, nrow = n, ncol = permutations)
+    for (i in 1:permutations) {
+        take <- sample(n)
+        tmp <- estimateR(apply(x, 2, cumsum))
+        S[,i] <- tmp[1,]
+        chao[,i] <- tmp[2,]
+        ace[, i] <- tmp[4,]
+    }
+    means <- cbind(N = N, S = rowMeans(S), Chao = rowMeans(chao),
+                   ACE = rowMeans(ace))
+    out <- list(S = S, chao = chao, ace = ace, N = N, means = means)
+    class(out) <- c("accumR", "poolaccum")
+    out
+}
