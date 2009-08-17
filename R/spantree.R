3af5530cbc6109ed2ec540da4ee96ce02c4bccb8
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-08-17 14:10:09 +0000

    spantree got a 'call' item, and first argument is 'd' like in hclust()
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@927 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/spantree.R
+++ R/spantree.R
@@ -1,14 +1,14 @@
 "spantree" <-
-    function (dis, toolong = 0) 
+    function (d, toolong = 0) 
 {
-    dis <- as.dist(dis)
+    dis <- as.dist(d)
     n <- attr(dis, "Size")
     labels <- labels(dis)
     dis <- .C("primtree", dist = as.double(dis), toolong = as.double(toolong), 
               n = as.integer(n), val = double(n + 1),
               dad = integer(n + 1), NAOK = TRUE, PACKAGE = "vegan")
     out <- list(kid = dis$dad[2:n] + 1, dist = dis$val[2:n],
-                labels = labels)
+                labels = labels, call = match.call())
     class(out) <- "spantree"
     out
 }
