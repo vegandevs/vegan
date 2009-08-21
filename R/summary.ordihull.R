34190a8fdc994789b2760ace8028443a8f7c0902
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-08-21 12:38:45 +0000

    simpler way of calculating the area of a polygon
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@936 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/summary.ordihull.R
+++ R/summary.ordihull.R
@@ -1,29 +1,15 @@
-### Centres and areas of convex hulls. The area of the *convex* polygon
-### is found as the *sum* of the areas of triangles. 
+### Centres and areas of convex hulls (simple polygons).
 `summary.ordihull` <-
     function(object, ...)
 {
-    ## The area of triangle from vertices using eq. 8.10 of Spiegel,
-    ## Liu & Lipschitz (1999), Mathematical Handbook of Formulas and
-    ## Tables (2nd ed.), McGraw & Hill. The hull is closed, so that
-    ## first and last point are identical.
-    triarea <- function(x) {
-        ones <- rep(1, 3)
-        if (nrow(x) < 4)
+    polyarea <- function(x) {
+        n <- nrow(x)
+        if (n < 4)
             return(0)
-        if (nrow(x) == 4)
-            return(abs(det(cbind(x[-1,], ones))/2))
-        else {
-            sol <- 0
-            cnt <- colMeans(x[-1,])
-            for (i in 2:nrow(x)) {
-                mat <- cbind(rbind(cnt, x[(i-1):i,]), ones)
-                sol <- sol + abs(det(mat)/2)
-            }
-            return(sol)
-        }
+        else
+            abs(sum(x[-n,1]*x[-1,2] - x[-1,1]*x[-n,2]))/2
     }
-    areas <- sapply(object, function(x) triarea(x))
+    areas <- sapply(object, function(x) polyarea(x))
     cnts <- sapply(object, function(x) colMeans(x[-1,]))
     rbind(cnts, `Area` = areas)
 }
