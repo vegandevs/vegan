780655f04cd7d39d1b21b6b8111988a84e2568a3
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-09-01 18:53:43 +0000

    indentation in coef.cca
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@980 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/coef.cca.R
+++ R/coef.cca.R
@@ -1,10 +1,10 @@
 "coef.cca" <-
 function (object, ...) 
 {
-        k <- !is.na(object$rowsum)
-	Q <- object$CCA$QR
-	u <- object$CCA$u[k,]
-	u <- sweep(u, 1, sqrt(object$rowsum[k]), "*")
-	qr.coef(Q, u)
+    k <- !is.na(object$rowsum)
+    Q <- object$CCA$QR
+    u <- object$CCA$u[k,]
+    u <- sweep(u, 1, sqrt(object$rowsum[k]), "*")
+    qr.coef(Q, u)
 }
 
