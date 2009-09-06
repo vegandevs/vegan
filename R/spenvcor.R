78425d502babe14a2364b54a23dcd4e58b4dadff
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-09-06 14:26:28 +0000

    spenvcor also mixed weights() with using internal cca object structures
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@993 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/spenvcor.R
+++ R/spenvcor.R
@@ -5,9 +5,11 @@ function (object)
         stop("Needs results from constrained ordination")
     u <- object$CCA$u
     wa <- object$CCA$wa
-    r <- sqrt(weights(object, "sites"))
-    u <- sweep(u, 1, r, "*")
-    wa <- sweep(wa, 1, r, "*")
+    if (!inherits(object, "rda")) { # is CCA
+        r <- sqrt(object$rowsum)
+        u <- sweep(u, 1, r, "*")
+        wa <- sweep(wa, 1, r, "*")
+    }
     diag(cor(u, wa))
 }
 
