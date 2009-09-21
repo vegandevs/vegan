8c34601c18704851df1ba9a8eee5c672275146ac
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-09-21 17:24:40 +0000

    calibrate.ordisurf always returns type = 'response'
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@1019 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/calibrate.ordisurf.R
+++ R/calibrate.ordisurf.R
@@ -2,7 +2,7 @@
     function(object, newdata, ...)
 {
     if (missing(newdata))
-        fit <- fitted(object)
+        fit <- predict(object, type = "response", ...)
     else {
         ## Got only a vector of two coordinates
         if (is.vector(newdata) && length(newdata) == 2) 
@@ -13,7 +13,7 @@
                 stop("needs a matrix or a data frame with two columns")
             newdata <- data.frame(x1 = newdata[,1], x2 = newdata[,2])
         }
-        fit <- predict(object, newdata = newdata, ...)
+        fit <- predict(object, newdata = newdata, type = "response", ...)
     }
     fit
 }
