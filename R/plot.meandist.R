d712e49f8b4420af5f919e4d45e54e568830f6f3
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-09-25 05:51:51 +0000

    plot.meandist draws now histogram plots as an alternative to dendrograms
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@1023 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/plot.meandist.R
+++ R/plot.meandist.R
@@ -1,19 +1,37 @@
 `plot.meandist` <-
-    function(x, cluster = "average", ylim, ...) 
+    function(x, kind = c("dendrogram", "histogram"),  cluster = "average", ylim,
+             axes = TRUE, ...) 
 {
+    kind <- match.arg(kind)
     n <- attr(x, "n")
-    cl <- hclust(as.dist(x), method = cluster, members = n)
-    cl <- as.dendrogram(cl, hang = 0)
-    w <- diag(x)[labels(cl)]
-    tr <- unlist(dendrapply(cl, function(n) attr(n, "height")))
-    root <- attr(cl, "height")
-    if (missing(ylim))
-        ylim <- range(c(w, tr, root), na.rm = TRUE)
-    plot(cl, ylim = ylim, leaflab = "none", ...)
-    for (i in 1:length(w)) segments(i, tr[i], i, w[i])
-    pos <- ifelse(w < tr, 1, 3)
-    pos[is.na(pos)] <- 1
-    w[is.na(w)] <- tr[is.na(w)]
-    text(1:length(w), w, labels = labels(cl), pos = pos, srt = 0)
+    if (kind == "dendrogram") {
+        cl <- hclust(as.dist(x), method = cluster, members = n)
+        cl <- as.dendrogram(cl, hang = 0)
+        w <- diag(x)[labels(cl)]
+        tr <- unlist(dendrapply(cl, function(n) attr(n, "height")))
+        root <- attr(cl, "height")
+        if (missing(ylim))
+            ylim <- range(c(w, tr, root), na.rm = TRUE)
+        plot(cl, ylim = ylim, leaflab = "none", axes = axes, ...)
+        for (i in 1:length(w)) segments(i, tr[i], i, w[i])
+        pos <- ifelse(w < tr, 1, 3)
+        pos[is.na(pos)] <- 1
+        w[is.na(w)] <- tr[is.na(w)]
+        text(1:length(w), w, labels = labels(cl), pos = pos, srt = 0)
+    } else {
+        w <- diag(x)
+        tr <- rep(summary(x)$B, length(w))
+        if (missing(ylim))
+            ylim <- range(c(w, tr), na.rm = TRUE)
+        plot(1:length(w), tr, ylim = ylim, axes = FALSE, xlab = "", ylab = "",
+             type = "l", ...)
+        if (axes)
+            axis(2, ...)
+        for (i in 1:length(w)) segments(i, tr, i, w[i])
+        pos <- ifelse(w < tr, 1, 3)
+        pos[is.na(pos)] <- 1
+        text(1:length(w), w, labels = names(n), pos = pos, srt = 0,
+             xpd = TRUE, ...)
+    }
 }
 
