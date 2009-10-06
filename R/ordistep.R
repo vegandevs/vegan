7cc6811090977fd76f13f8d473b5d3047df16161
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-10-06 19:16:03 +0000

    ordistep should be functional and have similar UI as step
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@1034 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/ordistep.R
+++ R/ordistep.R
@@ -1,18 +1,45 @@
 `ordistep` <-
-    function(object, scope, Pin = 0.05, Pout = 0.1,
-             pstep = 100, perm.max = 1000, steps=50, trace = TRUE, ...)
+    function(object, scope, direction =c("both", "backward", "foreward"),
+             Pin = 0.05, Pout = 0.1, pstep = 100, perm.max = 1000,
+             steps=50, trace = TRUE, ...)
 {
     if (!inherits(object, "cca"))
-        stop("function can be only used to 'cca' and related objects")
-    if (missing(scope))
-        stop("absolutely needs scope")
-    mod <- eval.parent(update(object, . ~  1))
+        stop("function can be only used with 'cca' and related objects")
+    ## handling 'direction' and 'scope' directly copied from
+    ## stats::step()
+    md <- missing(direction)
+    direction <- match.arg(direction)
+    backward <- direction == "both" | direction == "backward"
+    forward <- direction == "both" | direction == "forward"
+    ffac <- attr(terms(object), "factors")
+    if (missing(scope)) {
+        fdrop <- numeric(0)
+        fadd <- ffac
+        if (md) 
+            forward <- FALSE
+    }
+    else {
+        if (is.list(scope)) {
+            fdrop <- if (!is.null(fdrop <- scope$lower)) 
+                attr(terms(update.formula(object, fdrop)), "factors")
+            else numeric(0)
+            fadd <- if (!is.null(fadd <- scope$upper)) 
+                attr(terms(update.formula(object, fadd)), "factors")
+        }
+        else {
+            fadd <- if (!is.null(fadd <- scope)) 
+                attr(terms(update.formula(object, scope)), "factors")
+            fdrop <- numeric(0L)
+        }
+    }
+    scope <- factor.scope(ffac, list(add = fadd, drop = fdrop))
+    mod <- object
     for (i in 1:steps){
         change <- NULL
         ## Consider dropping
-        if (length(drop.scope(mod))) {
-            aod <- drop1(mod, test="perm", pstep = pstep, perm.max = perm.max,
-                         alpha = Pout)
+        if (backward && length(scope$drop)) {
+            aod <- drop1(mod, scope = scope$drop, test="perm", pstep = pstep,
+                         perm.max = perm.max, alpha = Pout, trace = trace, ...)
             aod <- aod[-1,]
             o <- order(-aod[,5], aod[,4], aod[,2])
             aod <- aod[o,]
@@ -24,6 +51,8 @@
             if (aod[1,5] > Pout) {
                 change <- rownames(aod)[1]
                 mod <- eval.parent(update(mod, paste("~  .", change)))
+                scope <- factor.scope(attr(terms(mod), "factors"),
+                                      list(add = fadd, drop = fdrop))
                 if (trace) {
                     cat("\n")
                     print(mod$call)
@@ -31,9 +60,9 @@
             }
         }
         ## Consider adding
-        if (length(add.scope(formula(mod), scope))) {
-            aod <- add1(mod, scope = scope, test = "perm", pstep = pstep,
-                        perm.max = perm.max, alpha = Pin)
+        if (forward && length(scope$add)) {
+            aod <- add1(mod, scope = scope$add, test = "perm", pstep = pstep,
+                        perm.max = perm.max, alpha = Pin, trace = trace, ...)
             aod <- aod[-1,]
             o <- order(aod[,5], aod[,4], aod[,2])
             aod <- aod[o,]
@@ -45,6 +74,8 @@
             if (aod[1,5] <= Pin) {
                 change <- rownames(aod)[1]
                 mod <- eval.parent(update(mod, paste( "~  .",change)))
+                scope <- factor.scope(attr(terms(mod), "factors"),
+                                      list(add = fadd, drop = fdrop))
                 if (trace) {
                     cat("\n")
                     print(mod$call)
