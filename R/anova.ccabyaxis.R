c560df99fca8b4d4bd9040afe16a13713708c402
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-10-14 13:46:05 +0000

    anova.ccabyaxis failed if fitted model had no data= statement
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@1044 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/anova.ccabyaxis.R
+++ R/anova.ccabyaxis.R
@@ -8,11 +8,15 @@ function (object, cutoff = 1,  ...)
     if (is.null(object$terms)) 
         stop("Analysis is only possible for models fitted using formula")
     lc <- object$CCA$u
+    mf <- model.frame(object)
     ## Handle missing values in scores, both "omit" and "exclude" to
     ## match dims with data.
-    if (!is.null(object$na.action))
+    if (!is.null(object$na.action)) {
         lc <- stats:::napredict.exclude(object$na.action, lc)
-    newdata <- cbind(lc, eval(as.list(object$call)$data))
+        mf <- lapply(mf, function(x) stats:::napredict.exclude(object$na.action, x))
+        mf <- as.data.frame(mf)
+    }
+    newdata <- cbind(lc, mf)
     axnam <- colnames(lc)
     df <- c(rep(1, rnk), object$CA$rank)
     chi <- c(object$CCA$eig, Residual = object$CA$tot.chi)
