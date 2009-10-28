86ae70d44a196baf4c1d6abf78316ece3211d67e
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-10-28 09:33:25 +0000

    Adding 'pcnm' from sedarVegan with history: first veganification
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@1056 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/pcnm.R
+++ R/pcnm.R
@@ -1,19 +1,28 @@
 "pcnm" <-
-function(matdist,thresh=give.thresh(as.dist(matdist)))
+    function(matdist, threshold, support = c("vegan", "ade4"))
 {
-    matdist <- as.matrix(matdist)
-   
-   
-            mattrunc <- ifelse(matdist >thresh, 4*thresh,matdist)
-    wa.old <- options()$warn
-    options(warn = -1)
-    mypcnm <- cmdscale(mattrunc,k=min(dim(matdist))-1,eig=TRUE)
-    eq0 <- apply(as.matrix(mypcnm$eig/max((mypcnm$eig))),1,function(x) identical(all.equal(x, 0), TRUE))
-    inf0 <- ifelse(mypcnm$eig<0,TRUE,FALSE)
+    EPS <- sqrt(.Machine$double.eps)
+    wa.old <- options(warn = -1)
+    on.exit(options(wa.old))
+    matdist <- as.dist(matdist)
+    if (missing(threshold)) {
+        support <- match.arg(support)
+        threshold <- 
+            switch(support,
+                   vegan =  max(spantree(matdist)$dist),
+                   ade4 = max(neig2mat(mstree(matdist)) * as.matrix(matdist))
+                   )
+    }
+    matdist[matdist > threshold] <- 4*threshold
+    k <- attr(matdist, "Size") - 1
+    mypcnm <- cmdscale(matdist, k = k, eig = TRUE)
+    eq0 <- abs(mypcnm$eig/max((mypcnm$eig))) <= EPS
+    inf0 <- mypcnm$eig < 0
     res <- list()
     res$values <- mypcnm$eig[!(eq0|inf0)]
-    res$vectors <- mypcnm$points[,!(eq0|inf0)]
-    res$vectors <- sweep(res$vectors,2,sqrt(res$values),"/")
-    options(warn = wa.old)
-    return(res)
+    res$vectors <- mypcnm$points[,!(eq0|inf0), drop = FALSE]
+    res$vectors <- sweep(res$vectors, 2, sqrt(res$values), "/")
+    res$threshold <- threshold
+    class(res) <- "pcnm"
+    res
 }
