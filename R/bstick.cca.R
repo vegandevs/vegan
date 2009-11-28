274c8858cc2edcfba89267d57bd9d02175e5c412
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-11-28 22:09:59 +0000

    bstick.cca refuses to handle capscale result with negative eigenvalues, because I have no clue how to do this
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@1071 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/bstick.cca.R
+++ R/bstick.cca.R
@@ -5,7 +5,11 @@
         stop("'n' not of class \"cca\" or \"rda\"")
     if(!is.null(n$CCA))
         stop("'bstick' only for unconstrained models.")
-    ## need to select appropriate total invertia
+    ## No idea how to define bstick for capscale with negative
+    ## eigenvalues
+    if (inherits(n, "capscale") && !is.null(n$CA$imaginary.rank))
+        stop("'bstick' cannot be used for 'capscale' with imaginary component") 
+    ## need to select appropriate total inertia
     tot.chi <- n$CA$tot.chi
     n.comp <- n$CA$rank
     res <- bstick.default(n.comp, tot.chi, ...)
