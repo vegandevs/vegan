e39f0fef078a13c715dbf7731a1f86ecc498bef7
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-11-29 13:57:38 +0000

    implemented simulate.cca
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@1075 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/simulate.rda.R
+++ R/simulate.rda.R
@@ -26,14 +26,54 @@
                nrow = nrow(ftd)))
     else
         ans <- as.data.frame(ftd + object$CA$Xbar[indx,])
-    attr(ans, "seed") <- RNGstate
+    if (is.null(indx))
+        attr(ans, "seed") <- RNGstate
+    else
+        attr(ans, "seed") <- indx
     ans
 }
 
+### simulate. cca was cloned from simulate.rda.  Works with internal
+### Chi-square standardized form, and at the end back-standardizes
+### with row and column totals and matrix grand totals. This does not
+### still guarantee that all marginal totals are positive.
+
 `simulate.cca` <-
-    function(object, nsim = 1, seed = NULL, ...)
+    function(object, nsim = 1, seed = NULL, indx = NULL, ...)
 {
-    .NotYetImplemented()
+    ## Handle RNG: code directly from stats::simulate.lm
+    if (!exists(".Random.seed", envir = .GlobalEnv, inherits = FALSE)) 
+        runif(1)
+    if (is.null(seed)) 
+        RNGstate <- get(".Random.seed", envir = .GlobalEnv)
+    else {
+        R.seed <- get(".Random.seed", envir = .GlobalEnv)
+        set.seed(seed)
+        RNGstate <- structure(seed, kind = as.list(RNGkind()))
+        on.exit(assign(".Random.seed", R.seed, envir = .GlobalEnv))
+    }
+    ## Proper simulation: very similar for simulate.lm, but produces a
+    ## response matrix.
+    if (nsim > 1)
+        .NotYetUsed("nsim")
+    ftd <- fitted(object, type = "working")
+    ## pCCA: add partial Fit to the constrained
+    if (!is.null(object$pCCA))
+        ftd <- ftd + object$pCCA$Fit
+    if (is.null(indx))
+        ans <- as.data.frame(ftd + matrix(rnorm(length(ftd), 
+               sd = outer(rep(1,nrow(ftd)), sd(object$CA$Xbar))), 
+               nrow = nrow(ftd)))
+    else
+        ans <- as.data.frame(ftd + object$CA$Xbar[indx,])
+    ## From internal form to the original form with fixed marginal totals
+    rc <- object$rowsum %o% object$colsum
+    ans <- (ans * sqrt(rc) + rc) * object$grand.total
+    if (is.null(indx))
+        attr(ans, "seed") <- RNGstate
+    else
+        attr(ans, "seed") <- indx
+    ans
 }
 
 `simulate.capscale` <-
