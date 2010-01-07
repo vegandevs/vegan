f8684ced60538468e097682c18c2a429d8db69d0
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-01-07 11:27:20 +0000

    removed doubled adjustment for overdispersion in fieller.MOStest
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@1107 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/fieller.MOStest.R
+++ R/fieller.MOStest.R
@@ -2,18 +2,18 @@
     function (object, level = 0.95) 
 {
     smodel <- summary(object$mod)
+    ## overdispersion included in cov.scaled
     var <- smodel$cov.scaled
     fam <- family(object$mod)
-    od <- smodel$dispersion
     k <- coef(object$mod)
     b2 <- -2 * k[3]
     u <- -k[2]/2/k[3]
     alpha <- (1-level)/2
     limits <- numeric(2)
     names(limits) <- paste(round(100*(c(alpha, 1-alpha)), 1), "%") 
-    wvar <- var[2,2] * od
-    uvar <- 4 * var[3,3] * od
-    vvar <- -2 * var[2,3] * od
+    wvar <- var[2,2] 
+    uvar <- 4 * var[3,3] 
+    vvar <- -2 * var[2,3]
     z <- qnorm(1 - alpha)
     g <- z^2 * uvar/b2^2
     if (g >= 1) {
