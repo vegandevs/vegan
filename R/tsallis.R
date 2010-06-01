8c57a3185f3106cd4ae82d5c57027381bd82352b
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-06-01 08:22:35 +0000

    marginal (<10 %) speed-up
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@1210 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/tsallis.R
+++ R/tsallis.R
@@ -21,10 +21,10 @@ function (x, scales = seq(0, 2, 0.2), norm=FALSE, hill=FALSE)
         }
         else {
             if (scales[a] == 1) result[, a] <- diversity(x, "shannon")
-            if (scales[a] == 0) result[, a] <- apply(x > 0, 1, function(y) sum(y) - 1)
+            if (scales[a] == 0) result[, a] <- rowSums(x > 0) - 1
         }
         if (norm) {
-            ST <- apply(x > 0, 1, sum)
+            ST <- rowSums(x > 0)
             if (scales[a] == 1) result[, a] <- result[, a] / log(ST)
             else result[, a] <- result[, a] / ((ST^(1-scales[a]) - 1) / (1 - scales[a]))
         }
