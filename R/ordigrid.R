e95fc66e880b237b559f3822627585eecf161ce0
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-07-22 12:47:27 +0000

    ordigrid can use different lines for levels and replicates
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@1246 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/ordigrid.R
+++ R/ordigrid.R
@@ -1,11 +1,14 @@
 "ordigrid" <-
-    function (ord, levels, replicates, display = "sites", ...) 
+    function (ord, levels, replicates, display = "sites", lty=c(1,1), col=c(1,1),
+              lwd = c(1,1), ...) 
 {
     pts <- scores(ord, display = display, ...)
     npoints <- nrow(pts)
     gr <- gl(levels, replicates, npoints)
-    ordisegments(pts, groups = gr, ...)
+    ordisegments(pts, groups = gr,  lty = lty[1], col = col[1],
+                 lwd = lwd[1], ...)
     gr <- gl(replicates, 1, npoints)
-    ordisegments(pts, groups = gr, ...)
+    ordisegments(pts, groups = gr, lty = lty[2], col = col[2],
+                 lwd = lwd[2], ...)
     invisible()
 }
