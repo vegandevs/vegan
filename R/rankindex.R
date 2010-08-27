812978188b25d28e7bc12226a5d95bd952e12a3d
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-08-27 16:46:12 +0000

    rankindex accepts list of functions as indices arg
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@1288 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/rankindex.R
+++ R/rankindex.R
@@ -12,15 +12,39 @@ function (grad, veg, indices = c("euc", "man", "gow", "bra",
     }
     veg <- as.matrix(veg)
     res <- numeric(length(indices))
-    names(res) <- indices
-    for (i in indices) {
-        y <- vegdist(veg, i)
-        if (stepacross) {
-            is.na(y) <- no.shared(veg)
-            y <- stepacross(y, trace = FALSE, toolong = -1, ...)
+    ## create names if indices is a list of functions without names
+    if (is.list(indices)) {
+        nam <- names(indices)
+        if (is.null(nam))
+            nam <- paste("dis", 1:length(idices), sep="")
+    } else nam <- indices
+    names(res) <- nam
+    ## indices is a list of functions which return dist objects
+    if (is.list(indices)) {
+        for (i in 1:length(indices)) {
+            ## don't accept similarities
+            if (indices[[i]](matrix(1, 2, 2)) != 0)
+                stop("define dissimilarity and not similarity")
+            y <- indices[[i]](veg)
+            ## check class of output
+            if (!inherits(y, "dist"))
+                stop("function in 'indices' must return a 'dist' object")
+            if (stepacross) {
+                is.na(y) <- no.shared(veg)
+                y <- stepacross(y, trace = FALSE, toolong = -1, ...)
+            }
+            res[i] <- cor(span, y, method = method)
         }
-        res[i] <- cor(span, y, method = method)
+    ## indices is a character vector naming methods in vegdist
+    } else {
+        for (i in indices) {
+            y <- vegdist(veg, i)
+            if (stepacross) {
+                is.na(y) <- no.shared(veg)
+                y <- stepacross(y, trace = FALSE, toolong = -1, ...)
+            }
+            res[i] <- cor(span, y, method = method)
+        }
     }
     res
 }
-
