a75f62fa864a2fc8b6088b0309496a698a578f60
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-09-17 09:40:41 +0000

    ordiR2step gained 'direction' argument with 'both' & 'forward'
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@1306 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/ordiR2step.R
+++ R/ordiR2step.R
@@ -3,9 +3,11 @@
 ### Blanchet, Legendre & Borcard: Ecology 89, 2623--2623; 2008.
 
 `ordiR2step` <-
-    function(object, scope, Pin = 0.05, pstep = 100,
-             perm.max = 1000, trace = TRUE, ...)
+    function(object, scope, direction = c("both", "forward"),
+             Pin = 0.05, pstep = 100, perm.max = 1000,
+             trace = TRUE, ...)
 {
+    direction <- match.arg(direction)
     if (missing(scope))
         stop("needs scope")
     ## Works only for rda(): cca() does not have (yet) R2.adjusted
@@ -33,25 +35,34 @@
     ## Step forward and continue as long as R2.adj improves and R2.adj
     ## remains below R2.adj < R2.all
     R2.previous <- R2.0
+    drops <- NULL
     repeat {
         if (trace) {
             cat("Step: R2.adj=", R2.previous, "\n")
             cat(pasteCall(formula(object)), "\n")
         }
         adds <- add.scope(object, scope)
-        ## Nothing to add, and we're done: break
-        if (length(adds) == 0)
+        if (direction == "both")
+            drops <- drop.scope(object)
+        ## Nothing to add or drop, and we're done: break
+        if (length(adds) == 0 && length(drops) == 0)
             break
-        R2.adds <- numeric(length(adds))
-        names(R2.adds) <- adds
+        R2.adds <- numeric(length(adds) + length(drops))
+        if (length(adds))
+            adds <- paste("+", adds)
+        if (length(drops))
+            drops <- paste("-", drops)
+        names(R2.adds) <- c(adds, drops) 
         ## Loop over add scope
-        for (trm in seq_along(adds)) {
-            fla <- paste("~  . + ", adds[trm])
-            R2.adds[trm] <- RsquareAdj(update(object, fla))$adj.r.squared
+        for (trm in seq_along(R2.adds)) {
+            fla <- paste(". ~ .", names(R2.adds[trm]))
+            R2.tmp <- RsquareAdj(update(object, fla))$adj.r.squared
+            if (!length(R2.tmp))
+                R2.tmp <- 0
+            R2.adds[trm] <- R2.tmp
         }
         best <- which.max(R2.adds)
         if (trace) {
-            names(R2.adds) <- paste("+", names(R2.adds))
             out <- sort(c("<All variables>" = R2.all, "<none>" = R2.previous,
                           R2.adds), decreasing = TRUE)
             out <- as.matrix(out)
