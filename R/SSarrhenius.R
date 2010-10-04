5afc82b0a3ba654cbbc3275a9db365ebf2f04611
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-10-04 05:30:18 +0000

    forgot exp(k) in Arrhenius
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@1326 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/SSarrhenius.R
+++ R/SSarrhenius.R
@@ -4,6 +4,7 @@ SSarrhenius <-
 {
     xy <- sortedXyData(mCall[["area"]], LHS, data)
     value <- as.vector(coef(lm(log(xy[,"y"]) ~ log(xy[,"x"]))))
+    value[1] <- exp(value[1])
     names(value) <- mCall[c("k","z")]
     value
 },
