52a71bab50fc5e3a059100adb709ca418eb4b4da
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-10-18 08:54:50 +0000

    isomap returns only axes with positive eigenvalues
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@1336 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/isomap.R
+++ R/isomap.R
@@ -3,6 +3,14 @@ function(dist, ndim=10, ...)
 {
     dist <- isomapdist(dist, ...)
     out <- cmdscale(dist, k=ndim, eig=TRUE)
+    ## some versions of cmdscale may return NaN points corresponding
+    ## to negative eigenvalues
+    if (any(out$eig < 0)) {
+        out$eig <- out$eig[out$eig > 0]
+        out$points <- out$points[, seq_along(out$eig), drop = FALSE]
+        warning(gettextf("isomap returns only %d axes with positive eigenvalues",
+                         length(out$eig)))
+    }
     npoints <- nrow(out$points)
     net <- matrix(FALSE, nrow=npoints, ncol=npoints)
     net[lower.tri(net)][attr(dist, "net")] <- TRUE 
