b6bc2d12721b67b1b8b95b9cbd0ea42efb1895bb
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-11-01 14:25:51 +0000

    nestednodf breaks ties of col/row frequencies by col/row sums: upgrade by Gustavo Carvalho
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@1341 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/nestednodf.R
+++ R/nestednodf.R
@@ -1,75 +1,71 @@
 `nestednodf` <- 
-    function(comm, order = TRUE, weighted = FALSE)
+    function(comm, order = TRUE, weighted = FALSE) 
 {
-    ## Keep it consistent with similar functions
-    if (!weighted) {
-        comm <- ifelse(comm > 0, 1, 0)
-    }
+    bin.comm <- ifelse(comm > 0, 1, 0)
+    rfill <- rowSums(bin.comm)
+    cfill <- colSums(bin.comm)
     if (order) {
-        comm <- comm[order(rowSums(comm), decreasing=TRUE),
-                     order(colSums(comm), decreasing=TRUE)]    
+        if (weighted) {
+            rgrad <- rowSums(comm)
+            cgrad <- colSums(comm)
+            rorder <- order(rfill, rgrad, decreasing = TRUE)
+            corder <- order(cfill, cgrad, decreasing = TRUE)
+        } else {
+            rorder <- order(rfill, decreasing = TRUE)
+            corder <- order(cfill, decreasing = TRUE)
+            comm <- bin.comm
+        }
+        comm <- comm[rorder, corder]
+        rfill <- rfill[rorder]
+        cfill <- cfill[corder]
     }
-    fill <- comm > 0
-    rfill <- rowSums(fill)
-    cfill <- colSums(fill)
     nr <- NROW(comm)
     nc <- NCOL(comm)
     fill <- sum(rfill)/length(comm)
-    ## Initializing the vectors that will hold nodf values
-    ## for each combination of rows and columns
-    N.paired.rows <- numeric(nr * (nr - 1) / 2)
-    N.paired.cols <- numeric(nc * (nc - 1) / 2)
+    N.paired.rows <- numeric(nr * (nr - 1)/2)
+    N.paired.cols <- numeric(nc * (nc - 1)/2)
     counter <- 0
-    ## Nested loops to get every combination of rows/columns
-    ## Row i
-    for (i in 1:(nr-1)) {
+    for (i in 1:(nr - 1)) {
         first <- comm[i, ]
-        ## Row j
-        for(j in (i + 1):nr) {
-            ## Counting the number of nodfs calculated so far
+        for (j in (i + 1):nr) {
             counter <- counter + 1
-            if (rfill[i] <= rfill[j] || any(rfill[c(i, j)] == 0))
+            if (rfill[i] <= rfill[j] || any(rfill[c(i, j)] == 0)) 
                 next
             if (weighted) {
                 second <- comm[j, ]
-                N.paired.rows[counter] <- sum(first - second > 0 & second > 0) /
-                    sum(second > 0)
+                N.paired.rows[counter] <-
+                    sum(first - second > 0 & second > 0)/sum(second > 0)
             }
             else {
-                N.paired.rows[counter] <- sum(first + comm[j, ] == 2) /
-                    rfill[j]
+                N.paired.rows[counter] <-
+                    sum(first + comm[j, ] == 2)/rfill[j]
             }
         }
     }
-    ## Reseting the counter
     counter <- 0
-    ## Column i
-    for (i in 1:(nc-1)) {
+    for (i in 1:(nc - 1)) {
         first <- comm[, i]
-        ## Column j
-        for(j in (i + 1):nc) {
+        for (j in (i + 1):nc) {
             counter <- counter + 1
-            if (cfill[i] <= cfill[j] || any(cfill[c(i, j)] == 0))
+            if (cfill[i] <= cfill[j] || any(cfill[c(i, j)] == 0)) 
                 next
             if (weighted) {
                 second <- comm[, j]
-                N.paired.cols[counter] <- sum(first - second > 0 & second > 0) /
-                    sum(second > 0)
+                N.paired.cols[counter] <-
+                    sum(first - second > 0 & second > 0)/sum(second > 0)
             }
             else {
-                N.paired.cols[counter] <- sum(first + comm[, j] == 2) /
-                    cfill[j]
+                N.paired.cols[counter] <-
+                    sum(first + comm[, j] == 2)/cfill[j]
             }
         }
     }
-    ## Calculating the means and the NODF for the whole matrix
     N.columns <- mean(N.paired.cols) * 100
     N.rows <- mean(N.paired.rows) * 100
     NODF <- (sum(c(N.paired.rows, N.paired.cols)) * 100)/
         ((nc * (nc - 1)/2) + (nr * (nr - 1)/2))
-    out <- list(comm = comm, fill = fill, 
-                statistic=c("N.columns" = N.columns, "N.rows" = N.rows,
-                "NODF" = NODF))
+    out <- list(comm = comm, fill = fill,
+                statistic = c(N.columns = N.columns, N.rows = N.rows, NODF = NODF))
     class(out) <- "nestednodf"
     return(out)
 }
