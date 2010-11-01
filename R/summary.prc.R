8b77200712b81b0fb9998e79493be2554f2974ba
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-11-01 13:20:43 +0000

    prc folded coefficients wrongly: reported & fixed by Cajo ter Braak
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@1340 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/summary.prc.R
+++ R/summary.prc.R
@@ -1,5 +1,5 @@
-"summary.prc" <-
-    function (object, axis = 1, scaling = 2, digits = 4, ...) 
+`summary.prc` <-
+    function (object, axis = 1, scaling = 2, digits = 4, ...)
 {
     species <- drop(scores(object, scaling = scaling, display="sp", choices=axis))
     b <- coef(object)[, axis]
@@ -8,12 +8,12 @@
     lenb <- length(b)
     b <- b[-(1:(2 * prnk))]
     bx <- b[1:(lentreat - 1)]
-    by <- b[lentreat:length(b)]
+    by <- b[lentreat:length(b)] + rep(bx, each = length(object$terminfo$xlev[[1]])-1)
     b <- cbind(bx, matrix(by, nrow = lentreat - 1, byrow = TRUE))
     rownames(b) <- (object$terminfo$xlev[[2]])[-1]
     colnames(b) <- object$terminfo$xlev[[1]]
-    out <- list(sp = species, coefficients = b, names = names(object$terminfo$xlev), 
-                corner = (object$terminfo$xlev[[2]])[1], call = object$call, 
+    out <- list(sp = species, coefficients = b, names = names(object$terminfo$xlev),
+                corner = (object$terminfo$xlev[[2]])[1], call = object$call,
                 digits = digits)
     class(out) <- "summary.prc"
     out
