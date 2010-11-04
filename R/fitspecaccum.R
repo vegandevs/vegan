76c905ee0fcf654d3a79653a8f575a6ab442bc6e
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-11-04 16:51:05 +0000

    fitspecaccum knows more nls() models
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@1350 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/fitspecaccum.R
+++ R/fitspecaccum.R
@@ -1,8 +1,10 @@
 fitspecaccum <-
-    function(object, model = c("michaelis-menten", "arrhenius"),
-             method = "random",  ...)
+    function(object, model, method = "random",  ...)
 {
-    model <- match.arg(model)
+    MODELS <- c("arrhenius", "gitay", "lomolino", "asymp",
+                "gompertz", "michaelis-menten", "logis",
+                "weibull")
+    model <- match.arg(model, MODELS)
     if (!inherits(object, "specaccum")) 
         object <- specaccum(object, method = method, ...)
     if (is.null(object$perm))
@@ -14,10 +16,23 @@ fitspecaccum <-
     else
         x <- object$sites
     mods <- switch(model,
+        "arrhenius" = apply(SpeciesRichness, 2,
+             function(y) nls(y ~ SSarrhenius(x, k, z))),
+        "gitay" = apply(SpeciesRichness, 2,
+             function(y) nls(y ~ SSgitay(x, k, slope))),
+        "lomolino" = apply(SpeciesRichness, 2,
+             function(y) nls(y ~ SSlomolino(x, Asym, xmid, slope))),
+        "asymp" = apply(SpeciesRichness, 2,
+             function(y) nls(y ~ SSlogis(x, Asym, xmid, scal))),
+        "gompertz" = apply(SpeciesRichness, 2,
+             function(y) nls(y ~ SSgompertz(x, Asym, xmid, scal))),
         "michaelis-menten" = apply(SpeciesRichness, 2,
              function(y) nls(y ~ SSmicmen(x, Vm, K))),
-        "arrhenius" = apply(SpeciesRichness, 2,
-                   function(y) nls(y ~ SSarrhenius(x, k, z))))
+        "logis" = apply(SpeciesRichness, 2,
+             function(y) nls(y ~ SSlogis(x, Asym, xmid, scal))),
+        "weibull" = apply(SpeciesRichness, 2,
+             function(y) nls(y ~ SSweibull(x, Asym, Drop, lrc, par)))
+                   )
     object$fitted <- drop(sapply(mods, fitted))
     object$residuals <- drop(sapply(mods, residuals))
     object$coefficients <- drop(sapply(mods, coef))
@@ -25,4 +40,16 @@ fitspecaccum <-
     object$call <- match.call()
     class(object) <- c("fitspecaccum", class(object))
     object
+}
+
+### plot function
+
+`plot.fitspecaccum` <-
+    function(x, col = par("fg"), lty = 1, 
+             xlab = "Sites", ylab = x$method, ...)
+{
+    fv <- fitted(x)
+    matplot(x$sites, fv, col = col, lty = lty, pch = NA,
+            xlab = xlab, ylab = ylab, type = "l", ...)
+    invisible()
 }
