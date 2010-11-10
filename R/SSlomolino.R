4291d082271fe2b7fb6942e2f9420921b2fb09c4
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-11-10 14:03:06 +0000

    Lomolino tweaks
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@1361 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/SSlomolino.R
+++ R/SSlomolino.R
@@ -11,14 +11,11 @@ SSlomolino <-
     .S <- exp(.p[1] + log(max(xy[["x"]])) * (.p[2]))
     .S <- max(.S, .Smax)
     .xmid <- exp((log(.S/2) - .p[1])/.p[2])
-    ## approximate slope using y = log(Smax/S - 1)
+    ## approximate slope for log(Asym/y - 1) ~ log(xmid/x) + 0
     .y <- log(.S/xy[["y"]] - 1)
-    .x <- xy[["x"]]
     .z <- log(.xmid/xy[["x"]])
-    .b <- coef(lm(.y ~ .z))
-    ## Adjust Asym: half of y = Asym/2 at xmid
-    ##.S <- .S * exp(-0.5 * (.b[1]))
-    value <- c(.S, .xmid, exp(.b[2]))
+    .b <- coef(lm(.y ~ .z - 1))
+    value <- c(.S, .xmid, exp(.b))
     names(value) <- mCall[c("Asym","xmid", "slope")]
     value
 },
