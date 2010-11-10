1352b2d5c3995bc56348a166d43e4eedbbabe549
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-11-10 10:18:55 +0000

    improved starting values for Lomolino model: now often works
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@1360 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/SSlomolino.R
+++ R/SSlomolino.R
@@ -3,20 +3,21 @@ SSlomolino <-
               function(mCall, data, LHS)
 {
     xy <- sortedXyData(mCall[["area"]], LHS, data)
-    ## assume Asym is const*max(S)
-    .S <- max(xy[["y"]])*1.5
-    ## approximate using y = log(Smax/S - 1)
+    ## approximate with Arrhenius model on log-log
+    .p <- coef(lm(log(xy[["y"]]) ~ log(xy[["x"]])))
+    ## Asym is value at max(x) but > max(y) and xmid is x which gives
+    ## Asym/2
+    .Smax <- max(xy[["y"]])*1.1
+    .S <- exp(.p[1] + log(max(xy[["x"]])) * (.p[2]))
+    .S <- max(.S, .Smax)
+    .xmid <- exp((log(.S/2) - .p[1])/.p[2])
+    ## approximate slope using y = log(Smax/S - 1)
     .y <- log(.S/xy[["y"]] - 1)
     .x <- xy[["x"]]
-    ## xmid is the inflection point: fit a parabola to log and find it
-    ## there
-    .p <- coef(lm(.y ~ .x + I(.x^2)))
-    .xmid <- -(.p[2])/2/.p[3] - sqrt(abs(1/2/.p[3]))
-     ## estimate slope assuming Asym and xmid are known
     .z <- log(.xmid/xy[["x"]])
     .b <- coef(lm(.y ~ .z))
     ## Adjust Asym: half of y = Asym/2 at xmid
-    .S <- .S * exp(-0.5 * (.b[1]))
+    ##.S <- .S * exp(-0.5 * (.b[1]))
     value <- c(.S, .xmid, exp(.b[2]))
     names(value) <- mCall[c("Asym","xmid", "slope")]
     value
