eabf57518f387d3decea8fceb7ab8364058182a8
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-11-17 19:43:15 +0000

    aliased terms must be dropped from VIF
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@1375 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/vif.cca.R
+++ R/vif.cca.R
@@ -1,13 +1,16 @@
-"vif.cca" <-
-function (object) 
+`vif.cca` <-
+    function(object) 
 {
     Q <- object$CCA$QR
-    V <- chol2inv(Q$qr)
-    X <- qr.X(Q)
+    out <- rep(NA, NCOL(Q$qr))
+    names(out)[Q$pivot] <- colnames(Q$qr)
+    rank <- Q$rank
+    V <- chol2inv(Q$qr, size = rank)
+    X <- qr.X(Q)[, 1:rank,drop=FALSE]
     Vi <- crossprod(X)
-    nam <- colnames(Vi)
     v1 <- diag(V)
     v2 <- diag(Vi)
-    structure(v1 * v2, names = nam)
+    out[Q$pivot[1:rank]] <- v1 * v2
+    out
 }
 
