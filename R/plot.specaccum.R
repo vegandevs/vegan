b9c587cfb06e0bb96604d6e1b288f53a76c37e8e
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-01-19 08:09:17 +0000

    gained option to plot rarefied specaccum against individuals
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@1466 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/plot.specaccum.R
+++ R/plot.specaccum.R
@@ -1,25 +1,30 @@
-"plot.specaccum" <-
+`plot.specaccum` <-
     function(x, add = FALSE, ci = 2, ci.type = c("bar","line","polygon"), 
-             col = par("fg"), ci.col = col, ci.lty = 1, xlab = "Sites",
-             ylab = x$method, ylim, ...)
+             col = par("fg"), ci.col = col, ci.lty = 1, xlab,
+             ylab = x$method, ylim, xvar = c("sites", "individuals"), ...)
 {
+    xvar <- match.arg(xvar)
+    xaxvar <- x[[xvar]]
+    if (missing(xlab))
+        xlab <- paste(toupper(substring(xvar, 1, 1)),
+                              substring(xvar, 2), sep="")
     ci.type <- match.arg(ci.type)
     if (!add) {
         if (missing(ylim))
-            ylim<- c(1, max(x$richness, x$richness + ci*x$sd))
-        plot(x$sites, x$richness, xlab=xlab, ylab=ylab, ylim=ylim,
+            ylim <- c(1, max(x$richness, x$richness + ci*x$sd))
+        plot(xaxvar, x$richness, xlab=xlab, ylab=ylab, ylim=ylim,
              type="n", ...)
     }
     if (!is.null(x$sd) && ci)
         switch(ci.type,
-               bar = segments(x$sites, x$richness - ci*x$sd, x$sites,
+               bar = segments(xaxvar, x$richness - ci*x$sd, xaxvar,
                   x$richness + ci*x$sd, col=ci.col, lty=ci.lty, ...),
-               line = matlines(x$sites, x$richness + t(rbind(-ci,ci) %*% x$sd),
+               line = matlines(xaxvar, x$richness + t(rbind(-ci,ci) %*% x$sd),
                  col=ci.col, lty=ci.lty, ...),
-               polygon = polygon(c(x$sites, rev(x$sites)),
+               polygon = polygon(c(xaxvar, rev(xaxvar)),
                  c(x$richness - ci*x$sd, rev(x$richness + ci*x$sd)), col=ci.col,
                  lty=ci.lty,  ...)
                )
-    lines(x$sites, x$richness,col=col, ...)
+    lines(xaxvar, x$richness,col=col, ...)
     invisible()
 }
