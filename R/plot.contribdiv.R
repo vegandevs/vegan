2745a285819aff685d802f45b6b8b9278272f91c
gsimpson <gsimpson@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-03-30 13:45:24 +0000

    tweak to plot.contribdiv in how the axes and box are drawn
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@1559 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/plot.contribdiv.R
+++ R/plot.contribdiv.R
@@ -11,8 +11,12 @@ function(x, sub, xlab, ylab, ylim, col, ...) {
         ylim <- c(0, max(y))
     if (missing(col))
         col <- c("lightgrey", "darkgrey")
-    matplot(y, type = "n", sub=sub,xlab=xlab,ylab=ylab,...)
+    matplot(y, type = "n", sub=sub, xlab=xlab, ylab=ylab, axes = FALSE,
+            bty = "n", ...)
     polygon(c(1,1:nrow(y),nrow(y)), c(0,y$gamma,0), col=col[1])
     polygon(c(1,1:nrow(y),nrow(y)), c(0,y$alpha,0), col=col[2])
+    axis(side = 1)
+    axis(side = 2)
+    box()
     invisible(x)
 }
