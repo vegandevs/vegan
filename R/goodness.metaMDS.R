1fa0e2a33efc8caebc037400263711eb29d2a590
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-04-09 08:08:07 +0000

    implement goodness.monoMDS()
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@1581 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/goodness.metaMDS.R
+++ R/goodness.metaMDS.R
@@ -1,6 +1,8 @@
-"goodness.metaMDS" <-
+`goodness.metaMDS` <-
     function(object, dis, ...)
 {
+    if (inherits(object, "monoMDS"))
+        return(NextMethod("goodness", object, ...))
     require(MASS) || stop("Needs MASS package")
     if (missing(dis))
         dis <- metaMDSredist(object)
@@ -19,3 +21,43 @@
     sqrt(colSums(as.matrix(res))/2*10000)
 }
 
+`goodness.monoMDS` <-
+    function(object, ...)
+{
+    ## Return vector 'x' for which sum(x^2) == stress
+    stresscomp <- function(y, yf, form)
+    {
+        num <- (y-yf)^2
+        if (form == 1)
+            den <- sum(y^2)
+        else
+            den <- sum((y-mean(y))^2)
+        num/den
+    }
+    ## Global, local
+    if (object$model %in% c("global", "linear")) {
+        x <- stresscomp(object$dist, object$dhat, object$isform)
+        mat <- matrix(0, object$nobj, object$nobj)
+        for (i in 1:object$ndis)
+            mat[object$iidx[i], object$jidx[i]] <- x[i]
+        res <- sqrt(colSums(mat + t(mat))/2)
+    }
+    ## Local: returns pointwise components of stress
+    else if (object$model == "local") {
+        res <- object$grstress/sqrt(object$ngrp)
+    } else if (object$model == "hybrid" && object$ngrp == 2) {
+        mat <- matrix(0, object$nobj, object$nobj)
+        gr <- seq_len(object$ndis) < object$istart[2]
+        x <- stresscomp(object$dist[gr], object$dhat[gr], object$isform)
+        x <- c(x, stresscomp(object$dist[!gr], object$dhat[!gr], object$isform))
+        i <- object$iidx
+        j <- object$jidx
+        for (k in 1:object$ndis) {
+            mat[i[k], j[k]] <- mat[i[k], j[k]] + x[k]
+        }
+        res <- sqrt(colSums(mat + t(mat))/4)
+    } else {
+        stop("unknown 'monoMDS' model")
+    }
+    res
+}
