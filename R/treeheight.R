535dda45f95e3ae774946d254ddd134a2bd94a62
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-05-12 14:38:42 +0000

    dramatic simplification of treedist
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@1601 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/treeheight.R
+++ R/treeheight.R
@@ -4,17 +4,6 @@
     if (inherits(tree, "spantree"))
         return(sum(tree$dist))
     tree <- as.hclust(tree)
-    m <- tree$merge
-    h <- tree$height
-    height <- 0
-    for (i in 1:nrow(m)) {
-        for (j in 1:2) {
-            if (m[i,j] < 0)
-                height <- height + h[i]
-            else
-                height <- height + h[i] - h[m[i,j]] 
-        }
-    }
-    height
+    sum(tree$height) + max(tree$height)
 }
 
