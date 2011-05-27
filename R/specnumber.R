b26ee8f14895e3282b89839c45b2b96b7059d722
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-05-27 08:21:20 +0000

    specnumber gained argument 'groups' for easier estimation of Whittaker's beta = gamma/alpha - 1
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@1609 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/specnumber.R
+++ R/specnumber.R
@@ -1,6 +1,13 @@
 `specnumber` <-
-    function(x, MARGIN = 1)
+    function(x, groups, MARGIN = 1)
 {
+    if (!missing(groups)) {
+        if (length(groups) == 1)
+            groups <- rep(groups, nrow(x))
+        x <- aggregate(x, list(groups), max)
+        rownames(x) <- x[,1]
+        x <- x[,-1]
+    }
     if (length(dim(x)) > 1)
         apply(x > 0, MARGIN, sum)
     else
