a09a3a645db5aa9370a8d275df3863d4a1e06b98
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-13 18:24:30 +0000

    anova.prc() uses original model data and formula instead of interal names and structures, and passes rda() arguments
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@1630 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/anova.prc.R
+++ R/anova.prc.R
@@ -1,12 +1,24 @@
 `anova.prc` <-
     function(object, ...)
 {
-    ## Refit prc() as an rda() for anova.cca()
-    Z <- qr.X(object$pCCA$QR)
-    X <- qr.X(object$CCA$QR)
-    ## Get the name of the original response variable
-    fla <- as.character(object$call$response)
-    fla <- as.formula(paste(fla, "~ X + Condition(Z)"))
-    mod <- rda(fla)
-    anova(mod, ...)
+    ## if user specified 'by', cast prc() to an rda() and call anova
+    ## on its result
+    extras <- match.call(expand.dots = FALSE)
+    if ("by" %in% names(extras$...)) {
+        Y <- as.character(object$call$response)
+        X <- as.character(object$call$treatment)
+        Z <- as.character(object$call$time)
+        fla <- paste(Y, "~", X, "*", Z, "+ Condition(", Z, ")")
+        fla <- as.formula(fla)
+        ## get extras
+        m <- match(c("data", "scale", "subset", "na.action"),
+                   names(object$call), 0)
+        call <- object$call[c(1,m)]
+        call$formula <- fla
+        call[[1]] <- as.name("rda.formula")
+        object <- eval(call, parent.frame())
+        anova(object, ...)
+    } else {
+        NextMethod("anova", object, ...)
+    }    
 }
