1b816c19b21d0f0ebe75ec17c4aa3e4480560d2d
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-08-02 06:13:57 +0000

    an element of flexibility and cleaner handling of dots in make.cepnames
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@1690 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/make.cepnames.R
+++ R/make.cepnames.R
@@ -1,13 +1,20 @@
-"make.cepnames" <-
-    function (names) 
+`make.cepnames` <-
+    function (names, seconditem = FALSE) 
 {
+    ## make valid names
     names <- make.names(names, unique = FALSE)
+    ## remove trailing and duplicated dots
+    names <- gsub("\\.[\\.]+", ".", names)
+    names <- gsub("\\.$", "", names)
+    ## split by dots and take 4 letters of each element (if several)
     names <- lapply(strsplit(names, "\\."), function(x) if (length(x) > 1) 
                     substring(x, 1, 4) else x )
+    ## Take first and last element or 8 characters if only one element
     names <- unlist(lapply(names, function(x) if (length(x) > 1)
-                           paste(x[c(1, length(x))], collapse = "")
+                           paste(x[c(1, if(seconditem) 2 else length(x))], collapse = "")
                            else x))
     names <- abbreviate(names, 8)
+    ## Final clean-up
     names <- make.names(names, unique = TRUE)
     names
 }
