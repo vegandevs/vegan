ff52082889e4b47073db545336dd5f1d7e665ff1
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-08-08 18:22:13 +0000

    NAMESPACE with maximal hiding of information of exports
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@1700 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/model.frame.cca.R
+++ R/model.frame.cca.R
@@ -8,7 +8,7 @@
         0)
     call <- call[c(1, m)]
     call[[1]] <- as.name("ordiParseFormula")
-    out <- eval(call, parent.frame())
+    out <- eval(call, environment(), parent.frame())
     mf <- out$modelframe
     attr(mf, "terms") <- out$terms.expand
     if (!is.null(out$na.action)) 
--- R/model.matrix.cca.R
+++ R/model.matrix.cca.R
@@ -8,7 +8,7 @@
         0)
     call <- call[c(1, m)]
     call[[1]] <- as.name("ordiParseFormula")
-    out <- eval(call, parent.frame())[c("Z", "Y")]
+    out <- eval(call, environment(), parent.frame())[c("Z", "Y")]
     m <- list()
     if (!is.null(out$Z))
         m$Conditions <- out$Z
