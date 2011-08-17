f009be12469abe3af269ad483e7d5542f6b4aa8c
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-08-17 06:32:43 +0000

    plot text and points over the net so that they are visible
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@1721 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/plot.isomap.R
+++ R/plot.isomap.R
@@ -1,12 +1,19 @@
 `plot.isomap` <-
-function (x, net=TRUE, n.col = "gray", ...) 
+    function (x, net = TRUE, n.col = "gray", type = "points", ...) 
 {
-	pl <- ordiplot(x, display="sites", ...)
-	if (net) {
-		z <- scores(pl, "sites")
-		k <- x$net
-		segments(z[k[,1],1], z[k[,1],2], z[k[,2],1], z[k[,2],2], col=n.col)
-	}
-	invisible(pl)
+    type <- match.arg(type, c("points", "text", "none"))
+    if (!net) {
+        pl <- ordiplot(x, display="sites", type = type, ...)
+    } else {
+        pl <- ordiplot(x, display = "sites", type = "none", ...)
+        z <- scores(pl, "sites")
+        k <- x$net
+        segments(z[k[,1],1], z[k[,1],2], z[k[,2],1], z[k[,2],2], col=n.col)
+        if (type == "points")
+            points(pl, "sites", ...)
+        else if (type == "text")
+            ordilabel(pl, ...)
+    }
+    invisible(pl)
 }
 
