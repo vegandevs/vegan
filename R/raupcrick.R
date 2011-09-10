611c64e5b99c9a85fee9a1e1afb6f7543652e390
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-09-10 14:17:59 +0000

    implement Raup-Crick dissimilarity with sampling probabilities of species proportional to frequency (like Brian Inouye told us to do)
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@1835 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/raupcrick.R
@@ -0,0 +1,27 @@
+`raupcrick` <-
+    function(comm, null = "r1", nsimul = 999, chase = FALSE)
+{
+    comm <- as.matrix(comm)
+    comm <- ifelse(comm > 0, 1, 0)
+    ## 'tri' is a faster alternative to as.dist(): it takes the lower
+    ## diagonal, but does not set attributes of a "dist" object
+    N <- nrow(comm)
+    tri <- matrix(FALSE, N, N)
+    tri <- row(tri) > col(tri)
+    ## function(x) designdist(x, "J", terms="binary") does the same,
+    ## but is much slower
+    sol <- oecosimu(comm, function(x) tcrossprod(x)[tri], method = null,
+                    nsimul = nsimul,
+                    alternative = if (chase) "greater" else "less")
+    ## Chase et al. way, or the standard way
+    if (chase)
+        out <- 1 - sol$oecosimu$pval
+    else
+        out <- sol$oecosimu$pval
+    ## set attributes of a "dist" object
+    attributes(out) <- list("class"=c("raupcrick", "dist"), "Size"=N,
+                            "Labels" = rownames(comm), "call" =
+                            match.call(), "Diag" = FALSE, "Upper" = FALSE,
+                            "method" = "raupcrick")
+    out
+}
