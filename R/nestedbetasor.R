62e7cb62317c72f7907f7e8d70913e3d1b1bac50
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-11 18:30:06 +0000

    implement decomposition of multiple Jaccard & Sorensen into turnover and nestedness components following Baselga (Global Ecol Biogeog 19, 134-143; 2010)
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@2018 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/nestedbetasor.R
@@ -0,0 +1,31 @@
+`nestedbetasor` <-
+    function(comm)
+{
+    beta <- betadiver(comm, method = NA)
+    b <- beta$b
+    c <- beta$c
+    diffbc <- sum(abs(b-c))
+    sumbc <- sum(b+c)
+    bmin <- sum(pmin(b, c))
+    a <- sum(comm) - sum(colSums(comm) > 0)
+    simpson <- bmin/(bmin + a)
+    nest <- a/(bmin + a) * diffbc/(2*a + sumbc)
+    sorensen <- sumbc/(2*a + sumbc)
+    c(turnover = simpson, nestedness = nest, sorensen = sorensen)
+}
+
+`nestedbetajac` <-
+    function(comm)
+{
+    beta <- betadiver(comm, method = NA)
+    b <- beta$b
+    c <- beta$c
+    diffbc <- sum(abs(b-c))
+    sumbc <- sum(b+c)
+    bmin <- sum(pmin(b, c))
+    a <- sum(comm) - sum(colSums(comm) > 0)
+    simpson <- 2*bmin/(2*bmin + a)
+    nest <- a/(2*bmin + a) * diffbc/(a + sumbc)
+    jaccard <- sumbc/(a + sumbc)
+    c(turnover = simpson, nestedness = nest, jaccard = jaccard)
+}
