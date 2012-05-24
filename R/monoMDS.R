ebe372d197ac5df18e91963a79bed83d4e7b6b60
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-05-24 13:25:59 +0000

    Warn about "too good stress" only in metaMDS
    Assume that users of monoMDS know what they are doing
    and do not warn them. Also no permanent print messages,
    but only a run-time warning. 
    
    Reverts commits r2150, 2172, 2179, 2181.
    
    
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@2184 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/monoMDS.R
+++ R/monoMDS.R
@@ -91,9 +91,6 @@
     }
     ## ndis: number of >0 dissimilarities (distinct points)
     ndis <- length(dist)
-    if (ndis <= k * nobj)
-        warning(gettextf("Not enough data: you have %d dissimilarities >0,\n  and you ask %d scores (%d dimensions times %d points)",
-                         ndis, k * nobj, k, nobj))
     ## starting configuration
     if (missing(y)) {
         y <- matrix(runif(nobj*k, -1, 1), nobj, k)
