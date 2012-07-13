06673480208e690cee096e2e97eba74eb5a92bb4
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-07-13 18:37:52 +0000

    new internal for parsing formula in adipart+allies
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@2232 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/hierParseFormula.R
@@ -0,0 +1,20 @@
+"hierParseFormula" <-
+function (formula, data)
+{
+    lhs <- formula[[2]]
+    if (any(attr(terms(formula, data = data), "order") > 1)) 
+        stop("interactions are not allowed")
+    lhs <- as.matrix(eval(lhs, data))
+    formula[[2]] <- NULL
+    rhs <- model.frame(formula, data, drop.unused.levels = TRUE)
+    rhs[] <- lapply(rhs, function(u) {
+        if (!is.factor(u)) 
+            u <- factor(u)
+        u
+    })
+    if (length(rhs) < 2)
+        stop("at least 2 hierarchy levels are needed")
+    attr(rhs, "terms") <- NULL
+    list(lhs=lhs, rhs=rhs)
+}
+
