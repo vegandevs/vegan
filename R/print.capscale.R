b57f7b061cd383b3b2ba7b26148b3b5f85a70c18
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-01-10 07:39:26 +0000

    capscale prints the value of additive constant with 'add = TRUE'
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@2357 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/print.capscale.R
+++ R/print.capscale.R
@@ -3,5 +3,7 @@
 {
     NextMethod("print", x, ...)
     if (!is.null(x$metaMDSdist))
-        cat("metaMDSdist transformed data:", x$metaMDSdist, "\n\n") 
+        cat("metaMDSdist transformed data:", x$metaMDSdist, "\n\n")
+    if (!is.null(x$ac))
+        cat("Constant added to distances:", x$ac, "\n\n")
 }
