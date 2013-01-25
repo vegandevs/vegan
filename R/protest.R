3efb36431e4327ae317ff8dc15f168bbd33f1848
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-01-25 12:51:10 +0000

    huge speed-up in protest
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@2388 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/protest.R
+++ R/protest.R
@@ -3,9 +3,27 @@
 {
     X <- scores(X, display = scores, ...)
     Y <- scores(Y, display = scores, ...)
-    sol <- procrustes(X, Y, symmetric = TRUE)
+    ## Centre and normalize X & Y here so that the permutations will
+    ## be faster
+    X <- scale(X, scale = FALSE)
+    Y <- scale(Y, scale = FALSE)
+    X <- X/sqrt(sum(X^2))
+    Y <- Y/sqrt(sum(Y^2))
+    ## Transformed X and Y will yield symmetric procrustes() and we
+    ## need not specify that in the call (but we set it symmetric
+    ## after the call).
+    sol <- procrustes(X, Y, symmetric = FALSE)
+    sol$symmetric <- TRUE
     sol$t0 <- sqrt(1 - sol$ss)
     N <- nrow(X)
+
+    ## Permutations: We only need the goodness of fit statistic from
+    ## Procrustes analysis, and therefore we only have the necessary
+    ## function here. This avoids a lot of overhead of calling
+    ## procrustes() for each permutation. The following gives the
+    ## Procrustes r directly.
+    procr <- function(X, Y) sum(svd(crossprod(X, Y), nv=0, nu=0)$d)
+    
     if (length(permutations) == 1) {
         if (permutations > 0) {
             arg <- if (missing(strata)) NULL else strata
@@ -20,9 +38,7 @@
         permutations <- nrow(permutations)
     }
     perm <- sapply(1:permutations,
-                   function(i, ...) procrustes(X, Y[permat[i,],],
-                                               symmetric = TRUE)$ss)
-    perm <- sqrt(1 - perm)
+                   function(i, ...) procr(X, Y[permat[i,],]))
     Pval <- (sum(perm >= sol$t0) + 1)/(permutations + 1)
     if (!missing(strata)) {
         strata <- deparse(substitute(strata))
