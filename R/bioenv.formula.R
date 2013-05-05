76e0e0a77904c7785957d7b112af96dca696cac5
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-05-05 15:58:34 +0000

    implement Mahalanobis, Manhattan and Gower metrics for bioenv
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@2499 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/bioenv.formula.R
+++ R/bioenv.formula.R
@@ -7,12 +7,7 @@
     comm <- formula[[2]]
     comm <- eval(comm, data, parent.frame())
     formula[[2]] <- NULL
-    mf <- model.frame(formula, data, na.action = NULL)
-    if (any(sapply(mf, function(x) is.factor(x) || !is.numeric(x)))) 
-        stop("bioenv applies only to numeric variables")
-    env <- attr(mf, "terms")
-    attr(env, "intercept") <- 0
-    env <- model.matrix(env, mf)
+    env <- model.frame(formula, data, na.action = NULL)
     out <- bioenv(comm, env, ...)
     out$formula <- fla
     out$call <- match.call()
--- R/print.bioenv.R
+++ R/print.bioenv.R
@@ -1,11 +1,12 @@
-"print.bioenv" <-
+`print.bioenv` <-
     function (x, ...) 
 {
     cat("\nCall:\n")
     cat(deparse(x$call), "\n")
     cat("\nSubset of environmental variables with best correlation to community data.\n\n")
-    cat("Correlations:     ", x$method, "\n")
-    cat("Dissimilarities:  ", x$index, "\n\n")
+    cat("Correlations:   ", x$method, "\n")
+    cat("Dissimilarities:", x$index, "\n")
+    cat("Metric:         ", x$metric, "\n\n") 
     i <- which.max(lapply(x$models, function(tmp) tmp$est))
     cat("Best model has", i, "parameters (max.", x$upto, "allowed):\n")
     cat(paste(x$names[x$models[[i]]$best], collapse = " "))
