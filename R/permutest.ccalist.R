3454233259d51dee649c581552819a8dc4687402
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-10-14 07:40:57 +0000

    add permutest.ccalist() to analyse a sequence of cca models (experimental)
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@2633 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/permutest.ccalist.R
@@ -0,0 +1,74 @@
+`permutest.ccalist` <-
+    function(x, ..., permutations = 99)
+{
+    ## Collect cca class objects. FIXME: Eventually this should be in
+    ## a function that calls permutest.ccalist after collecting model
+    ## objects from dotargs.
+    dotargs <- list(...)
+    if (length(dotargs)) {
+        isCCA <- sapply(dotargs, function(z) inherits(z, "cca"))
+        dotargs <- dotargs[isCCA]
+        if (length(dotargs))
+            x <- c(list(x), dotargs)
+    }
+    nmodels <- length(x)
+    ## No. of observations and check
+    N <- sapply(x, nobs)
+    if (!all(N = N[1]))
+        stop("models have different numbers of observations")
+    else
+        N <- N[1]
+    ## Create permutation matrix if it does not exist. FIXME: should
+    ## take arguments for restricted permutation
+    if (length(permutations) == 1) 
+            permutations <- shuffleSet(N, permutations)
+    ## permutations is now matrix
+    nperm <- nrow(permutations)
+    ## check
+    if (ncol(permutations) != N)
+        stop(gettextf("permutation matrix has %d columns, but you have %d sites",
+                      ncol(nperm), N))
+    ## All models are evaluated in permutest.cca with identical
+    ## permutations so that the differences of single permutations can
+    ## be used to assess the significance of differences of fitted
+    ## models. This strictly requires nested models (not checked
+    ## here): all terms of the smaller model must be included in the
+    ## larger model. FIXME: should pass arguments to permutest.cca.
+    mods <- lapply(x, function(z)
+                   permutest(z, permutations = permutations))
+    dfs <- sapply(mods, function(z) z$df)
+    dev <- sapply(mods, function(z) z$chi)
+    resdf <- dfs[2,]
+    df <- -diff(resdf)
+    resdev <- dev[2,]
+    changedev <- -diff(resdev)
+    big <- which.min(resdf)
+    scale <- resdev[big]/resdf[big]
+    fval <- changedev/df/scale
+    ## Collect permutation results: denominator of F varies in each
+    ## permutation.
+    pscale <- mods[[big]]$den/resdf[big]
+    ## Numerator of F
+    pfvals <- sapply(mods, function(z) z$num)
+    pfvals <- apply(pfvals, 1, diff)
+    ## dropped to vector?
+    if (!is.matrix(pfvals))
+        pfvals <- matrix(pfvals, nrow=1, ncol=nperm)
+    pval <- rowSums(sweep(pfvals, 1, fval, ">="))
+    pval <- (pval + 1)/(nperm+1)
+    pfvals <- sweep(pfvals, 1, df, "/")
+    pfvals <- sweep(pfvals, 2, pscale, "/")
+    ## collect table
+    table <- data.frame(resdf, resdev, c(NA, df),
+                        c(NA,changedev), c(NA,fval), c(NA,pval))
+    dimnames(table) <- list(1L:nmodels, c("Resid. Df", "Res. Chisq", 
+                                          "Df", "Chisq", "F", "Pr(>F)"))
+    ## Collect header information
+    formulae <- sapply(x, function(z) deparse(formula(z)))
+    head <- paste0("Permutation tests for ", x[[1]]$method, " under ",
+                  mods[[1]]$model, " model\nwith ", nperm,
+                   " permutations\n")
+    topnote <- paste("Model ", format(1L:nmodels), ": ", formulae,
+                     sep = "", collapse = "\n")
+    structure(table, heading=c(head,topnote), class = c("anova", "data.frame"))
+}
