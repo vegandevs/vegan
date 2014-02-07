6ff37742bf6ecefdfe7c3b1bad0a0077e3026a3d
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-02-07 09:33:46 +0000

    use hclustMergeOrder in as.hclust.spantree
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@2852 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/as.hclust.spantree.R
+++ R/as.hclust.spantree.R
@@ -23,36 +23,13 @@
     labs <- -seq_len(npoints)
     merge <- matrix(0, nrow=npoints-1, ncol=2)
     for(i in 1:nrow(merge)) {
-        ## add items of labs, keep tighter cluster on the left
         merge[i, ] <- c(labs[dad[i]], labs[kid[i]])
         ## update labs for the current group and its kids
         labs[labs %in% labs[c(dad[i], kid[i])]] <- i
     }
-    ## Get order of leaves with recursive search from the root.
-    visited <- matrix(FALSE, nrow = nrow(merge), ncol=ncol(merge))
-    order <- numeric(npoints)
-    ind <- 0
-    ## "<<-" updates data only within this function, but outside the
-    ## visit() function.
-    visit <- function(i, j) {
-        if(visited[i,j])
-            return(NULL)
-        else {
-            visited[i,j] <<- TRUE
-        }
-        if (merge[i,j] < 0) {
-            ind <<- ind+1
-            order[ind] <<- -merge[i,j]
-            if (j == 1)
-                visit(i, 2)
-        } else {
-            visit(merge[i,j], 1)
-            visit(merge[i,j], 2)
-        }
-    }
-    visit(nrow(merge), 1)
-    visit(nrow(merge), 2)
-    
+
+    order <- hclustMergeOrder(merge)
+      
     out <- list(merge = merge, height = x$dist[o], order = order,
                 labels = x$labels, method = "spantree", call =
                 match.call())
