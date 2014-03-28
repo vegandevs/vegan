09f9341aa01b77886dd7e31df14e3bfeab670fcf
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-03-28 10:04:53 +0000

    radfit plots work for sites with one or no species
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@2866 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/fitted.radfit.R
+++ R/fitted.radfit.R
@@ -1,7 +1,13 @@
 `fitted.radfit` <-
     function(object, ...)
 {
-    sapply(object$models, fitted)
+    out <- sapply(object$models, fitted)
+    if (!length(object$y))
+        out <- numeric(length(object$models))
+    if (length(object$y) <= 1) 
+        out <- structure(as.vector(out), dim = c(1, length(object$models)),
+                         dimnames = list(names(object$y), names(object$models)))
+    out
 }
 
 `fitted.radfit.frame` <-
