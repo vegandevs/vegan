42d4476da12dce45a9f4dc0236dbc4ed6c0b89a6
Jari Oksanen <jari.oksanen@oulu.fi>
2014-09-04 16:17:01 +0300

    remove permuted.index2 (defunct/deprecated for long

--- R/vegan-defunct.R
+++ R/vegan-defunct.R
@@ -1,9 +1,6 @@
 ## "new" permutation code was moved to package 'permute' in R 2.0-0.
 ## Here we list as defunct those functions that are not in 'permute'.
 
-`permuted.index2` <- function (n, control = permControl()) 
-    .Defunct("permute::shuffle", package="vegan")
-
 `metaMDSrotate` <-
     function(object, vec, na.rm = FALSE, ...)
 {
