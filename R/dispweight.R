70277611eacc86153d56cac101f076d547bf3666
Jari Oksanen <jari.oksanen@oulu.fi>
2014-09-20 08:45:20 +0300

    dispweight used arg 'group', but other vegan functions use 'groups'

--- R/dispweight.R
+++ R/dispweight.R
@@ -1,28 +1,28 @@
 `dispweight` <-
-    function(comm, group, nsimul = 1000)
+    function(comm, groups, nsimul = 1000)
 {
     ## only applicable for counts
     if (!identical(all.equal(comm, round(comm)), TRUE))
         stop("function needs counts (integers)")
     ## no groups
-    if (missing(group))
-        group <- rep(1, nrow(comm))
+    if (missing(groups))
+        groups <- rep(1, nrow(comm))
     # number of replicates per group
-    nrep <- tabulate(group) 
+    nrep <- tabulate(groups) 
     # workhorse
-    dfun <- function(comm, group, nsimul, nrep) {
+    dfun <- function(comm, groups, nsimul, nrep) {
         ## Calc Dispersion
         # group means
-        means <-  tapply(comm, group, mean)
+        means <-  tapply(comm, groups, mean)
         # omit groups with mean == 0
         if(any(means == 0)) {
-            comm <- comm[means[group] != 0]
-            group <- group[means[group] != 0, drop = TRUE]
+            comm <- comm[means[groups] != 0]
+            groups <- groups[means[groups] != 0, drop = TRUE]
             nrep <- nrep[means != 0]
             means <- means[means != 0]
         }
         # group variances
-        vars <-  tapply(comm, group, var)
+        vars <-  tapply(comm, groups, var)
         # dispersion
         d <- vars / means
         # average dispersion 
@@ -30,35 +30,35 @@
         
         ## Test
         # original chisq values
-        chi_o <- sum((comm - means[group])^2 / means[group])
+        chi_o <- sum((comm - means[groups])^2 / means[groups])
         
         # permutations
         # calculate chisq
-        pfun <- function(comm, group){
-            means <-  tapply(comm, group, mean)
+        pfun <- function(comm, groups){
+            means <-  tapply(comm, groups, mean)
             if(any(means == 0)) {
-                comm <- comm[means[group] != 0]
-                group <- group[means[group] != 0, drop = TRUE]
+                comm <- comm[means[groups] != 0]
+                groups <- groups[means[groups] != 0, drop = TRUE]
                 means <- means[means != 0]
             }
-            chi <- sum((comm - means[group])^2 / means[group])
+            chi <- sum((comm - means[groups])^2 / means[groups])
             return(chi)
         }
         # sum of individuals per group
-        sums <- tapply(comm, group, sum)
+        sums <- tapply(comm, groups, sum)
         # realocate randomly individuals to replications
         perms <- vector('list', length(sums))
         for(i in seq_along(sums)) {
             perms[[i]] <- rmultinom(n = nsimul, size = sums[i], prob = rep(1, nrep[i]) / nrep[i])
         }
         perms <- t(do.call(rbind, perms))
-        chi_p <- apply(perms, 1, pfun, sort(group))
+        chi_p <- apply(perms, 1, pfun, sort(groups))
         p <- (sum(chi_p >= chi_o) + 1) / (nsimul + 1)
         out <- list(D = d_hat, p = p, weights = ifelse(p < 0.05, 1/d_hat, 1))
         return(out)
     }
     # apply workhorse to every species
-    out <- apply(comm, 2, dfun, group, nsimul, nrep)
+    out <- apply(comm, 2, dfun, groups, nsimul, nrep)
     
     # format output
     weights <-  unlist(sapply(out, '[', 3))
