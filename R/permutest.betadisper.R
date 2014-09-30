vegandevs/vegan

commit a25576704dcdf9bab31e5995d38d54cf60177a1a

parallelise the pairwise tests too

R/permutest.betadisper.R
@@ -1,5 +1,5 @@
 `permutest.betadisper` <- function(x, pairwise = FALSE,
-                                   permutations = how(nperm = 999),
+                                   permutations = 999,
                                    parallel = getOption("mc.cores"), ...)
 {
     t.statistic <- function(x, y) {
@@ -18,18 +18,36 @@
             dim(i) <- c(1, length(idx))
         }
         R <- nrow(idx)
-        Fperm <- numeric(length = R)
+        Fperm <- matrix(nrow = R, ncol = 1)
+        if (pairwise) {                 # set up object to hold t stats
+            Tperm <- matrix(ncol = n.pairs, nrow = R)
+            Jseq <- seq_len(n.pairs)
+        }
+        rdf <- nobs - p                 # residual degrees of freedom
         ## iterate
-        for (i in seq_len(R)) {
+        for (i in seq_len(R)) {         # iterate
             take <- idx[i, ]                # current permutation from set
             p.resid <- resids[take]         # permute residuals
-            f <- qr.fitted(mod.Q, p.resid) # create new data
+            f <- qr.fitted(mod.Q, p.resid)  # create new data
             mss <- sum((f - mean(f))^2)
             r <- qr.resid(mod.Q, p.resid)
             rss <- sum(r^2)
-            rdf <- nobs - p
             resvar <- rss / rdf
-            Fperm[i] <- (mss / (p - 1)) / resvar
+            Fperm[i, ] <- (mss / (p - 1)) / resvar
+
+            ## pairwise tests
+            if(pairwise) {
+                for(j in Jseq) {
+                    grp1 <- x$distance[take][group == combin[1, j]]
+                    grp2 <- x$distance[take][group == combin[2, j]]
+                    Tperm[i, j] <- t.statistic(grp1, grp2)
+                }
+            }
+        }
+
+        ## bind on pairwise stats if any
+        if (pairwise) {
+            Fperm <- cbind(Fperm, Tperm)
         }
         Fperm
     }
@@ -52,6 +70,12 @@
     permutations <- getPermuteMatrix(permutations, nobs)
     nperm <- nrow(permutations)
 
+    ## pairwise comparisons
+    if(pairwise) {
+        combin <- combn(levels(x$group), 2) # unique pairings
+        n.pairs <- ncol(combin)
+    }
+
     ## Parallel processing of permutations
     if (is.null(parallel)) {
         parallel <- 1
@@ -59,7 +83,7 @@
     hasClus <- inherits(parallel, "cluster")
     if ((hasClus || parallel > 1L) && requireNamespace("parallel")) {
         if (.Platform$OS.type == "unix" && !hasClus) {
-            Fstats <- do.call("c",
+            Pstats <- do.call("rbind",
                            mclapply(seq_len(nperm),
                                     function(x) permFun(permutations[x, , drop = FALSE]),
                                     mc.cores = parallel))
@@ -68,65 +92,38 @@
             if (!hasClus) {
                 parallel <- makeCluster(parallel)
             }
-            Fstats <- parRapply(parallel, permutations, function(x) permFun(x))
+            Pstats <- parRapply(parallel, permutations, function(x) permFun(x))
             if (!hasClus) {
                 stopCluster(parallel)
             }
         }
     } else {
-        Fstats <- permFun(permutations)
+        Pstats <- permFun(permutations)
     }
-    F0 <- summary(mod)$fstatistic[1]
-    Fstats <- round(Fstats, 12)
-    F0 <- round(F0, 12)
 
-    ## set-up objects to hold permuted results
-    ## res <- numeric(length = nperm + 1)
-    ## res[1] <- summary(mod)$fstatistic[1]
+    ## Process results
+    F0 <- summary(mod)$fstatistic[1]
+    Fstats <- round(Pstats[, 1], 12)    # allow empty dim to be dropped
+    F0 <- round(F0, 12)
     ## pairwise comparisons
     if(pairwise) {
-        ## unique pairings
-        combin <- combn(levels(x$group), 2)
-        n.pairs <- ncol(combin)
-        t.stats <- matrix(0, ncol = n.pairs, nrow = nperm + 1)
-        t.stats[1,] <- apply(combn(levels(group), 2), 2, function(z) {
+        T0 <- apply(combn(levels(group), 2), 2, function(z) {
             t.statistic(x$distances[group == z[1]],
                         x$distances[group == z[2]])})
-    }
-
-    ## begin loop over shuffleSet perms
-    for(i in seq_len(nperm)) {
-        perm <- permutations[i,] ## take current permutation from set
-        ## perm.resid <- resids[perm] ## permute residuals
-        ## f <- qr.fitted(mod.Q, perm.resid) ## create new data
-        ## mss <- sum((f - mean(f))^2)
-        ## r <- qr.resid(mod.Q, perm.resid)
-        ## rss <- sum(r^2)
-        ## rdf <- nobs - p
-        ## resvar <- rss / rdf
-        ## res[i+1] <- (mss / (p - 1)) / resvar
-
-        ## pairwise comparisons
-        if(pairwise) {
-            for(j in seq_len(n.pairs)) {
-                grp1 <- x$distance[perm][group == combin[1, j]]
-                grp2 <- x$distance[perm][group == combin[2, j]]
-                t.stats[i+1, j] <- t.statistic(grp1, grp2)
-            }
-        }
+        Tstats <- round(Pstats[, -1, drop = FALSE], 12)
+        T0 <- round(T0, 12)
     }
 
     ## compute permutation p-value
-    ## pval <- sum(res >= res[1]) / length(res)
     pval <- (sum(Fstats >= F0) + 1) / (length(Fstats) + 1)
 
     if(pairwise) {
         df <- apply(combin, 2, function(z) {
             length(x$distances[group == z[1]]) +
                 length(x$distance[group == z[2]]) - 2})
-        pairwise <- list(observed = 2 * pt(-abs(t.stats[1,]), df),
-                         permuted = apply(t.stats, 2,
-                         function(z) sum(abs(z) >= abs(z[1]))/length(z)))
+        pairwise <- list(observed = 2 * pt(-abs(T0), df),
+                         permuted = apply(Tstats, 2,
+                         function(z) (sum(abs(z) >= abs(z[1])) + 1) / (length(z) + 1)))
         names(pairwise$observed) <- names(pairwise$permuted) <-
             apply(combin, 2, paste, collapse = "-")
     } else {

commit 305c56b53f082620c1c1e753038576923cce42b1

Add test statistic among permutations in permutest.betadisper
Fixes bug introduced in a255767, where pairwise tests compared
permutations against wrong value of test statistic. Now we add
test statistic among permutations. Because this is done (= first
row of permutations is the test statistic), we also must omit the
+1 term in the numerator and denominator.

R/permutest.betadisper.R
@@ -122,8 +122,8 @@
             length(x$distances[group == z[1]]) +
                 length(x$distance[group == z[2]]) - 2})
         pairwise <- list(observed = 2 * pt(-abs(T0), df),
-                         permuted = apply(Tstats, 2,
-                         function(z) (sum(abs(z) >= abs(z[1])) + 1) / (length(z) + 1)))
+                         permuted = apply(rbind(T0, Tstats), 2,
+                         function(z) (sum(abs(z) >= abs(z[1]))) / (length(z))))
         names(pairwise$observed) <- names(pairwise$permuted) <-
             apply(combin, 2, paste, collapse = "-")
     } else {