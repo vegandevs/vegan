vegandevs/vegan

commit efd575f1f654348dad12245e19ca2f276ea7c7dc

pass relevant named arguments from anova.cca to permutest.cca

R/anova.cca.R
@@ -2,7 +2,7 @@
     function(object, ..., permutations = how(nperm=999), by = NULL,
              model = c("reduced", "direct", "full"),
              parallel = getOption("mc.cores"), strata = NULL,
-             cutoff = 1, scope = NULL) 
+             cutoff = 1, scope = NULL)
 {
     model <- match.arg(model)
     ## permutation matrix
@@ -21,7 +21,7 @@
             dotargs <- dotargs[isCCA]
             object <- c(list(object), dotargs)
             sol <-
-                anova.ccalist(object, 
+                anova.ccalist(object,
                               permutations = permutations,
                               model = model,
                               parallel = parallel)
@@ -54,15 +54,17 @@
         return(sol)
     }
     ## basic overall test
-    tst <- permutest.cca(object, permutations = permutations, ...)
+    tst <- permutest.cca(object, permutations = permutations,
+                         model = model, parallel = parallel,
+                         strata = strata, ...)
     Fval <- c(tst$F.0, NA)
     Pval <- (sum(tst$F.perm >= tst$F.0) + 1)/(tst$nperm + 1)
     Pval <- c(Pval, NA)
     table <- data.frame(tst$df, tst$chi, Fval, Pval)
     is.rda <- inherits(object, "rda")
-    colnames(table) <- c("Df", ifelse(is.rda, "Variance", "ChiSquare"), 
+    colnames(table) <- c("Df", ifelse(is.rda, "Variance", "ChiSquare"),
                          "F", "Pr(>F)")
-    head <- paste0("Permutation test for ", tst$method, " under ", 
+    head <- paste0("Permutation test for ", tst$method, " under ",
                   tst$model, " model\n", howHead(control))
     mod <- paste("Model:", c(object$call))
     structure(table, heading = c(head, mod), Random.seed = seed,

commit 19256873076e8322165943d938db91fdcb4e48af

Squashed commit of the following:
commit 65e815f0f4e49ce728e88f128af5e00bc68e97f4
Author: Jari Oksanen <jari.oksanen@oulu.fi>
Date:   Mon Nov 10 12:26:54 2014 +0200

    Update DESCRIPTION date

    Cherry-picked fixes to 'strata' handling in anova.cca from
    master/fix-strata-redefinition

commit 4436bcb8dbb530e38cb0a541926d0606b55109e4
Author: Jari Oksanen <jari.oksanen@oulu.fi>
Date:   Mon Nov 10 11:06:09 2014 +0200

    anova.cca passes permutationMatrix: no need to pass 'strata'

    anova.cca alwyas passes a permutation matrix to permutest.cca
    and there is no need to pass 'strata' (which will be ingored).

commit 3e14e65e8d70332b35818f3c4ac7f1c4849d4289
Author: Jari Oksanen <jari.oksanen@oulu.fi>
Date:   Sun Nov 9 20:23:28 2014 +0200

    Do not set strata if they appear to have been set earlier

    anova.cca generated permutation matrix with strata in
    getPermuteMatrix and passed that on to other functions (like
    permutest.cca). This was broken in efd575f1 which passed
    strata from anova.cca (unnecessarily), but we fix it in
    getPermuteMatrix so that the solution is more general (and
    strata are to be deprecated in the next releases in any case).

git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@2910 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

DESCRIPTION
@@ -1,7 +1,7 @@
 Package: vegan
 Title: Community Ecology Package
 Version: 2.2-0
-Date: 2014-11-07
+Date: 2014-11-10
 Author: Jari Oksanen, F. Guillaume Blanchet, Roeland Kindt, Pierre Legendre, 
    Peter R. Minchin, R. B. O'Hara, Gavin L. Simpson, Peter Solymos, 
    M. Henry H. Stevens, Helene Wagner  

R/anova.cca.R
@@ -53,10 +53,10 @@
         attr(sol, "control") <- control
         return(sol)
     }
-    ## basic overall test
+    ## basic overall test: pass other arguments except 'strata'
+    ## because 'permutations' already is a permutationMatrix
     tst <- permutest.cca(object, permutations = permutations,
-                         model = model, parallel = parallel,
-                         strata = strata, ...)
+                         model = model, parallel = parallel, ...)
     Fval <- c(tst$F.0, NA)
     Pval <- (sum(tst$F.perm >= tst$F.0) + 1)/(tst$nperm + 1)
     Pval <- c(Pval, NA)

R/getPermuteMatrix.R
@@ -14,13 +14,10 @@
     if (length(perm) == 1) {
         perm <- how(nperm = perm) 
     }
-    ## apply 'strata'
+    ## apply 'strata', but only if possible: ignore silently other cases
     if (!missing(strata) && !is.null(strata)) {
-        if (!inherits(perm, "how"))  # 'perm' is a matrix
-            stop("'strata' can be used only with simple permutation or with 'how()'")
-        if (!is.null(getBlocks(perm)))
-            stop("'strata' cannot be applied when 'blocks' are defined in 'how()'")
-        setBlocks(perm) <- strata
+        if (inherits(perm, "how") && is.null(getBlocks(perm)))
+            setBlocks(perm) <- strata
     }
     ## now 'perm' is either a how() or a matrix
     if (inherits(perm, "how"))