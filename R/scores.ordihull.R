b664f6069ecc0290ef34785d04d7d26c6729518b
Gavin Simpson <ucfagls@gmail.com>
2014-11-22 19:13:51 -0600

    use seq_along(x) instead of 1:length(x)

--- R/scores.ordihull.R
+++ R/scores.ordihull.R
@@ -3,7 +3,7 @@
     function(x, ...)
 {
     out <- NULL
-    for(i in 1:length(x))
+    for(i in seq_along(x))
         out <- rbind(out, x[[i]])
     hulls <- rep(names(x), sapply(x, function(z) NROW(z)))
     attr(out, "hulls") <- hulls
