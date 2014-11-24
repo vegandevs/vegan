658ec416d51cbead065beee8a62973219f8d8555
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-11-24 10:44:01 +0000

    Squashed commit of the following:
    commit 465dc0303fe4a756b9a0680457a28e474c546eed
    Author: Gavin Simpson <ucfagls@gmail.com>
    Date:   Sat Nov 22 15:41:52 2014 -0600
    
        linestack now handles labels arg without setting names(x); allows expressions as labels for example
    
    commit 59564fb5e9a93b8a6b724b32ff77c18f8d183bae
    Author: Gavin Simpson <ucfagls@gmail.com>
    Date:   Sat Nov 22 11:44:47 2014 -0600
    
        fix bug in labels length conditional test
    
    commit c9f6b8a93d4fe8bc8ec26352ebe8772263a925a8
    Author: Jari Oksanen <jari.oksanen@oulu.fi>
    Date:   Wed Nov 19 13:38:05 2014 +0200
    
        Fix reconstruction of response matrix in CA (no CCA) for tolerance.cca
        (cherry picked from commit 0434ac4e653aeccb9b3f903a30a9fdfa55f6d7ee)
    
    commit c9817cb9bd315847db10bb257085b206e4499530
    Author: Jari Oksanen <jari.oksanen@oulu.fi>
    Date:   Tue Nov 18 15:04:36 2014 +0200
    
        More comprehensible error message when user asks biplots of CA
    
        There is no biplot for CA, and its results were handled by
        biplot.default that gave weird error messages. Now biplot.cca() was
        added to give an informative error message. An alternative to this
        would be to rename biplot.rda to biplot.cca: it already has an
        error message for non-rda objects, and would only handle rda.
        (cherry picked from commit 543ead1092a37e5791408bfc771d66a9f89efe28)
    
    commit dcf2399a79043b520f76a78b4beff85fea38a758
    Author: Jari Oksanen <jari.oksanen@oulu.fi>
    Date:   Tue Nov 18 13:45:18 2014 +0200
    
        Fail informatively if support functions for CCA are used with CA
    
        Several cca/rda/capscale support functions only worked with
        constrained ordination, but did not check their input. This resulted
        in cryptic error messages such as
    
        Error in chol2inv(Q$qr, size = rank) :
          'size' argument must be a positive integer
    
        Now they fail with more informative error message that tells that
        the method can be used only with constrained models.
        (cherry picked from commit 4d98639b1cd10a84ed41182a6e7123b84315e441)
    
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/vegan/pkg/vegan@2913 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/coef.cca.R
+++ R/coef.cca.R
@@ -1,9 +1,11 @@
 "coef.cca" <-
 function (object, ...) 
 {
-	Q <- object$CCA$QR
-	u <- object$CCA$u
-	u <- sweep(u, 1, sqrt(object$rowsum), "*")
-	qr.coef(Q, u)
+    if(is.null(object$CCA))
+        stop("unconstrained models do not have coefficients")
+    Q <- object$CCA$QR
+    u <- object$CCA$u
+    u <- sweep(u, 1, sqrt(object$rowsum), "*")
+    qr.coef(Q, u)
 }
 
