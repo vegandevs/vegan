8bc11fa94ca5ab17e007aaec30507505e377847c
Jari Oksanen <jari.oksanen@oulu.fi>
2014-11-25 15:57:08 +0200

    Handle degenerate spantrees with two, one or zero points

--- R/lines.spantree.R
+++ R/lines.spantree.R
@@ -1,9 +1,10 @@
-"lines.spantree" <-
+`lines.spantree` <-
     function (x, ord, display = "sites", ...)
 {
     ord <- scores(ord, display = display, ...)
     tree <- x$kid
-    ordiArgAbsorber(ord[-1, 1], ord[-1, 2], ord[tree, 1], ord[tree, 2],
-                   FUN = segments, ...)
+    if (x$n > 1)
+        ordiArgAbsorber(ord[-1, 1], ord[-1, 2], ord[tree, 1], ord[tree, 2],
+                        FUN = segments, ...)
     invisible()
 }
--- R/spantree.R
+++ R/spantree.R
@@ -1,4 +1,4 @@
-"spantree" <-
+`spantree` <-
     function (d, toolong = 0) 
 {
     dis <- as.dist(d)
@@ -8,7 +8,7 @@
               n = as.integer(n), val = double(n + 1),
               dad = integer(n + 1), NAOK = TRUE, PACKAGE = "vegan")
     out <- list(kid = dis$dad[2:n] + 1, dist = dis$val[2:n],
-                labels = labels, call = match.call())
+                labels = labels, n = n, call = match.call())
     class(out) <- "spantree"
     out
 }
