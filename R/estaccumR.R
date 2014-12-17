873e8f32ee9753b433020142f6b42799a9471d69
Jari Oksanen <jari.oksanen@oulu.fi>
2014-12-17 18:49:30 +0200

    estaccumR uses permute API and is parallel ready

--- R/estaccumR.R
+++ R/estaccumR.R
@@ -5,17 +5,18 @@
 {
     n <- nrow(x)
     N <- seq_len(n)
-    S <- chao <- ace <- matrix(0, nrow = n, ncol = permutations)
-    for (i in 1:permutations) {
-        take <- sample(n)
-        tmp <- estimateR(apply(x[take,], 2, cumsum))
-        S[,i] <- tmp[1,]
-        chao[,i] <- tmp[2,]
-        ace[, i] <- tmp[4,]
+    estFun <- function(idx) {
+        estimateR(apply(x[idx,], 2, cumsum))[c(1,2,4),]
     }
+    permat <- getPermuteMatrix(permutations, n)
+    tmp <- lapply(1:nperm, function(i) estFun(permat[i,]))
+    S <- sapply(tmp, function(x) x[1,])
+    chao <- sapply(tmp, function(x) x[2,])
+    ace <- sapply(tmp, function(x) x[3,])
     means <- cbind(N = N, S = rowMeans(S), Chao = rowMeans(chao),
                    ACE = rowMeans(ace))
     out <- list(S = S, chao = chao, ace = ace, N = N, means = means)
+    attr(out, "control") <- attr(permat, "control")
     class(out) <- c("estaccumR", "poolaccum")
     out
 }
