9db07ee68398f33b4d90bcf44c4b0920797b0bd2
Jari Oksanen <jari.oksanen@oulu.fi>
2014-12-18 04:41:30 +0200

    parallel processing in estaccumR
    (cherry picked from commit f5fbba392609a2d37ad68adff1d15423e6c54780)

--- R/estaccumR.R
+++ R/estaccumR.R
@@ -1,7 +1,7 @@
 ##" Individual based accumulation model. Similar to poolaccum but uses
 ##estimateR. Inherits from "poolaccum" class and uses its methods.
 `estaccumR` <-
-    function(x, permutations = 100)
+    function(x, permutations = 100, parallel = getOption("mc.cores"))
 {
     n <- nrow(x)
     N <- seq_len(n)
@@ -9,7 +9,28 @@
         estimateR(apply(x[idx,], 2, cumsum))[c(1,2,4),]
     }
     permat <- getPermuteMatrix(permutations, n)
-    tmp <- lapply(1:nperm, function(i) estFun(permat[i,]))
+    nperm <- nrow(permat)
+    ## parallel processing
+    if (is.null(parallel))
+        parallel <- 1
+    hasClus <- inherits(parallel, "cluster")
+    if ((hasClus || parallel > 1)  && require(parallel)) {
+        if(.Platform$OS.type == "unix" && !hasClus) {
+            tmp <- mclapply(1:nperm, function(i)
+                            estFun(permat[i,]),
+                            mc.cores = parallel)
+        } else {
+            if (!hasClus) {
+                parallel <- makeCluster(parallel)
+            }
+            tmp <- parLapply(parallel, 1:nperm, function(i) estFun(permat[i,]))
+            if (!hasClus)
+                stopCluster(parallel)
+        }
+    } else {
+        tmp <- lapply(1:permutations, function(i) estFun(permat[i,]))
+    }
+
     S <- sapply(tmp, function(x) x[1,])
     chao <- sapply(tmp, function(x) x[2,])
     ace <- sapply(tmp, function(x) x[3,])
