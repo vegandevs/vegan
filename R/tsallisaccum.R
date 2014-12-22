6d62897d4ada0b635418d526a287d7a693efebfe
Jari Oksanen <jari.oksanen@oulu.fi>
2014-12-22 12:33:11 +0200

    tsallisaccum uses permute API
    (cherry picked from commit 794c0633b371eb044de95a8132d0acbb9d468fb7)

--- R/tsallisaccum.R
+++ R/tsallisaccum.R
@@ -1,6 +1,6 @@
-tsallisaccum <-
-function (x, scales = seq(0, 2, 0.2), permutations = 100, raw = FALSE,
-          subset, ...)
+`tsallisaccum` <-
+    function (x, scales = seq(0, 2, 0.2), permutations = 100, raw = FALSE,
+              subset, ...)
 {
     if (!missing(subset))
         x <- subset(x, subset)
@@ -12,12 +12,13 @@ function (x, scales = seq(0, 2, 0.2), permutations = 100, raw = FALSE,
         n <- nrow(x)
         p <- ncol(x)
     }
+    pmat <- getPermuteMatrix(permutations, n)
     m <- length(scales)
     result <- array(dim = c(n, m, permutations))
     dimnames(result) <- list(pooled.sites = c(1:n), scale = scales, 
         permutation = c(1:permutations))
     for (k in 1:permutations) {
-        result[, , k] <- as.matrix(tsallis((apply(x[sample(n), 
+        result[, , k] <- as.matrix(tsallis((apply(x[pmat[i,], 
             ], 2, cumsum)), scales = scales, ...))
     }
     if (raw) {
