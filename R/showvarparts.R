af06840cd27af78a9c0b0fe72446a5b4515ada8f
Jari Oksanen <jari.oksanen@oulu.fi>
2015-01-14 22:04:01 +0200

    Redesign 4-group varpart plot with four ellipses
    New design following Legendre, Borcard & Roberts: Ecology 93,
    1234-1240 (2012).

--- R/showvarparts.R
+++ R/showvarparts.R
@@ -6,18 +6,40 @@ function(parts = 2, labels, ...)
                  c(0,0),
                  c(0,0, 1,0),
                  c(0,0, 1,0, 0.5, -sqrt(3/4)),
-                 c(-0.5,0.3, 0.5, 0.3, 0, -sqrt(3/4)+0.3)
+                 c(NA,NA)
                  )
     cp <- matrix(cp, ncol=2, byrow=TRUE)
+    if (parts < 4) {
+        xlim <- range(cp[,1] + c(-rad, rad))
+        ylim <- range(cp[,2] + c(-rad, rad))
+    } else {
+        xlim <- c(-1.7, 1.7)
+        ylim <- c(-1.7, 1.1)
+    }
     plot(cp, axes=FALSE, xlab="", ylab="", asp=1, type="n", 
-         xlim = (range(cp[,1]) + c(-rad, rad)),
-         ylim = (range(cp[,2]) + c(-rad, rad)))
+         xlim = xlim, ylim = ylim)
     box()
-    symbols(cp, circles = rep(rad, min(parts,3)), inches = FALSE, add=TRUE, ...)
-    if (parts == 4) {
-        symbols(0, 0.2, rectangles=cbind(1, 0.5), inches=FALSE, add=TRUE, ...)
-        symbols(sqrt(1/2), -sqrt(3/4)+0.2, rectangles=cbind(0.5,0.3),
-                inches=FALSE, add=TRUE, ...)
+    if (parts < 4) 
+        symbols(cp, circles = rep(rad, min(parts,3)), inches = FALSE,
+                add=TRUE, ...)
+    else {
+        ## Draw ellipses with veganCovEllipse. Supply 2x2
+        ## matrix(c(d,a,a,d), 2, 2) which defines an ellipse of
+        ## semi-major axis length sqrt(d+a) semi-minor axis sqrt(d-a).
+        d <- 1
+        a <- 1/sqrt(2)
+        ## Small ellipses X2, X3 at the centroid
+        e2 <- veganCovEllipse(matrix(c(d,-a,-a,d), 2, 2))
+        e3 <- veganCovEllipse(matrix(c(d, a, a,d), 2, 2))
+        ## wider ellipses X1, X4 at sides going through the centroid
+        L <- d+a
+        W <- (sqrt(L) - sqrt(d-a))^2
+        d <- (L+W)/2
+        a <- (L-W)/2
+        cnt <- sqrt(W/2)
+        e1 <- veganCovEllipse(matrix(c(d,-a,-a,d), 2, 2), c(-cnt, -cnt))
+        e4 <- veganCovEllipse(matrix(c(d, a, a,d), 2, 2), c( cnt, -cnt))
+        polygon(rbind(e1,NA,e2,NA,e3,NA,e4), ...)
     }
     nlabs <- switch(parts, 2, 4, 8, 16)
     if (missing(labels))
@@ -29,14 +51,10 @@ function(parts = 2, labels, ...)
            text(rbind(cp[1,], colMeans(cp), cp[2,]), labels[-nlabs], ...),
            text(rbind(cp, colMeans(cp[1:2,]), colMeans(cp[2:3,]),
                       colMeans(cp[c(1,3),]), colMeans(cp)), labels[-nlabs], ...),
-           text(rbind(1.4*cp, c(0.8, -sqrt(3/4)+0.2),
-                      colMeans(cp[1:2,]) + c(0,0.25),
-                      colMeans(cp[2:3,]), colMeans(cp[c(1,3),]),
-                      cp[1,] + c(0.1,0), cp[2,] -c(0.1,0),
-                      c(0.6, -sqrt(3/4)+0.2), colMeans(cp[1:2,]),
-                      colMeans(cp)-c(0,0.12), colMeans(cp[2:3,]) + c(0,0.14),
-                      colMeans(cp[c(1,3),]) + c(0, 0.14),
-                      colMeans(cp) + c(0,0.08)),
+           text(structure(c(-1.2, -0.6, 0.6, 1.2, -0.7, 0, -0.7, 0,
+                            0.7, 0.7, 0.3, -0.4, 0.4, -0.3, 0, 0, 0.7, 0.7,
+                            0, 0.3, 0.4, -0.6,-1.2, -0.6, 0.3, -0.7, 0, 0,
+                            -0.7, -0.4), .Dim = c(15L, 2L)),
                 labels[-nlabs], ...)
            )
     xy <- par("usr")
