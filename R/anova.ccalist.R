vegandevs/vegan

commit 77f79c8075b49922f063a5f5d5599e00ca7a8319

Fix headers of anova.ccalist

R/anova.ccalist.R
@@ -72,13 +72,13 @@
     ## collect table
     table <- data.frame(resdf, resdev, c(NA, df),
                         c(NA,changedev), c(NA,fval), c(NA,pval))
-    isRDA <- method != "cca"
+    varname <- switch(method,
+                      "cca" = "ChiSquare",
+                      "rda" = "Variance",
+                      "capscale" = "SumOfSqs")
     dimnames(table) <- list(1L:nmodels,
-                            c("Res.Df",
-                              ifelse(isRDA,"Res.Variance", "Res.ChiSquare"), 
-                              "Df",
-                              ifelse(isRDA,"Variance","ChiSquare"),
-                                     "F", "Pr(>F)"))
+                            c("ResDf", paste0("Res", varname), "Df",
+                              varname, "F", "Pr(>F)"))
     ## Collect header information
     formulae <- sapply(object, function(z) deparse(formula(z)))
     head <- paste0("Permutation tests for ", method, " under ",

commit 764d7228d63ff0097ade652fc376f0f5f0bbfe2d

Fix headers of anova.ccalist: 77f79c8075 broke (new) adonis2

R/anova.ccalist.R
@@ -72,10 +72,13 @@
     ## collect table
     table <- data.frame(resdf, resdev, c(NA, df),
                         c(NA,changedev), c(NA,fval), c(NA,pval))
-    varname <- switch(method,
-                      "cca" = "ChiSquare",
-                      "rda" = "Variance",
-                      "capscale" = "SumOfSqs")
+    if (inherits(object, "capscale") &&
+        (object$adjust != 1 || is.null(object$adjust)))
+        varname <- "SumOfSqs"
+    else if (inherits(object, "rda"))
+        varname <- "Variance"
+    else
+        varname <- "ChiSquare"
     dimnames(table) <- list(1L:nmodels,
                             c("ResDf", paste0("Res", varname), "Df",
                               varname, "F", "Pr(>F)"))