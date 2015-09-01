vegandevs/vegan

commit 1befc45e69caebc9db15e9024306921221f19adb

permute:::shuffleSet with minperm allows simpler code
Earlier we had separate code for taking allPerms or sampling
from allPerms for no. of ties <= 7 and shuffleSet for n > 7. Now
we utilize the minperm argument with shuffleSet for the same
effect.

R/nesteddisc.R
@@ -28,31 +28,23 @@
     ## Function to evaluate discrepancy
     FUN <- function(x) sum(comm[col(comm)[,x] <= rowSums(comm)] == 0)
     Ad <- FUN(x)
-    ## Go through all le-items and permute ties. Functions allPerms
-    ## and shuffleSet are in permute package.
+    ## Go through all le-items and permute ties. Function shuffleSet
+    ## is in permute package, and its minperm argument triggers
+    ## complete enumeration for no. of ties <= 7.
     for (i in seq_along(le)) {
         if (le[i] > 1) {
             take <- x
-            idx <- (1:le[i]) + cle[i]
+            idx <- seq_len(le[i]) + cle[i]
             ## Can swaps influence discrepancy?
             if (idx[1] > rs[2] || idx[le[i]] < rs[1])
                 next
-            ## Complete enumeration if no. of tied value <= NALL
-            if (le[i] <= NALL) {
-                perm <- matrix(allPerms(le[i]), ncol=le[i]) + cle[i]
-                ## Take at maximum NITER cases from complete enumeration
-                if (nrow(perm) > niter) {
-                    perm <- perm[sample.int(nrow(perm), niter),]
-                    ties <- TRUE
-                }
-            }
-            ## No complete enumeration, but a sample and potentially
-            ## duplicated orders
-            else {
+            perm <- suppressMessages(shuffleSet(le[i], niter,
+                                                control = how(minperm=5041)))
+            perm <- perm + cle[i]
+            ## maxperm is a double -- needs EPS -0.5, and original
+            ## order is not iterated, so -1 totalling -1.5
+            if ((attr(perm, "control")$maxperm - 1.5) > niter)
                 ties <- TRUE
-                perm <- shuffleSet(le[i], niter)
-                perm <- perm + cle[i]
-            }
             vals <- sapply(1:nrow(perm), function(j) {
                 take[idx] <- perm[j,]
                 FUN(take)

commit 39db8c73106c1956b12fb2e106902b2aa352de5d

Circumvent shuffleSet(2) bug in CRAN release of permute  0.8-5
shuffleSet(2) should return a 1x2 matrix (row), but it returns
a 2x1 matrix (column). This is fixed in GitHub for permute, but
not yet released to CRAN. Because of this, commit 1befc45e6
broke nesteddisc.

There are some other permute:::shuffleSet kluges that should
be removed after new permute becomes available in CRAN.

R/nesteddisc.R
@@ -38,13 +38,18 @@
             ## Can swaps influence discrepancy?
             if (idx[1] > rs[2] || idx[le[i]] < rs[1])
                 next
+            ## Following lines have some kluges to circumvent problems
+            ## in current permute release (0.8-5):
+            ## - suppressMessages(shuffleSet): future has queitly=TRUE arg
+            ## - minperm must be one too high: 5040 should suffice
+            ## - matrix(perm): shuffleSet(2) returns transposed matrix
             perm <- suppressMessages(shuffleSet(le[i], niter,
                                                 control = how(minperm=5041)))
-            perm <- perm + cle[i]
             ## maxperm is a double -- needs EPS -0.5, and original
             ## order is not iterated, so -1 totalling -1.5
             if ((attr(perm, "control")$maxperm - 1.5) > niter)
                 ties <- TRUE
+            perm <- matrix(perm, ncol = le[i]) + cle[i]
             vals <- sapply(1:nrow(perm), function(j) {
                 take[idx] <- perm[j,]
                 FUN(take)