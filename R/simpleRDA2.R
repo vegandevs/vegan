cfbe43b07296dbbd123b1eaec8737df593a4bceb
Jari Oksanen <jari.oksanen@oulu.fi>
2015-09-02 15:52:50 +0300

    checkout simpleRDA2.R from master

--- R/simpleRDA2.R
+++ R/simpleRDA2.R
@@ -1,5 +1,7 @@
-"simpleRDA2" <-
-function (Y, X, SS.Y, ...)
+### An internal function used in varpart(): Returns only the raw
+### Rsquare and the rank of constraints in RDA.
+`simpleRDA2` <-
+    function (Y, X, SS.Y, ...)
 {
     Q <- qr(X, tol=1e-6)
     Yfit.X <- qr.fitted(Q, Y)
@@ -9,3 +11,16 @@ function (Y, X, SS.Y, ...)
     list(Rsquare = Rsquare, m = Q$rank)
 }
 
+### Analogous function, but the input must be Gower double-centred
+### dissimilarities 'G = -GowerDblcen(as.matrix(dist(Y)^2))/2'. The
+### math is based on McArdle & Anderson, Ecology 82: 290-297 (2001).
+`simpleDBRDA` <-
+    function(G, X, SS.G, ...)
+{
+    Q <- qr(X, tol=1e-6)
+    Yfit.X <- qr.fitted(Q, G)
+    SS <- sum(diag(Yfit.X))
+    if (missing(SS.G)) SS.G <- sum(diag(G))
+    Rsquare <- SS/SS.G
+    list(Rsquare = Rsquare, m = Q$rank)
+}
