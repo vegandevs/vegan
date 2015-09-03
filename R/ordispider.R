48cb07f215a0398270709be973a149ca16c294de
James Weedon <jtweedon@gmail.com>
2015-09-03 16:41:41 +0200

    fixing bugs raised by R check

--- R/ordispider.R
+++ R/ordispider.R
@@ -72,7 +72,7 @@
         }
     }
     if (label) 
-        ordiArgAbsorber(cntrs, label = rownames(ctrs), FUN = ordilabel, ...)
+        ordiArgAbsorber(cntrs, label = rownames(cntrs), FUN = ordilabel, ...)
     spids <- t(spids)
     class(spids) <- "ordispider"
     invisible(spids)
