57fd562973ceb6e6c244b231343e227b4b02532e
Jari Oksanen <jari.oksanen@oulu.fi>
2015-09-15 16:18:26 +0300

    return constrained HGH instead of its input G
    with this x$CCA$G + x$CA$G = input Gower G (in non-partial
    dbrda). Prior to this x$CCA$G gave the input G, and the constrained
    had to be find as H <- tcrossprod(qr.Q(x$CCA$QR));
    H %*% x$CCA$G %*% H.

--- R/dbrda.R
+++ R/dbrda.R
@@ -128,7 +128,7 @@
                     tot.chi = sum(diag(HGH)),
                     QR = Q,
                     envcentre = attr(d$Y, "scaled:center"),
-                    Xbar = NA, G = G)
+                    Xbar = NA, G = HGH)
         G <- G - HGH
     }
     ## CA
