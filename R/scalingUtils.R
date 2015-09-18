7773f9e6cfea332f808ac725681d5542031be5eb
Gavin <gavin@haul.biol.uregina.ca>
2015-09-18 11:51:06 -0600

    alternative to fix in 668a08a41e

--- R/scalingUtils.R
+++ R/scalingUtils.R
@@ -2,21 +2,24 @@
 ##'
 ##' @description Convert user-friendly descriptions of scalings to numeric codes used by \code{scores} to date.
 ##'
-##' @param scaling character; which type of scaling is required?
+##' @param scaling character or numeric; which type of scaling is required? Numeric values are returned unaltered
 ##' @param correlation logical; should correlation-like scores be returned?
 ##' @param hill logical; should Hill's scaling scores be returned?
 `scalingType` <- function(scaling = c("none", "sites", "species", "symmetric"),
-                          correlation = FALSE, hill = FALSE)
-{
-    ## numeric scaling: return as-is
-    if (is.numeric(scaling))
-        return(scaling)
-    ## non-numeric scaling: change to numeric
-    tab <- c("none", "sites", "species", "symmetric")
-    scaling <- match.arg(scaling)
-    scl <- match(scaling, tab) - 1      # -1 as none == scaling 0
-    if (scl > 0 && (correlation || hill)) {
-        scl <- -scl
+                          correlation = FALSE, hill = FALSE) {
+    ## Only process scaling further if it is character
+    if (is.character(scaling)) {
+        ## non-numeric scaling: change to numeric
+        tab <- c("none", "sites", "species", "symmetric")
+        scaling <- match.arg(scaling)
+        scaling <- match(scaling, tab) - 1      # -1 as none == scaling 0
+        if (scaling > 0 && (correlation || hill)) {
+            scaling <- -scaling
+        }
+    } else {
+        if (!is.numeric(scaling)) {
+            stop("'scaling' is neither 'numeric' nor 'character'.")
+        }
     }
-    scl                                 # return
+    scaling                             # return
 }
