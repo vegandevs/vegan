vegandevs/vegan

commit 2be76ae61a9ccdb20cf169b79f5f93bf950407bb

Use nobs() instead of nrow(Xbar): Xbar does not exist in dbrda()

R/weights.rda.R
@@ -1,14 +1,14 @@
-"weights.rda" <-
+`weights.rda` <-
     function (object, display = "sites", ...) 
 {
     display <- match.arg(display, c("sites", "species", "lc", 
                                     "wa"))
     if (display %in% c("sites", "lc", "wa")) {
-        n <- max(nrow(object$CA$Xbar), nrow(object$CCA$Xbar))
+        n <- nobs(object)
         if (!is.null(object$na.action) &&
             inherits(object$na.action, "exclude"))
             n <- n + length(object$na.action)
     }
-    else n <- max(ncol(object$CA$Xbar), ncol(object$CCA$Xbar))
+    else n <- nobs(object)
     rep(1, n)
 }

R/deviance.rda.R
@@ -1,5 +1,5 @@
 `deviance.rda` <-
     function(object, ...)
 {
-    object$CA$tot.chi * (nrow(object$CA$Xbar) - 1)
+    object$CA$tot.chi * (nobs(object) - 1)
 }

R/extractAIC.cca.R
@@ -1,7 +1,7 @@
-"extractAIC.cca" <-
-function (fit, scale = 0, k = 2, ...)
+`extractAIC.cca` <-
+    function (fit, scale = 0, k = 2, ...)
 {
-   n <- nrow(fit$CA$Xbar)
+   n <- nobs(fit)
    edf <- 1
    if (!is.null(fit$CCA$rank)) edf <- edf + fit$CCA$qrank
    if (!is.null(fit$pCCA$rank)) edf <- edf + fit$pCCA$rank

commit 0002e85c9cd275a772f54b08a3f8bd0a10f9391e

species weigth vector had a wrong length in weights.rda
bug was introduced in 2be76ae61a9

R/weights.rda.R
@@ -9,6 +9,6 @@
             inherits(object$na.action, "exclude"))
             n <- n + length(object$na.action)
     }
-    else n <- nobs(object)
+    else n <- max(ncol(object$CA$Xbar), ncol(object$CCA$Xbar))
     rep(1, n)
 }