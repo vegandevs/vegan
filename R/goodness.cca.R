2084f4e485473e38648c0abc14307217eb4cbe47
Jari Oksanen <jari.oksanen@oulu.fi>
2015-09-21 11:23:17 +0300

    goodness2.cca: newly designed parallel implementation
    This commit retains both the old exported goodness.cca and
    unexported goodness2.cca for testing. These gave identical (within
    1e-8) results for cca(), rda() and dbrda() with Euclidean distance,
    but not for non-Euclidean distance (got to check which gave the
    correct one).

--- R/goodness.cca.R
+++ R/goodness.cca.R
@@ -77,3 +77,68 @@
         vexp <- vexp[, ncol(vexp)]
     vexp
 }
+
+`goodness2.cca` <-
+    function (object, display = c("species", "sites"), choices,
+              model = c("CCA", "CA"),
+              statistic = c("explained", "distance"),
+              summarize = FALSE, addprevious = FALSE, ...)
+{
+    display <- match.arg(display)
+    model <- match.arg(model)
+    statistic <- match.arg(statistic)
+    if (!inherits(object, "cca"))
+        stop("can be used only with objects inheriting from 'cca'")
+    if ((inherits(object, "capscale") || inherits(object, "dbrda")) &&
+        display == "species")
+        stop(gettextf("cannot analyse species with '%s'", object$method))
+    what <- if(display == "species") "v" else "u"
+    w <- weights(object, display = display)
+    pCCA <- object$pCCA$Fit
+    CA <- object[[model]][[what]]
+    eig <- object[[model]]$eig
+    eig <- eig[eig > 0]
+    ## imaginary dimensions for dbrda
+    if (inherits(object, "dbrda"))
+        CA <- cbind(CA, object[[model]][["imaginary.u"]])
+    if (inherits(object, "rda"))
+        nr <- nobs(object) - 1
+    else
+        nr <- 1
+    if (!is.null(pCCA)) {
+        if (display == "sites")
+            pCCA <- t(pCCA)
+        if (inherits(object, "dbrda"))
+            pCCA <- diag(pCCA)
+        else
+            pCCA <- diag(crossprod(pCCA))/nr
+    }
+    CA <- t(apply(diag(w) %*% CA^2 %*% diag(eig), 1,
+                  cumsum))
+    totals <- inertcomp(object, display = display)
+    comps <- colnames(totals)
+    if (statistic == "explained") {
+        tot <- rowSums(totals)
+        if (addprevious) {
+            if ("pCCA" %in% comps)
+                CA <- sweep(CA, 1, totals[,"pCCA"], "+")
+            if (model == "CA" && "CCA" %in% comps)
+                CA <- sweep(CA, 1, totals[, "CCA"], "+")
+        }
+        CA <- sweep(CA, 1, tot, "/")
+    } else {
+        if ("CA" %in% comps)
+            tot <- totals[,"CA"]
+        else
+            tot <- 0
+        if (model == "CCA" && "CCA" %in% comps)
+            tot <- totals[,"CCA"] + tot
+        CA <- sweep(-CA, 1, tot, "+")
+        CA[CA < 0] <- 0
+        CA <- sqrt(CA)
+        CA <- sweep(CA, 1, sqrt(w), "/")
+    }
+    if (summarize)
+        CA <- CA[,ncol(CA)]
+    CA
+}
