vegandevs/vegan

commit c291b29020e46b32221423f038c30705e0f842a4

fixed bug that was breaking example(ordihull)

R/ordispider.R
@@ -72,7 +72,7 @@
         }
     }
     if (label) 
-        ordiArgAbsorber(cntrs, label = names, FUN = ordilabel, ...)
+        ordiArgAbsorber(cntrs, label = rownames(ctrs), FUN = ordilabel, ...)
     spids <- t(spids)
     class(spids) <- "ordispider"
     invisible(spids)

R/ordihull.R
@@ -66,9 +66,9 @@
     }
     if (label && draw != "none") {
       if (draw == "lines") 
-        ordiArgAbsorber(cntrs[, 1], cntrs[, 2], labels = names, 
+        ordiArgAbsorber(cntrs[, 1], cntrs[, 2], labels = rownames(cntrs), 
                         col = col[match(is, inds)], FUN = text, ...)
-      else ordiArgAbsorber(cntrs, labels = names, col = NULL, 
+      else ordiArgAbsorber(cntrs, labels = rownames(cntrs), col = col[match(is, inds)], 
                            FUN = ordilabel, ...)
     }
     class(res) <- "ordihull"

commit 555d66b483ed6bbcbbfb42ffd3c101ae054ebf31

ordispider did not label spiders correctly (broken in c291b2902)

R/ordispider.R
@@ -76,7 +76,7 @@
         }
     }
     if (label) 
-        ordiArgAbsorber(cntrs, label = rownames(cntrs), FUN = ordilabel, ...)
+        ordiArgAbsorber(cntrs, label = names, FUN = ordilabel, ...)
     spids <- t(spids)
     class(spids) <- "ordispider"
     invisible(spids)