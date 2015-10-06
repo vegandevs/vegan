6e98d8b82d21d230e767f6c1a2faee29c3f75f17
Jari Oksanen <jari.oksanen@oulu.fi>
2015-10-06 19:05:12 +0300

    points and text for procrustes gained arg truemean

--- R/points.procrustes.R
+++ R/points.procrustes.R
@@ -1,19 +1,23 @@
 `points.procrustes` <-
-    function(x, display = c("target","rotated"), ...)
+    function(x, display = c("target","rotated"), truemean = FALSE, ...)
 {
     display <- match.arg(display)
-    x <- if (display == "target") x$X else x$Yrot
-    points(x, ...)
+    X <- if (display == "target") x$X else x$Yrot
+    if (truemean)
+        X <- sweep(X, 2, x$xmean, "+")
+    ordiArgAbsorber(X, FUN = points, ...)
     invisible()
 }
 
 `text.procrustes` <-
-    function(x, display = c("target","rotated"), labels, ...)
+    function(x, display = c("target","rotated"), labels, truemean = FALSE, ...)
 {
     display <- match.arg(display)
-    x <- if (display == "target") x$X else x$Yrot
+    X <- if (display == "target") x$X else x$Yrot
+    if (truemean)
+        X <- sweep(X, 2, x$xmean, "+")
     if (missing(labels))
-        labels <- rownames(x)
-    text(x, labels = labels, ...)
+        labels <- rownames(X)
+    ordiArgAbsorber(X, labels = labels, FUN = text, ...)
     invisible()
 }
