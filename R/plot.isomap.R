5af0a25abb3218971ae011d2ffc13cc775b72369
Jari Oksanen <jari.oksanen@oulu.fi>
2015-11-07 16:16:21 +0200

    isomap net colour can be a mixture of point colours

--- R/plot.isomap.R
+++ R/plot.isomap.R
@@ -8,6 +8,12 @@
         pl <- ordiplot(x, display = "sites", type = "none", ...)
         z <- scores(pl, "sites")
         k <- x$net
+        ## recycle colour for points
+        n.col <- rep(n.col, length = nrow(z))
+        n.col <- col2rgb(n.col)/255
+        ## get average of colours of connected points
+        n.col <- (n.col[,k[,1]] + n.col[,k[,2]])/2
+        n.col <- rgb(t(n.col))
         segments(z[k[,1],1], z[k[,1],2], z[k[,2],1], z[k[,2],2], col=n.col)
         if (type == "points")
             points(pl, "sites", ...)
