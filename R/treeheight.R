742c91e58c3451917cbfc82f262e5c1f6c169916
Jari Oksanen <jari.oksanen@oulu.fi>
2015-11-28 14:57:59 +0200

    refuse to measure trees with negative heights
    (cherry picked from commit ae4d462d34f51292dde927e2a564d5eec1c34335)

--- R/treeheight.R
+++ R/treeheight.R
@@ -4,6 +4,10 @@
     if (inherits(tree, "spantree"))
         return(sum(tree$dist))
     tree <- as.hclust(tree)
+    ## nodes should start from 0 -- if there are negative heights,
+    ## tree is too pathological to be measured.
+    if (any(tree$height < 0))
+        stop("negative heights: tree cannot be measured")
     ## can be done really fast if there are no reversals, but we need
     ## to traverse the tree with reversals
     if (is.unsorted(tree$height)) { # slow
@@ -13,7 +17,7 @@
         for (i in 1:nrow(m)) {
             for (j in 1:2) {
                 if (m[i,j] < 0)
-                    height <- height + abs(h[i])
+                    height <- height + h[i]
                 else
                     height <- height + abs(h[i] - h[m[i,j]])
             }
