8544df49ae4a39078fce8509ae02cb056d77fe75
Jari Oksanen <jari.oksanen@oulu.fi>
2015-11-28 14:52:39 +0200

    treeheight did not handle reversals in tree
    The whole tree must be traversed to find reversals. This is
    similar as the old slow code of 2011 (pre 535dda45), but also
    adds abs() to handle negative heights.

--- R/treeheight.R
+++ R/treeheight.R
@@ -4,6 +4,23 @@
     if (inherits(tree, "spantree"))
         return(sum(tree$dist))
     tree <- as.hclust(tree)
-    sum(tree$height) + max(tree$height)
+    ## can be done really fast if there are no reversals, but we need
+    ## to traverse the tree with reversals
+    if (is.unsorted(tree$height)) { # slow
+        h <- tree$height
+        m <- tree$merge
+        height <- 0
+        for (i in 1:nrow(m)) {
+            for (j in 1:2) {
+                if (m[i,j] < 0)
+                    height <- height + abs(h[i])
+                else
+                    height <- height + abs(h[i] - h[m[i,j]])
+            }
+        }
+        height
+    }
+    else    # fast
+        sum(tree$height) + max(tree$height)
 }
 
