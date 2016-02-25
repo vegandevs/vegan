a10516e1f69ebd33415da4d8a3bed5d87acbc673
Jari Oksanen <jari.oksanen@oulu.fi>
2016-02-25 11:07:09 +0200

    partition.pdf is now a vignette and vegandocs is pretty useless

--- R/vegandocs.R
+++ R/vegandocs.R
@@ -10,13 +10,6 @@
         vignette(doc, package = "vegan")
     else if (doc == "permutations")
         vignette(doc, package = "permute")
-    else if (doc == "partitioning") { # pdf but not a vignette
-        doc <- paste0(doc, ".pdf")
-        doc <- file.path(system.file(package="vegan"), "doc", doc)
-        if (.Platform$OS.type == "windows")
-            shell.exec(doc)
-        else system(paste(getOption("pdfviewer"), doc, "&"))
-    }
     else # last resort
         file.show(system.file(package="vegan", doc))
 }
