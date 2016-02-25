f54ac7ee598b65b3e9f4367f2d170762746a14e0
Jari Oksanen <jari.oksanen@oulu.fi>
2016-02-25 13:19:13 +0200

    deprecate vegandocs
    The main reason for the urgent deprecation was that old R (3.0.0)
    does not work with HTML vignette and now we at least can point
    to an alternative that works (browseVignettes).
    
    (cherry picked from commit 6e83a72377b35752e67f1408d45c990aea2363ef)

--- R/vegandocs.R
+++ R/vegandocs.R
@@ -4,12 +4,15 @@
               "decision-vegan", "partitioning", "permutations")) 
 {
     doc <- match.arg(doc)
-    if (doc == "NEWS")
+    if (doc == "NEWS") {
+        .Deprecated('news(package="vegan")')
         news(package = "vegan")
-    else if (doc %in% vignette(package="vegan")$results[, "Item"])
+    } else if (doc %in% vignette(package="vegan")$results[, "Item"]) {
+        .Deprecated('browseVignettes("vegan")')
         vignette(doc, package = "vegan")
-    else if (doc == "permutations")
+    } else if (doc == "permutations") {
+        .Deprecated('browseVignettes("permute")')
         vignette(doc, package = "permute")
-    else # last resort
+    } else  # last resort
         file.show(system.file(package="vegan", doc))
 }
