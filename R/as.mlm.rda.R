95e0a0a1b0859296243b1ddd4c1a724078c6352c
Jari Oksanen <jari.oksanen@oulu.fi>
2016-03-07 10:49:44 +0200

    as.mlm.rda works with dbrda after it got WA scores

--- R/as.mlm.rda.R
+++ R/as.mlm.rda.R
@@ -1,8 +1,6 @@
 `as.mlm.rda` <-
     function (x) 
 {
-    if (inherits(x, "dbrda"))
-        stop("cannot be used with 'dbrda'")
     X <- qr.X(x$CCA$QR)
     lm(x$CCA$wa ~ . - 1, data = as.data.frame(X))
 }
