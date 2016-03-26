af076227ef24f0fdf0f96415de03062eb224e455
Jari Oksanen <jari.oksanen@oulu.fi>
2016-03-26 21:30:13 +0200

    C code for Strona's curveball algorithm of sequential swapping
    Strona et al. 2014 (NATURE COMMUNICATIONS | 5:4114 |
    DOI:10.1038/ncomms5114 | www.nature.com/naturecommunications)
    suggested swapping all possible pairs (unique in one site) of
    two random sites. This gives faster advancing sequences than
    other sequential swap algorithms which only swap 2x2 submatrices.

--- src/nestedness.c
+++ src/nestedness.c
@@ -163,6 +163,67 @@ void swap(int *m, int *nr, int *nc, int *thin)
     PutRNGstate();
 }
 
+/* Strona et al. 2014 (NATURE COMMUNICATIONS | 5:4114 |
+ * DOI:10.1038/ncomms5114 | www.nature.com/naturecommunications)
+ * suggested a boosted sequential binary swap method. Instead of
+ * looking for random 2x2 submatrices, they look for 2 rows and swap
+ * every swappable item in one sweep. Swappable item is a presense in
+ * only one of the compared rows, and the maximum number of swappable
+ * items is the smaller of unique presences in compared sites. They
+ * used a bizarre name ("curveball") and narrative for their method,
+ * and we also use this name here.
+ */
+
+void curveball(int *m, int *nr, int *nc, int *thin, int *wrk1, int *wrk2)
+{
+    int row[2], i, j, jind, ind1, ind2, itmp, tmp;
+
+    /* Set RNG */
+    GetRNGstate();
+
+    for (i = 0; i < *thin; i++) {
+	/* Random sites */
+	i2rand(row, (*nr)-1);
+	/* Vectors of unique species for each random species */
+	for (j = 0, ind1 = -1, ind2 = -1; j < (*nc); j++) {
+	    jind <- j * (*nr)
+	    if (m[row[0] + jind] > 0 && m[row[1] + jind] == 0)
+		wrk1[++ind1] = j;
+	    if (m[row[1] + jind] > 0 && m[row[0] + jind] == 0)
+		wrk2[++ind2] = j;
+	}
+	/* If both rows had unique species, we swap the smaller number */
+	if (ind1 > -1 && ind2 > -1) {
+	    /* All items of smaller set are swapped, but we need a random
+	     * subset for the longer one */
+	    if (ind1 > ind2)
+		for (j = ind1; j >= ind2; j--) {
+		    tmp = wrk1[j];
+		    itmp = IRAND(j);
+		    wrk1[itmp] = wrk1[j];
+		    wrk1[j] = tmp;
+		}
+	    if (ind2 > ind1)
+		for (j = ind2; j >= ind1; j--)  {
+		    tmp = wrk2[j];
+		    itmp = IRAND(j);
+		    wrk2[itmp] = wrk2[j];
+		    wrk2[j] = tmp;
+		}
+	}
+	/* Do swaps */
+	if (ind1 > ind2)
+	    ind1 = ind2;
+	for (j = 0; j < ind1; j++) {
+	    m[INDX(row[0], wrk1[j], *nr)] = 0;
+	    m[INDX(row[0], wrk2[j], *nr)] = 1;
+	    m[INDX(row[1], wrk2[j], *nr)] = 0;
+	    m[INDX(row[1], wrk1[j], *nr)] = 1;
+	}
+    }
+
+    PutRNGstate();
+}
 
 /* 'swapcount' is a C translation of Peter Solymos's R code. It is
  * similar to 'swap', but can swap > 1 values and so works for
