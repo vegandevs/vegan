5ca604173800f8aa41e74e6abfe44ce925e8a49d
Gavin Simpson <ucfagls@gmail.com>
2016-04-17 13:59:22 -0600

    prettier plots by default of betadisper output; also adds changing of lwd of ellipses and hulls as per #168

--- R/plot.betadisper.R
+++ R/plot.betadisper.R
@@ -1,13 +1,16 @@
 `plot.betadisper` <- function(x, axes = c(1,2), cex = 0.7, pch = seq_len(ng),
-                              col = NULL, hull = TRUE,
+                              col = NULL, lty = "solid", lwd = 1, hull = TRUE,
                               ellipse = FALSE, ellipse.type = c("sd","se"),
                               ellipse.conf = NULL,
+                              segments = TRUE, seg.col = "grey",
+                              seg.lty = lty, seg.lwd = lwd,
+                              label = TRUE, label.cex = 1,
                               ylab, xlab, main, sub, ...)
 {
     localAxis <- function(..., col, bg, pch, cex, lty, lwd) axis(...)
     localBox <- function(..., col, bg, pch, cex, lty, lwd) box(...)
     localTitle <- function(..., col, bg, pch, cex, lty, lwd) title(...)
-    Ellipse <- function(scrs, type, conf, col, lty, lwd) {
+    Ellipse <- function(scrs, type, conf, col, lty, lwd, ...) {
         mat <- cov.wt(scrs)
         if (mat$n.obs == 1)
             mat$cov[] <- 0
@@ -19,7 +22,7 @@
         } else {
             scrs
         }
-        ordiArgAbsorber(xy, FUN = lines, col = col, lty = lty, lwd = lwd)
+        ordiArgAbsorber(xy, FUN = lines, col = col, lty = lty, lwd = lwd, ...)
     }
     if(missing(main))
         main <- deparse(substitute(x))
@@ -42,7 +45,8 @@
     if (is.null(col)) {
         col <- palette()
     }
-    col <- rep_len(col, ng)  # make sure there are enough colors
+    col <- rep_len(col, ng)        # make sure there are enough colors
+    seg.col <- rep_len(seg.col, ng)     # ditto for segments
     plot(g$sites, asp = 1, type = "n", axes = FALSE, ann = FALSE, ...)
     ## if more than 1 group level
     if(is.matrix(g$centroids)) {
@@ -50,39 +54,47 @@
             curlev <- lev[i]
             take <- x$group == curlev
             j <- which(lev == curlev)
-            segments(g$centroids[j, 1L], g$centroids[j, 2L],
-                     g$sites[take, 1L],
-                     g$sites[take, 2L], col = "blue", ...)
+            if (segments) {
+                segments(g$centroids[j, 1L], g$centroids[j, 2L],
+                         g$sites[take, 1L],
+                         g$sites[take, 2L], col = seg.col[i], lty = seg.lty,
+                         lwd = seg.lwd)
+            }
             if(hull) {
                 ch <- chull(g$sites[take, ])
                 ch <- c(ch, ch[1])
-                lines(x$vectors[take, axes][ch, ],
-                      col = col[i], lty = "dashed", ...)
+                lines(x$vectors[take, axes][ch, ], col = col[i], lty = lty,
+                      lwd = lwd, ...)
             }
             if (ellipse) {
                 Ellipse(g$sites[take, , drop = FALSE], type = ellipse.type,
-                        conf = conf, col = col[i], lty = 1, lwd = 1)
+                        conf = conf, col = col[i], lty = lty, lwd = lwd, ...)
             }
+            points(g$centroids[j, , drop = FALSE], pch = 16, cex = 1, col = col[i], ...)
         }
-        points(g$centroids, pch = 16, cex = 1, col = "red", ...)
     } else {
         ## single group
-        segments(g$centroids[1L], g$centroids[2L],
-                 g$sites[, 1L], g$sites[, 2L], col = "blue", ...)
+        if (segments) {
+            segments(g$centroids[1L], g$centroids[2L], g$sites[, 1L], g$sites[, 2L],
+                     col = seg.col, lty = seg.lty, ...)
+        }
         if(hull) {
             ch <- chull(g$sites)
             ch <- c(ch, ch[1])
-            lines(x$vectors[, axes][ch, ],
-                  col = col[1L], lty = "dashed", ...)
+            lines(x$vectors[, axes][ch, ], col = col[1L], lty = lty,
+                  lwd = lwd, ...)
         }
         if (ellipse) {
                 Ellipse(g$sites, type = ellipse.type,
-                        conf = conf, col = col[1L], lty = 1, lwd = 1)
+                        conf = conf, col = col[1L], lty = lty, lwd = lwd,...)
         }
         points(g$centroids[1L], g$centroids[1L],
-               pch = 16, cex = 1, col = "red", ...)
+               pch = 16, cex = 1, col = col[1L], ...)
     }
     points(g$sites, pch = pch[x$group], cex = cex, col = col[x$group], ...)
+    if (label) {
+        ordilabel(x, display = "centroids", choices = axes, cex = label.cex)
+    }
     localTitle(main = main, xlab = xlab, ylab = ylab, sub = sub, ...)
     localAxis(1, ...)
     localAxis(2, ...)
