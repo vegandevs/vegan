02dcd5ea5f7fd6b4417486e26e30aebd31033fc3
Peter Solymos <psolymos@gmail.com>
2016-04-20 08:54:30 -0600

    start = burnin + thin

--- R/as.ts.oecosimu.R
+++ R/as.ts.oecosimu.R
@@ -3,8 +3,8 @@
 {
     if  (!x$oecosimu$isSeq)
         stop("as.ts available only for sequential methods")
-    startval <- attr(x$oecosimu$simulated, "burnin") + 1 
     thin <- attr(x$oecosimu$simulated, "thin")
+    startval <- attr(x$oecosimu$simulated, "burnin") + thin
     out <- ts(t(x$oecosimu$simulated), start = startval, deltat=thin,
         names = names(x$oecosimu$z))
     attr(out, "burnin") <- NULL
