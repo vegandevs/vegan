9e51e1cdd84f6a79238d399d82cac2076f67bb9a
Peter Solymos <psolymos@gmail.com>
2016-05-18 20:40:19 -0600

    method arg exposed in hiersimu

--- R/hiersimu.formula.R
+++ R/hiersimu.formula.R
@@ -1,18 +1,16 @@
 `hiersimu.formula` <-
     function(formula, data, FUN, location = c("mean", "median"),
-             relative = FALSE, drop.highest = FALSE, nsimul=99, ...)
+             relative = FALSE, drop.highest = FALSE, nsimul=99,
+             method = "r2dtable", ...)
 {
     ## evaluate formula
     if (missing(data))
         data <- parent.frame()
     tmp <- hierParseFormula(formula, data)
-    lhs <- tmp$lhs
-    rhs <- tmp$rhs
-
     ## run simulations
-    sim <- hiersimu.default(lhs, rhs, FUN = FUN, location = location,
+    sim <- hiersimu.default(tmp$lhs, tmp$rhs, FUN = FUN, location = location,
                             relative = relative, drop.highest = drop.highest,
-                            nsimul = nsimul, ...)
+                            nsimul = nsimul, method = method, ...)
     call <- match.call()
     call[[1]] <- as.name("hiersimu")
     attr(sim, "call") <- call
