7123934922d8f497284e3025f96dd37e04119c92
Jari Oksanen <jari.oksanen@oulu.fi>
2016-06-04 12:09:32 +0300

    remove euclidifying constant in fitted.dbrda (effects also residuals)
    now fitted & residuals are consistent in capscale and dbrda which
    was one of the concerns in issue #179 in github.

--- R/fitted.dbrda.R
+++ R/fitted.dbrda.R
@@ -31,6 +31,15 @@
         U <- object[[model]]$u
         U <- sweep(U, 2, sqrt(eig[eig>0]), "*")
         D <- dist(U)
+        ## remove additive constant
+        if (!is.null(object$ac)) {
+            if (object$add == "lingoes")
+                D <- sqrt(D^2 - 2 * object$ac)
+            else if (object$add == "cailliez")
+                D <- D - object$ac
+            else stop("unknown Euclidifying adjustment")
+        }
+        ## remove negative distances in imaginary space
         if (any(eig < 0)) {
             U <- object[[model]]$imaginary.u
             U <- sweep(U, 2, sqrt(abs(eig[eig<0])), "*")
