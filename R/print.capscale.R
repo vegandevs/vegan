7022ab5e1e366e5f56bd62d6ad899861248e2b1b
Jari Oksanen <jari.oksanen@oulu.fi>
2016-06-04 11:57:36 +0300

    remove separate print.capscale and use print.cca for capscale & dbrda

--- R/print.capscale.R
+++
@@ -1,9 +0,0 @@
-`print.capscale` <-
-    function(x, ...)
-{
-    NextMethod("print", x, ...)
-    if (!is.null(x$metaMDSdist))
-        cat("metaMDSdist transformed data:", x$metaMDSdist, "\n\n")
-    if (!is.null(x$ac))
-        cat("Constant added to distances:", x$ac, "\n\n")
-}
--- R/print.cca.R
+++ R/print.cca.R
@@ -69,5 +69,11 @@
         else print(zapsmall(x$CA$eig, digits = digits), ...)
     }
     cat("\n")
+    if (inherits(x, c("capscale", "dbrda"))) {
+        if (!is.null(x$metaMDSdist))
+            cat("metaMDSdist transformed data:", x$metaMDSdist, "\n\n")
+        if (!is.null(x$ac))
+            cat("Constant added to distances:", x$ac, "\n\n")
+    }
     invisible(x)
 }
