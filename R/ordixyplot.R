cbe30dfc4c30f75c9c8ba6d126d1a51218f11838
Jari Oksanen <jari.oksanen@oulu.fi>
2016-07-22 10:27:32 +0300

    indentation: offset 4

--- R/ordixyplot.R
+++ R/ordixyplot.R
@@ -3,27 +3,28 @@
              panel = "panel.ordi", aspect = "iso", envfit,
              type = c("p", "biplot"),  ...)
 {
-  localXyplot <- function(..., shrink, origin, scaling) xyplot(...)
-  p <- as.data.frame(scores(x, display = display, choices = choices, ...))
-  if (!is.null(data))
-    p <- cbind(p, data)
+    localXyplot <- function(..., shrink, origin, scaling) xyplot(...)
+    p <- as.data.frame(scores(x, display = display, choices = choices, ...))
+    if (!is.null(data))
+        p <- cbind(p, data)
     if (missing(formula)) {
-      v <- colnames(p)
-      formula <- as.formula(paste(v[2], "~", v[1]))
+        v <- colnames(p)
+        formula <- as.formula(paste(v[2], "~", v[1]))
     }
-  if ("biplot" %in% type && ((!is.null(x$CCA) && x$CCA$rank > 0) ||
-                             !missing(envfit))) {
-    if (missing(envfit))
-      envfit <- NULL
-    env <- ordilattice.getEnvfit(formula, x, envfit, choices, ...)
-    if (!is.null(env$arrows)) {
-      mul <- apply(p[,colnames(env$arrows)], 2, range)/apply(env$arrows, 2, range)
-      mul <- min(mul[is.finite(mul) & mul > 0])
+    if ("biplot" %in% type && ((!is.null(x$CCA) && x$CCA$rank > 0) ||
+                                   !missing(envfit))) {
+        if (missing(envfit))
+            envfit <- NULL
+        env <- ordilattice.getEnvfit(formula, x, envfit, choices, ...)
+        if (!is.null(env$arrows)) {
+            mul <- apply(p[,colnames(env$arrows)], 2, range)/apply(env$arrows, 2, range)
+            mul <- min(mul[is.finite(mul) & mul > 0])
             env$arrows <- mul * env$arrows
+        }
+    } else {
+        env <- NULL
     }
-  } else {
-    env <- NULL
-  }
-  localXyplot(formula, data = p, panel = panel,  aspect = aspect, biplot = env, type = type,
-         ...)
+
+    localXyplot(formula, data = p, panel = panel,  aspect = aspect,
+                biplot = env, type = type, ...)
 }
