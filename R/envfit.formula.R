1942df4c23f6777f07a7a476c0224b6666f267d5
Jari Oksanen <jari.oksanen@oulu.fi>
2016-10-06 14:06:14 +0300

    better (I hope so)  handling of data & environments in formula
    (cherry picked from commit b651ad28bb9007c2113414b9154275307cbd6fc9)

--- R/envfit.formula.R
+++ R/envfit.formula.R
@@ -1,10 +1,10 @@
-"envfit.formula" <-
+`envfit.formula` <-
     function(formula, data, ...)
 {
     if (missing(data))
-        data <- parent.frame()
+        data <- environment(formula)
     X <- formula[[2]]
-    X <- eval(X, data, parent.frame())
+    X <- eval(X, environment(formula), enclos = .GlobalEnv)
     formula[[2]] <- NULL
     P <- model.frame(formula, data, na.action = na.pass)
     envfit.default(X, P, ...)
