9b3f365e9ab5567b644bf38f3eb0387f3b6189df
Jari Oksanen <jari.oksanen@oulu.fi>
2016-10-08 08:02:20 +0300

    search lhs in the environment(formula)
    (cherry picked from commit dad5070f4d68eb6bafb75a1878a9bb42f6a54333)

--- R/hierParseFormula.R
+++ R/hierParseFormula.R
@@ -1,14 +1,14 @@
-"hierParseFormula" <-
-function (formula, data)
+`hierParseFormula` <-
+    function (formula, data)
 {
     lhs <- formula[[2]]
-    if (any(attr(terms(formula, data = data), "order") > 1)) 
+    if (any(attr(terms(formula, data = data), "order") > 1))
         stop("interactions are not allowed")
-    lhs <- as.matrix(eval(lhs, data))
+    lhs <- as.matrix(eval(lhs, environment(formula), parent.frame()))
     formula[[2]] <- NULL
     rhs <- model.frame(formula, data, drop.unused.levels = TRUE)
     rhs[] <- lapply(rhs, function(u) {
-        if (!is.factor(u)) 
+        if (!is.factor(u))
             u <- factor(u)
         u
     })
