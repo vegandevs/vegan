09f3a4539f6c0f2359bd45adc6912ab92d0bbc35
Jari Oksanen <jari.oksanen@oulu.fi>
2016-10-13 19:44:10 +0300

    use rowSums & colSums: faster than apply + remove trailing white space
    (cherry picked from commit 059745c5bf568349f833c0a4a19808696460bd08)

--- R/read.cep.R
+++ R/read.cep.R
@@ -1,6 +1,6 @@
 "read.cep" <-
   function (file, maxdata = 10000, positive = TRUE, trace = FALSE,
-            force = FALSE) 
+            force = FALSE)
 {
   if (!force) {
     stop("R may crash: if you want to try, save your session and use 'force=TRUE'")
@@ -9,27 +9,27 @@
       warning("It seems that you have used gfortran: the input may be corrupted\n")
   ftypes <- c("free", "open", "condensed")
   file <- path.expand(file)
-  if (trace) 
+  if (trace)
     cat("File", file, "\n")
   if (file.access(file, 4) < 0) {
     stop("file does not exist or is not readable")
   }
   on.exit(.Fortran("cepclose", PACKAGE = "vegan"))
-  cep <- .Fortran("cephead", file = file, kind = integer(1), 
-                  nitem = integer(1), nst = integer(1), fmt = character(1), 
+  cep <- .Fortran("cephead", file = file, kind = integer(1),
+                  nitem = integer(1), nst = integer(1), fmt = character(1),
                   PACKAGE = "vegan")
-  if (cep$kind > 3) 
+  if (cep$kind > 3)
     stop("Unknown CEP file type")
   if (trace) {
     cat("looks like", ftypes[cep$kind], "CEP file,\n")
     cat("with", cep$nitem, "items per record")
-    if (cep$kind == 1) 
+    if (cep$kind == 1)
       cat(" and", cep$nst, "records")
     cat(".\n")
   }
   switch(cep$kind,
          cd <- .Fortran("cepfree",
-                        nitem = as.integer(cep$nitem), 
+                        nitem = as.integer(cep$nitem),
                         axdat = as.integer(maxdata),
                         nsp = integer(1),
                         nst = as.integer(cep$nst),
@@ -38,17 +38,17 @@
                         y = double(maxdata),
                         w = double(cep$nitem),
                         ier = integer(1),
-                        PACKAGE = "vegan"), 
+                        PACKAGE = "vegan"),
          cd <- .Fortran("cepopen",
-                        fmt = as.character(cep$fmt), 
+                        fmt = as.character(cep$fmt),
                         nitem = as.integer(cep$nitem),
-                        maxdat = as.integer(maxdata), 
+                        maxdat = as.integer(maxdata),
                         nsp = integer(1),
                         nst = integer(1),
-                        i = integer(maxdata), 
+                        i = integer(maxdata),
                         j = integer(maxdata),
                         y = double(maxdata),
-                        w = double(cep$nitem), 
+                        w = double(cep$nitem),
                         ier = integer(1),
                         PACKAGE = "vegan"),
          cd <- .Fortran("cepcond",
@@ -65,11 +65,11 @@
                         ier = integer(1),
                         PACKAGE = "vegan"))
   if (cd$ier) {
-    if (cd$ier == 1) 
+    if (cd$ier == 1)
       stop("too many non-zero entries: increase maxdata")
     else stop("unknown and obscure error: I do not know what to do")
   }
-  if (trace) 
+  if (trace)
     cat("Read", cd$nsp, "species, ", cd$nst, "sites.\n")
   d <- matrix(0, cd$nst, cd$nsp)
   for (i in seq_along(cd$i)) d[cd$i[i], cd$j[i]] <- cd$y[i]
@@ -78,7 +78,7 @@
   for (i in seq_len(nlines)) {
     tmpnames <- .Fortran("cepnames", character(1), PACKAGE = "vegan")
     tmpnames <- substring(as.character(tmpnames), 1, 80)
-    tmpnames <- substring(tmpnames, seq(1, 80, by = 8), seq(8, 
+    tmpnames <- substring(tmpnames, seq(1, 80, by = 8), seq(8,
                                                  80, by = 8))
     names <- c(names, tmpnames)
   }
@@ -90,7 +90,7 @@
   for (i in seq_len(nlines)) {
     tmpnames <- .Fortran("cepnames", character(1), PACKAGE = "vegan")
     tmpnames <- substring(as.character(tmpnames), 1, 80)
-    tmpnames <- substring(tmpnames, seq(1, 80, by = 8), seq(8, 
+    tmpnames <- substring(tmpnames, seq(1, 80, by = 8), seq(8,
                                                  80, by = 8))
     names <- c(names, tmpnames)
   }
@@ -98,8 +98,8 @@
   names <- make.names(names, unique = TRUE)
   rownames(d) <- names[seq_len(nrow(d))]
   if (positive) {
-    rsum <- apply(d, 1, sum)
-    csum <- apply(d, 2, sum)
+    rsum <- rowSums(d)
+    csum <- colSums(d)
     d <- d[rsum > 0, csum > 0]
   }
   as.data.frame(d)
