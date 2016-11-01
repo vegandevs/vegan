ff40a5ad6d836f6e074334b2a2d4c9dd175a6477
Jari Oksanen <jari.oksanen@oulu.fi>
2016-11-01 15:00:15 +0200

    only set constraints if they are non-NULL

--- R/cca.formula.R
+++ R/cca.formula.R
@@ -10,18 +10,21 @@
     d <- ordiParseFormula(formula, data = data, na.action = na.action,
                           subset = substitute(subset))
     sol <- cca.default(d$X, d$Y, d$Z)
-    centroids <- centroids.cca(sol$CCA$wa, d$modelframe,
+    if (!is.null(sol$CCA) && sol$CCA$rank > 0) {
+        centroids <- centroids.cca(sol$CCA$wa, d$modelframe,
                                    sol$rowsum)
-    if (!is.null(sol$CCA$alias))
-        centroids <- unique(centroids)
-    ## See that there really are centroids
-    if (!is.null(centroids)) {
-        rs <- rowSums(centroids^2)
-        centroids <- centroids[rs > 1e-04,, drop = FALSE]
-        if (length(centroids) == 0)
-            centroids <- NULL
+        if (!is.null(sol$CCA$alias))
+            centroids <- unique(centroids)
+        ## See that there really are centroids
+        if (!is.null(centroids)) {
+            rs <- rowSums(centroids^2)
+            centroids <- centroids[rs > 1e-04,, drop = FALSE]
+            if (length(centroids) == 0)
+                centroids <- NULL
+        }
+        if (!is.null(centroids))
+            sol$CCA$centroids <- centroids
     }
-    sol$CCA$centroids <- centroids
     ## replace cca.default call
     call <- match.call()
     call[[1]] <- as.name("cca")
