9e5390b758bf1ed269a3b84f3d42ced274bdd802
Jari Oksanen <jari.oksanen@oulu.fi>
2016-11-02 14:08:05 +0200

    re-instate old scaling of internal results in RDA/PCA for support functions
    The new ordConstraint scales data so that the eigenvalues directly
    decompose the variance, whereas earlier we analysed non-scaled data
    and only scaled the eigenvalues. Several support functions should
    be changed to handle this, and therefore we now make rda.default
    to back-scale internal data (Xbar, colsum) to the original data.
    Eventually we should fix the support functions.

--- R/rda.default.R
+++ R/rda.default.R
@@ -8,6 +8,18 @@
         stop("function cannot be used with (dis)similarities")
     X <- as.matrix(X)
     sol <- ordConstrained(X, Y, Z, scale = scale, method = "rda")
+    ## back-scale Xbar/Fit and colsum to the scale of observations:
+    ## this should be handled in support functions, but we test it
+    ## here to see that the results are consistent with previus ones.
+    if (!scale) {
+        scl <- sqrt(nrow(X) - 1)
+        if (!is.null(sol$pCCA))
+            sol$pCCA$Fit <- scl * sol$pCCA$Fit
+        if (!is.null(sol$CCA))
+            sol$CCA$Xbar <- scl * sol$CCA$Xbar
+        sol$CA$Xbar <- scl * sol$CA$Xbar
+        sol$colsum <- scl * sol$colsum
+    }
     call <- match.call()
     call[[1]] <- as.name("rda")
     sol$call <- call
