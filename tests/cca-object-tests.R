bfe7d970cd6f029eb9a335302e0fbe5ea0c3ec0e
Jari Oksanen <jari.oksanen@oulu.fi>
2016-11-06 09:18:19 +0200

    add tests for scaled rda and anova by-cases in cca-object-tests
    reference output generated at commit f3c2204bc2

--- tests/cca-object-tests.R
+++ tests/cca-object-tests.R
@@ -5,6 +5,7 @@ set.seed(4711)
 data(dune, dune.env)
 mcca <- cca(dune ~ Condition(Management) + Manure + A1, dune.env)
 mrda <- rda(dune ~ Condition(Management) + Manure + A1, dune.env)
+mrda1 <- rda(dune ~ Condition(Management) + Manure + A1, dune.env, scale=TRUE)
 mcap <- capscale(dune ~ Condition(Management) + Manure + A1, dune.env,
           dist = "bray")
 mdb <- dbrda(dune ~ Condition(Management) + Manure + A1, dune.env,
@@ -29,16 +30,19 @@ str(mdb, max.level=2)
 
 head(goodness(mcca, display = "sites"))
 head(goodness(mrda, display = "sites"))
+head(goodness(mrda1, display = "sites"))
 head(goodness(mcap, display = "sites"))
 head(goodness(mdb, display="sites"))
 
 head(inertcomp(mcca))
 head(inertcomp(mrda))
+head(inertcomp(mrda1))
 head(inertcomp(mcap, display="sites"))
 head(inertcomp(mdb, display = "sites"))
 
 intersetcor(mcca)
 intersetcor(mrda)
+intersetcor(mrda1)
 intersetcor(mcap)
 intersetcor(mdb)
 
@@ -58,11 +62,13 @@ alias(mdb)
 
 coef(mcca)
 coef(mrda)
+coef(mrda1)
 coef(mcap)
 coef(mdb)
 
 eigenvals(mcca)
 eigenvals(mrda)
+eigenvals(mrda1)
 eigenvals(mcap)
 eigenvals(mdb)
 
@@ -73,6 +79,7 @@ nobs(mdb)
 
 RsquareAdj(mcca)
 RsquareAdj(mrda)
+RsquareAdj(mrda1)
 RsquareAdj(mcap)
 RsquareAdj(mdb)
 
@@ -80,47 +87,78 @@ RsquareAdj(mdb)
 
 as.mlm(mcca)
 as.mlm(mrda)
+as.mlm(mrda1)
 as.mlm(mcap)
 as.mlm(mdb)
 
 head(model.frame(mcca))
 head(model.frame(mrda))
+head(model.frame(mrda1))
 head(model.frame(mcap))
 head(model.frame(mdb))
 
-## testing and model building -- NB, most anova.cca tests and model
-## building are in are in vegan-tests.R and in Examples/vegan-Ex.R
+## testing and model building -
 
 deviance(mcca)
 deviance(mrda)
+deviance(mrda1)
 deviance(mcap)
 deviance(mdb)
 
 per <- shuffleSet(nrow(dune), 49)
 permutest(mcca, per)
 permutest(mrda, per)
+permutest(mrda1, per)
 permutest(mcap, per)
 permutest(mdb, per)
 
-drop1(mcca)
-drop1(mrda)
-drop1(mcap)
-drop1(mdb)
+drop1(mcca, test="permutation", permutations=per)
+drop1(mrda, test="permutation", permutations=per)
+drop1(mrda1, test="permutation", permutations=per)
+drop1(mcap, test="permutation", permutations=per)
+drop1(mdb, test="permutation", permutations=per)
+
+anova(mcca, permutations = per)
+anova(mrda, permutations = per)
+anova(mrda1, permutations = per)
+anova(mcap, permutations = per)
+anova(mdb, permutations = per)
+
+anova(mcca, permutations = per, by="term")
+anova(mrda, permutations = per, by="term")
+anova(mrda1, permutations = per, by="term")
+anova(mcap, permutations = per, by="term")
+anova(mdb, permutations = per, by="term")
+
+anova(mcca, permutations = per, by="margin")
+anova(mrda, permutations = per, by="margin")
+anova(mrda1, permutations = per, by="margin")
+anova(mcap, permutations = per, by="margin")
+anova(mdb, permutations = per, by="margin")
+
+anova(mcca, permutations = per, by="axis")
+anova(mrda, permutations = per, by="axis")
+anova(mrda1, permutations = per, by="axis")
+anova(mcap, permutations = per, by="axis")
+anova(mdb, permutations = per, by="axis")
 
 ## the following do not work with partial models
 
 mcca <- cca(dune ~ Management + Manure + A1, dune.env)
 mrda <- rda(dune ~ Management + Manure + A1, dune.env)
+mrda1 <- rda(dune ~ Management + Manure + A1, dune.env, scale = TRUE)
 mcap <- capscale(dune ~ Management + Manure + A1, dune.env, dist = "bray")
 mdb <- dbrda(dune ~ Management + Manure + A1, dune.env, dist = "bray")
 
 head(calibrate(mcca))
 head(calibrate(mrda))
+head(calibrate(mrda1))
 head(calibrate(mcap))
 head(calibrate(mdb))
 
 head(calibrate(mcca, newdata=dune[11:15,]))
 head(calibrate(mrda, newdata=dune[11:15,]))
+head(calibrate(mrda1, newdata=dune[11:15,]))
 
 
 
