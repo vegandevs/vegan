31330720af2fa100a74fd099563e282d47e35041
Jari Oksanen <jari.oksanen@oulu.fi>
2016-11-10 15:36:58 +0200

    intersetcor did not do proper weighting in cca
    the results were, and have always been, slightly off do to
    wrong centring in weighted methods (cca)

--- R/intersetcor.R
+++ R/intersetcor.R
@@ -1,15 +1,18 @@
 `intersetcor` <-
-    function(object) 
+    function(object)
 {
     if (!inherits(object, "cca"))
         stop("can be used only with objects inheriting from 'cca'")
     if (is.null(object$CCA))
         stop("can be used only with constrained ordination")
     wa <- object$CCA$wa
-    if (!inherits(object, "rda")) {   # is CCA
-        w <- object$rowsum
-        wa <- sweep(object$CCA$wa, 1, sqrt(w), "*")
-    }
     X <- qr.X(object$CCA$QR)
-    cor(X, wa)
+    if (inherits(object, "rda"))
+        cor(X, wa)
+    else { # cca
+        w <- object$rowsum
+        cov <- crossprod(X, sqrt(w) * wa)
+        sd <- outer(1/sqrt(colSums(X^2)), 1/sqrt(colSums(w * wa^2)))
+        cov * sd
+    }
 }
