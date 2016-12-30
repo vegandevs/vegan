dc091de2934d01e66b28b31cf13ad135f47d15f3
Jari Oksanen <jari.oksanen@oulu.fi>
2016-12-30 15:53:08 +0200

    print permutation model and termlabels from permutest.cca

--- R/print.permutest.cca.R
+++ R/print.permutest.cca.R
@@ -2,7 +2,7 @@
     function (x, ...)
 {
     EPS <- sqrt(.Machine$double.eps)
-    cat("\nPermutation test for", x$method, "\n\n")
+    cat("\nPermutation test for", x$method, "under", x$model, "model", "\n\n")
     cat(howHead(x$control), "\n")
     writeLines(strwrap(pasteCall(x$testcall)))
     Pval <- (colSums(sweep(x$F.perm, 2, x$F.0 - EPS, ">=")) + 1)/(x$nperm + 1)
@@ -15,10 +15,7 @@
         cat("sequential contrasts\n")
     anotab <- data.frame(x$df, x$chi, c(x$F.0, NA), c(Pval, NA))
     colnames(anotab) <- c("Df", "Inertia", "F", "Pr(>F)")
-    if (nrow(anotab) == 2)
-        rownames(anotab) <- c("Model", "Residual")
-    else
-        rownames(anotab)[nrow(anotab)] <- "Residual"
+    rownames(anotab) <- c(x$termlabels, "Residual")
     class(anotab) <- c("anova", "data.frame")
     print(anotab)
     invisible(x)
