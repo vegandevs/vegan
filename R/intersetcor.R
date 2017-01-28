516b02e092b10db8b8c03af1ca7701de5c8cd8bd
Jari Oksanen <jari.oksanen@oulu.fi>
2017-01-28 18:13:45 +0200

    intersetcor is with LC scores, not with WA scores
    In non-partial analysis LC and WA scores give the same results,
    but not in the partial analysis.

--- R/intersetcor.R
+++ R/intersetcor.R
@@ -5,14 +5,14 @@
         stop("can be used only with objects inheriting from 'cca'")
     if (is.null(object$CCA))
         stop("can be used only with constrained ordination")
-    wa <- object$CCA$wa
+    lc <- object$CCA$u
     X <- qr.X(object$CCA$QR)
     if (inherits(object, "rda"))
-        cor(X, wa)
+        cor(X, lc)
     else { # cca: weighted analysis, terms already weighted-centred
-        wa <- sqrt(object$rowsum) * wa
-        cov <- crossprod(X, wa)
-        isd <- outer(1/sqrt(colSums(X^2)), 1/sqrt(colSums(wa^2)))
+        lc <- sqrt(object$rowsum) * lc
+        cov <- crossprod(X, lc)
+        isd <- outer(1/sqrt(colSums(X^2)), 1/sqrt(colSums(lc^2)))
         cov * isd
     }
 }
