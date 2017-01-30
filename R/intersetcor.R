78ba580961fc2b1aa4f8c3e2092e6b93aca4cea1
Jari Oksanen <jari.oksanen@oulu.fi>
2017-01-30 10:18:17 +0200

    intersetcor needs constraints partialled by conditions

--- R/intersetcor.R
+++ R/intersetcor.R
@@ -5,17 +5,19 @@
         stop("can be used only with objects inheriting from 'cca'")
     if (is.null(object$CCA))
         stop("can be used only with constrained ordination")
-    lc <- object$CCA$u
+    wa <- object$CCA$wa
     X <- qr.X(object$CCA$QR)
     ## remove conditions (partial terms)
-    if (!is.null(object$pCCA))
+    if (!is.null(object$pCCA)) {
         X <- X[, -seq_along(object$pCCA$envcentre), drop = FALSE]
+        X <- qr.resid(object$pCCA$QR, X)
+    }
     if (inherits(object, "rda"))
-        cor(X, lc)
+        cor(X, wa)
     else { # cca: weighted analysis, terms already weighted-centred
-        lc <- sqrt(object$rowsum) * lc
-        cov <- crossprod(X, lc)
-        isd <- outer(1/sqrt(colSums(X^2)), 1/sqrt(colSums(lc^2)))
+        wa <- sqrt(object$rowsum) * wa
+        cov <- crossprod(X, wa)
+        isd <- outer(1/sqrt(colSums(X^2)), 1/sqrt(colSums(wa^2)))
         cov * isd
     }
 }
