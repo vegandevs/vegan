1663e481aae94e5c09a6534b501ab19ca06098c5
Jari Oksanen <jari.oksanen@oulu.fi>
2017-02-13 18:11:11 +0200

    argument to return coef for norm standardized variables

--- R/coef.cca.R
+++ R/coef.cca.R
@@ -1,11 +1,14 @@
 `coef.cca` <-
-    function (object, ...)
+    function (object, norm = FALSE, ...)
 {
     if(is.null(object$CCA))
         stop("unconstrained models do not have coefficients")
     Q <- object$CCA$QR
     u <- object$CCA$u
     u <- sqrt(object$rowsum) * u
-    qr.coef(Q, u)
+    b <- qr.coef(Q, u)
+    if (norm)
+        b <- sqrt(colSums(qr.X(object$CCA$QR)^2)) * b
+    b
 }
 
