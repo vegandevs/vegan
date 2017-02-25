d55553edbe163fbcab6f914618eb21f09f83e4d5
Jari Oksanen <jari.oksanen@oulu.fi>
2017-02-25 08:14:05 +0200

    tweaks in zapping near-zero values + comment on Lingoes/Cailliez

--- R/fitted.dbrda.R
+++ R/fitted.dbrda.R
@@ -13,15 +13,15 @@
         ## revert Gower double centring
         de <- diag(D)
         D <- -2 * D + outer(de, de, "+")
-        ## we may have tiny negative zeros
-        if (any(D < 0)) {
-            D[abs(D) < ZAP] <- 0
-            if (any(D < 0))
-                warning("some squared dissimilarities are negative")
-        }
+        ## we may have tiny negative zeros: zero them, but let large
+        ## negative values be and give NaN in sqrt (with a warning)
+        D[abs(D) < ZAP] <- 0
         D <- sqrt(D)
         D <- D * object$adjust
         D <- as.dist(D)
+        ## we do not remove Lingoes or Cailliez adjustment: this
+        ## typically gives too many negative distances as unadjusted D
+        ## often has zero-values
     }
     D
 }
