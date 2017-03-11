d29a4ab169a3e31a8a75fa47e18a264a77db17ed
Jari Oksanen <jari.oksanen@oulu.fi>
2017-03-11 08:21:43 +0200

    as.rad returns the ordered index of returned species as an attribute

--- R/as.rad.R
+++ R/as.rad.R
@@ -1,4 +1,4 @@
-"as.rad" <-
+`as.rad` <-
     function(x)
 {
     if (inherits(x, "rad"))
@@ -7,7 +7,11 @@
     nm <- names(x)
     comm <- x[take]
     names(comm) <- nm[take]
-    comm <- rev(sort(comm))
+    comm <- sort(comm, decreasing = TRUE, index.return = TRUE)
+    ## ordered index of included taxa
+    index <- which(take)[comm$ix]
+    comm <- comm$x
+    attr(comm, "index") <- index
     class(comm) <- "rad"
     comm
 }
