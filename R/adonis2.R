ffe774499428ae1dfe5ae9f7d151a2e209b585b4
Jari Oksanen <jari.oksanen@oulu.fi>
2017-03-22 15:57:53 +0200

    heading was lost when adding R2

--- R/adonis2.R
+++ R/adonis2.R
@@ -42,13 +42,14 @@
     sol <- adonis0(formula, data = data, method = method)
     out <- anova(sol, permutations = permutations, by = by,
                  parallel = parallel)
-    ## add traditional adonis output on R2
+    ## head and class will be lost when adding new row & column
+    head <- attr(out, "heading")
     ocl <- class(out)
+    ## add traditional adonis output on R2
     out <- rbind(out, "Total" = c(nobs(sol)-1, sol$tot.chi, NA, NA))
     out <- cbind(out[,1:2], "R2" = out[,2]/sol$tot.chi, out[,3:4])
     class(out) <- ocl
     ## Fix output header to show the adonis2() call instead of adonis0()
-    head <- attr(out, "heading")
     head[2] <- deparse(match.call(), width.cutoff = 500L)
     attr(out, "heading") <- head
     out
