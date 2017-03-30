cb5a6c224dafe286874904c148ed61a4f09052b9
Jari Oksanen <jari.oksanen@oulu.fi>
2017-03-30 10:48:39 +0300

    add qr.cca to extract QR decomposition from ordination object

--- R/influence.cca.R
+++ R/influence.cca.R
@@ -1,5 +1,14 @@
 ### influence statistics for cca objects
 
+## extract QR decomposition
+
+`qr.cca` <-
+    function(x, ...)
+{
+    if (is.null(r <- x$CCA$QR))
+        stop("unconstrained model or rank zero: no 'qr' component")
+    r
+}
 
 ## hat values need adjustment, because QR ignores Intercept
 
