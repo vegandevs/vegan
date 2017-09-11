ddc8f339c9cfac646dfcb54b91a417feae309172
Geoffrey Hannigan <ghanni@upenn.edu>
2017-09-10 21:05:28 -0400

    Fixed warning printing and input file mistake.

--- R/avgdist.R
+++ R/avgdist.R
@@ -1,5 +1,6 @@
-`avgdist` <- 
-    function(x, sample, distfun = vegdist, meanfun = mean, transf = NULL, iterations = 100, dmethod = "bray", ...)
+`avgdist` <-
+    function(x, sample, distfun = vegdist, meanfun = mean,
+             transf = NULL, iterations = 100, dmethod = "bray", ...)
 {
     if (is.na(sample))
         stop("invalid subsampling depth")
@@ -7,8 +8,10 @@
         stop("invalid iteration count")
     inputcast <- x
     distfun <- match.fun(distfun)
+    if (!is.null(transf))
+        transf <- match.fun(transf)
     # Get the list of iteration matrices
-    distlist <- lapply(c(1:iterations), function(i) {
+    distlist <- lapply(seq_len(iterations), function(i) {
         # Suppress warnings because it will otherwise return many warnings about
         # subsampling depth not being met, which we deal with below by returning
         # samples that do not meet the threshold.
@@ -16,11 +19,11 @@
         # Remove those that did not meet the depth cutoff
         inputcast <- inputcast[c(rowSums(inputcast) %in% sample),]
         if (!is.null(transf)) {
-            transmat <- match.fun(transf)
-            inputcast <- transmat(inputcast)
+            inputcast <- transf(inputcast)
         }
-        outdist <- distfun(inputcast, method = dmethod, diag = TRUE, upper = TRUE, ...)
-        return(as.matrix(outdist))
+        outdist <- distfun(inputcast, method = dmethod,
+                           diag = TRUE, upper = TRUE, ...)
+        as.matrix(outdist)
     })
     # Use the dist list to get the average values
     meanfun <- match.fun(meanfun)
@@ -28,21 +31,25 @@
     # Take from first element since should all be the same
     rnames <- row.names(distlist[[1]])
     afunc <- array(
-        unlist(as.matrix(distlist)), c(dim(as.matrix(distlist[[1]])), length(distlist)))
+        unlist(as.matrix(distlist)),
+               c(dim(as.matrix(distlist[[1]])), length(distlist)))
     output <- apply(afunc, 1:2, meanfun, ...)
     # Set the names on the matrix
     colnames(output) <- rownames(output) <- rnames
     # Print any samples that were removed, if they were removed
     if(nrow(x) != nrow(output)) {
         dropsamples <- setdiff(row.names(inputcast), row.names(output))
-        write(
-            "The following samples were removed because they failed to meet the subsampling depth threshold:",
-            stdout()
-        )
-        print(
-            dropsamples,
-            stdout()
+        warning(
+            paste(
+                "The following sample was removed because it failed to meet the subsampling depth threshold:",
+                dropsamples,
+                "\n",
+                sep = " "
+            )
         )
     }
-    as.dist(output, upper = TRUE, diag = TRUE)
+    output <- as.dist(output, diag = TRUE, upper = TRUE)
+    attr(output, "call") <- match.call()
+    attr(output, "method") <- "avgdist"
+    output
 }
