7d6427a3f3eee20e8a4ca27f9a5061225e1aa52c
Jari Oksanen <jari.oksanen@oulu.fi>
2017-10-10 14:33:57 +0300

    species sd's needed for plot(..., correlation = TRUE) + a typo

--- R/specscores.R
+++ R/specscores.R
@@ -18,11 +18,12 @@
     function(object, comm)
 {
     comm <- scale(comm, center = TRUE, scale = FALSE)
+    object$colsum <- apply(comm, 2, sd)
     if (!is.null(object$pCCA) && object$pCCA$rank > 0) {
         comm <- qr.resid(object$pCCA$QR, comm)
     }
     if (!is.null(object$CCA) && object$CCA$rank > 0) {
-        v <- crosspord(comm, object$CCA$u)
+        v <- crossprod(comm, object$CCA$u)
         v <- decostand(v, "normalize", MARGIN = 2)
         object$CCA$v <- v
         comm <- qr.resid(object$CCA$QR, comm)
