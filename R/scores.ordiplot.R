c294637eaea1421f3f1f248fee42c93b04903aca
Jari Oksanen <jari.oksanen@oulu.fi>
2017-11-17 10:44:19 +0200

    scores.ordiplot returns the kind of scores as an attribute 'score'
    also uses simpler code: access list items directly with [[]]

--- R/scores.ordiplot.R
+++ R/scores.ordiplot.R
@@ -1,11 +1,12 @@
-"scores.ordiplot" <-
-function (x, display = "sites", ...) 
+`scores.ordiplot`  <-
+    function (x, display = "sites", ...)
 {
     if (length(x) == 1)
         return(x[[1]])
     items <- names(x)
     items <- items[!is.na(items)]
     display <- match.arg(display, items)
-    cmd <- paste("x", display, sep = "$")
-    eval(parse(text = cmd))
+    x <- x[[display]]
+    attr(x, "score") <- display
+    x
 }
