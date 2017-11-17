b5d37e2398c03e91f63b9824f1c2f4dc37607cfd
Jari Oksanen <jari.oksanen@oulu.fi>
2017-11-17 10:53:49 +0200

    draw adjusted arrows automatically for biplot scores
    Arrows will be always drawn when requested also for other type
    of scores, but they are drawn to input coordinates instead of
    adjustment. This allows implementing biplot.rda display.

--- R/points.ordiplot.R
+++ R/points.ordiplot.R
@@ -4,8 +4,13 @@
     sco <- scores(x, what)
     if (!missing(select))
         sco <- .checkSelect(select, sco)
-    if (arrows) {
+    ## draw adjusted arrows automatically for biplot scores
+    if (attr(sco, "score") == "biplot") {
+        arrows = TRUE
         sco <- sco * ordiArrowMul(sco)
+    }
+    ## draw arrows when requested, also for "species" etc
+    if (arrows) {
         arrows(0, 0, sco[,1], sco[,2], ...)
     } else {
         points(sco, ...)
