258ccbdf7591dca6b4e9ad4530183f8063af52eb
Jari Oksanen <jari.oksanen@oulu.fi>
2017-11-17 11:04:52 +0200

    text.ordiplot gains argument arrows=FALSE
    Adjusted rrows are always drawn for biplot scores, and non-adjusted
    arrows for other scores on request. The arrow heads will point to the
    score coordinate, and the text will be moved outwards, similarly as
    in plot.cca for biplot arrows.

--- R/text.ordiplot.R
+++ R/text.ordiplot.R
@@ -1,11 +1,19 @@
 `text.ordiplot`  <-
-    function (x, what, labels, select, ...)
+    function (x, what, labels, select, arrows = FALSE, ...)
 {
     sco <- scores(x, what)
     if (!missing(labels))
         rownames(sco) <- labels
     if (!missing(select))
         sco <- .checkSelect(select, sco)
+    if (attr(sco, "score") == "biplot") {
+        arrows = TRUE
+        sco <- sco * ordiArrowMul(sco)
+    }
+    if (arrows) {
+        arrows(0, 0, sco[,1], sco[,2], ...)
+        sco <- ordiArrowTextXY(sco, rownames(sco), rescale = FALSE)
+    }
     text(sco, labels = rownames(sco), ...)
     invisible(x)
 }
