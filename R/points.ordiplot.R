85df6dc64d841e0e995abe39f3cff61370635b48
Jari Oksanen <jari.oksanen@oulu.fi>
2017-11-17 10:40:14 +0200

    add arg arrows=FALSE to points.ordiplot
    This draws now biplot arrows adjusted by ordiArrowMul to *any*
    scores item

--- R/points.ordiplot.R
+++ R/points.ordiplot.R
@@ -1,9 +1,14 @@
 `points.ordiplot`  <-
-    function (x, what, select, ...)
+    function (x, what, select, arrows = FALSE, ...)
 {
     sco <- scores(x, what)
     if (!missing(select))
         sco <- .checkSelect(select, sco)
-    points(sco, ...)
+    if (arrows) {
+        sco <- sco * ordiArrowMul(sco)
+        arrows(0, 0, sco[,1], sco[,2], ...)
+    } else {
+        points(sco, ...)
+    }
     invisible(x)
 }
