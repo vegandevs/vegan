2a2187d007183e272dbebe7b44ea8fcd46a3cd35
Jari Oksanen <jari.oksanen@oulu.fi>
2017-12-18 12:44:07 +0200

    improved scope for model.frame.cca
    model.frame.cca failed if `data` had a function name (e.g.,
    `data = df`), because the function first tried to find the
    function instead of the data frame

--- R/model.frame.cca.R
+++ R/model.frame.cca.R
@@ -10,8 +10,11 @@
     ## subset must be evaluated before ordiParseFormula
     if (!is.null(call$subset))
         call$subset <- formula$subset
-    call[[1]] <- as.name("ordiParseFormula")
-    out <- eval(call, environment(), parent.frame())
+    if (is.null(call$na.action))
+        call$na.action <- na.pass
+    data <- eval(call$data, environment(call$formula), .GlobalEnv)
+    out <- ordiParseFormula(call$formula, data, na.action = call$na.action,
+                            subset = call$subset)
     mf <- out$modelframe
     attr(mf, "terms") <- out$terms.expand
     if (!is.null(out$na.action))
