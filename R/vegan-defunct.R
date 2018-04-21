9aa5ec38b7b19dadc9fe19dce49ad07a102f1b43
Jari Oksanen <jari.oksanen@oulu.fi>
2018-04-21 16:11:48 +0300

    as.mlm is now .Defunct

--- R/vegan-defunct.R
+++ R/vegan-defunct.R
@@ -1,4 +1,33 @@
-### commsimulator was deprecated in 2.4-0
+## as.mlm: deprecated in 2.5-0, defunct in 2.6-0
+
+`as.mlm` <-
+    function(x)
+{
+    .Defunct("see ?hatvalues.cca for new alternatives")
+    if (is.null(x$CCA))
+        stop("'as.mlm' can be used only for constrained ordination")
+    UseMethod("as.mlm")
+}
+`as.mlm.cca` <-
+    function (x)
+{
+    w <- x$rowsum
+    WA <- x$CCA$wa
+    X <- qr.X(x$CCA$QR)
+    ## shall use weighted regression: deweight X
+    X <- (1/sqrt(w)) * X
+    X <- as.data.frame(X)
+    lm(WA ~ ., data = X, weights = w)
+}
+
+`as.mlm.rda` <-
+    function (x)
+{
+    X <- as.data.frame(qr.X(x$CCA$QR))
+    WA <- x$CCA$wa
+    lm(WA ~ . , data = X)
+}
+### commsimulator was deprecated in 2.4-0, defunct in 2.6-0
 
 "commsimulator" <-
 function (x, method, thin = 1)
