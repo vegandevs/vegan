2b164ae50fcbff255935e503d2868b00e00b8405
Jari Oksanen <jari.oksanen@oulu.fi>
2018-05-24 09:04:25 +0300

    No idea how to find bstick() for dbrda with negative eigenvalues
    Somebody asked this in the PAST mailing list (readable for members
    only), and PAST maintainer was unsure if this can be done. I agreed
    in my comments for non-metric dissimilarities. We have previously
    refused bstick() for capscale with imaginary components, but it seems
    that dbrda() got through and used total inertia (including negative
    eigenvalues) and full rank (including negative eigenvalues). As bstick
    values are bound to be positive, but some eigenvalues are negative,
    they underestimate variation of positive eigenvalues). See, e.g.,
    
    mod <- dbrda(vegdist(dune) ~ 1)
    screeplot(mod, npcs=19, bstick = TRUE)
    
    which works before this commit.
    
    (cherry picked from commit c4092953b503991436818b80238a5fd1b7f0ecc2)

--- R/bstick.cca.R
+++ R/bstick.cca.R
@@ -5,10 +5,12 @@
         stop("'n' not of class \"cca\" or \"rda\"")
     if(!is.null(n$CCA) && n$CCA$rank > 0)
         stop("'bstick' only for unconstrained models")
-    ## No idea how to define bstick for capscale with negative
-    ## eigenvalues
+    ## No idea how to define bstick for dbrda or capscale with
+    ## negative eigenvalues
+    if (inherits(n, "dbrda") && (n$CA$poseig < n$CA$rank))
+        stop("'bstick' cannot be used for 'dbrda' with imaginary components")
     if (inherits(n, "capscale") && !is.null(n$CA$imaginary.rank))
-        stop("'bstick' cannot be used for 'capscale' with imaginary component") 
+        stop("'bstick' cannot be used for 'capscale' with imaginary components")
     ## need to select appropriate total inertia
     tot.chi <- n$CA$tot.chi
     n.comp <- n$CA$rank
