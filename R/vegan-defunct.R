341691426287ab11f440b577a00ccefbaef074cf
Jari Oksanen <jari.oksanen@oulu.fi>
2018-05-29 12:47:29 +0300

    remove r0_old nullmodel that provided vegan <2.2-0 compatibility
    With vegan 2.5-1 release (and some bug fixes) we will no longer
    guarantee replicated simulation results across vegan versions (and
    we never could guarantee these across R versions and R configurations).

--- R/vegan-defunct.R
+++ R/vegan-defunct.R
@@ -36,8 +36,10 @@ function (x, method, thin = 1)
     method <- match.arg(method,
                         c("r0","r1","r2","r00","c0","swap", "tswap",
                           "backtrack", "quasiswap"))
-    if (method == "r0")
-        method <- "r0_old"
+    ## r0_old is also removed from vegan 2.6-0, but needed for <2.2-0
+    ## compatibility
+    ##if (method == "r0")
+    ##    method <- "r0_old"
     x <- as.matrix(x)
     out <- simulate(nullmodel(x, method), nsim = 1, thin = thin)
     out <- out[,,1]
