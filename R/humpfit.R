501cb486abc233f32c73b4cc691d54850f4e1567
Jari Oksanen <jari.oksanen@oulu.fi>
2018-08-23 10:45:21 +0300

    deprecate humpfit()

--- R/humpfit.R
+++ R/humpfit.R
@@ -1,6 +1,7 @@
 "humpfit" <-
-    function (mass, spno, family = poisson, start) 
+    function (mass, spno, family = poisson, start)
 {
+    .Deprecated("natto::humpfit() from https://github.com/jarioksa/natto/")
     hump <- function(p, mass, spno, ...) {
         x <- ifelse(mass < p[1], mass/p[1], p[1] * p[1]/mass/mass)
         fv <- p[3] * log(1 + p[2] * x/p[3])
@@ -25,9 +26,9 @@
     residuals <- spno - fv
     aic <- fit$minimum * 2 + 6
     rdf <- length(x) - 3
-    out <- list(nlm = fit, family = fam, y = spno, x = mass, 
-                coefficients = p, fitted.values = fv, aic = aic, rank = 3, 
-                df.residual = rdf, deviance = dev, residuals = residuals, 
+    out <- list(nlm = fit, family = fam, y = spno, x = mass,
+                coefficients = p, fitted.values = fv, aic = aic, rank = 3,
+                df.residual = rdf, deviance = dev, residuals = residuals,
                 prior.weights = rep(1, length(x)))
     class(out) <- c("humpfit", "glm")
     out
