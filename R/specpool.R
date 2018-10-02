69d0f6f420cd07634909a0637690603f6169a6e4
Jari Oksanen <jari.oksanen@oulu.fi>
2018-10-02 12:05:04 +0300

    input data must be numeric
    (cherry picked from commit 1b6b8f11c5eed81d455a4799cf1ae3fcce981f24)

--- R/specpool.R
+++ R/specpool.R
@@ -1,8 +1,10 @@
 `specpool` <-
-    function (x, pool, smallsample = TRUE) 
+    function (x, pool, smallsample = TRUE)
 {
     x <- as.matrix(x)
-    if (missing(pool)) 
+    if (!is.numeric(x))
+        stop("input data must be numeric")
+    if (missing(pool))
         pool <- rep("All", nrow(x))
     ## check dims
     if (length(pool) != NROW(x))
@@ -33,7 +35,7 @@
         freq <- colSums(X > 0)
         p <- freq[freq > 0]/n
         S[is] <- sum(freq > 0)
-        if (S[is] == 0) 
+        if (S[is] == 0)
             next
         if (n >= 1L)
             a1 <- sum(freq == 1)
@@ -60,9 +62,9 @@
             var.chao[is] <-
                 ssc * (ssc * (a1*(2*a1-1)^2/4 - a1^4/chao[is]/4) + a1*(a1-1)/2)
         if (!is.na(a1) && a1 > 0) {
-            jf <- table(rowSums(X[, freq == 1, drop = FALSE] > 
+            jf <- table(rowSums(X[, freq == 1, drop = FALSE] >
                                 0))
-            var.jack1[is] <- (sum(as.numeric(names(jf))^2 * jf) - 
+            var.jack1[is] <- (sum(as.numeric(names(jf))^2 * jf) -
                               a1/n) * (n - 1)/n
         } else {
             var.jack1[is] <- 0
@@ -72,8 +74,8 @@
         Zp <- (crossprod(X == 0)/n)^n - outer(pn, pn, "*")
         var.boot[is] <- sum(pn * (1 - pn)) + 2 * sum(Zp[lower.tri(Zp)])
     }
-    out <- list(Species = S, chao = chao,  chao.se = sqrt(var.chao), 
-                jack1 = jack.1, jack1.se = sqrt(var.jack1), jack2 = jack.2, 
+    out <- list(Species = S, chao = chao,  chao.se = sqrt(var.chao),
+                jack1 = jack.1, jack1.se = sqrt(var.jack1), jack2 = jack.2,
                 boot = bootS, boot.se = sqrt(var.boot), n = as.vector(groups))
     out <- as.data.frame(out)
     attr(out, "pool") <- pool
