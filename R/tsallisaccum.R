22ad45079d170d75fc5277319fcdda718bbda5ca
Jari Oksanen <jari.oksanen@oulu.fi>
2018-10-02 12:09:43 +0300

    input data must be numeric (+ whitespace)
    (cherry picked from commit faf55b50c1ec0b9a7f6c868213268e6da5dd8aa1)

--- R/tsallisaccum.R
+++ R/tsallisaccum.R
@@ -5,6 +5,8 @@
     if (!missing(subset))
         x <- subset(x, subset)
     x <- as.matrix(x)
+    if (!is.numeric(x))
+        stop("input data must be numeric")
     n <- nrow(x)
     p <- ncol(x)
     if (p == 1) {
@@ -16,10 +18,10 @@
     permutations <- nrow(pmat)
     m <- length(scales)
     result <- array(dim = c(n, m, permutations))
-    dimnames(result) <- list(pooled.sites = c(1:n), scale = scales, 
+    dimnames(result) <- list(pooled.sites = c(1:n), scale = scales,
         permutation = c(1:permutations))
     for (k in 1:permutations) {
-        result[, , k] <- as.matrix(tsallis((apply(x[pmat[k,], 
+        result[, , k] <- as.matrix(tsallis((apply(x[pmat[k,],
             ], 2, cumsum)), scales = scales, ...))
     }
     if (raw) {
@@ -35,14 +37,14 @@
                 tmp[i, j, 2] <- sd(result[i, j, 1:permutations])
                 tmp[i, j, 3] <- min(result[i, j, 1:permutations])
                 tmp[i, j, 4] <- max(result[i, j, 1:permutations])
-                tmp[i, j, 5] <- quantile(result[i, j, 1:permutations], 
+                tmp[i, j, 5] <- quantile(result[i, j, 1:permutations],
                   0.025)
-                tmp[i, j, 6] <- quantile(result[i, j, 1:permutations], 
+                tmp[i, j, 6] <- quantile(result[i, j, 1:permutations],
                   0.975)
             }
         }
         result <- tmp
-        dimnames(result) <- list(pooled.sites = c(1:n), scale = scales, 
+        dimnames(result) <- list(pooled.sites = c(1:n), scale = scales,
             c("mean", "stdev", "min", "max", "Qnt 0.025", "Qnt 0.975"))
     }
     attr(result, "control") <- attr(pmat, "control")
