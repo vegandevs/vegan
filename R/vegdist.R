f173233458a7004de4eb871711fac1dec67214ed
Jari Oksanen <jhoksane@gmail.com>
2020-11-12 13:19:53 +0200

    chord distance can be calculated to negative values, chi-square not
    (cherry picked from commit 493de2cc80ada0add3749522b2531140e20c04fa)

--- R/vegdist.R
+++ R/vegdist.R
@@ -28,8 +28,8 @@
     if (!method %in% c(1,2,6,16,18) && any(rowSums(x, na.rm = TRUE) == 0))
         warning("you have empty rows: their dissimilarities may be meaningless in method ",
                 dQuote(inm))
-    ## 1 manhattan, 2 euclidean, 3 canberra, 6 gower, 16 mahalanobis, 18 chisq
-    if (!method %in% c(1,2,3,6,16,18) && any(x < 0, na.rm = TRUE))
+    ## 1 manhattan, 2 euclidean, 3 canberra, 6 gower, 16 mahalanobis, 19 chord
+    if (!method %in% c(1,2,3,6,16,19) && any(x < 0, na.rm = TRUE))
         warning("results may be meaningless because data have negative entries in method ",
                 dQuote(inm))
     if (method %in% c(11,18) && any(colSums(x) == 0))
