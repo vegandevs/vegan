f5c464e2c58cc776dea6fd755fcadef8980f3174
Jari Oksanen <jhoksane@gmail.com>
2021-03-22 15:54:31 +0200

    scores can return a list of sites and species scores in metaMDS
    This seems to break nothing: plot.metaMDS already assumed to have
    such a structure and it prepared it internally.

--- R/scores.metaMDS.R
+++ R/scores.metaMDS.R
@@ -1,20 +1,26 @@
 `scores.metaMDS` <-
     function(x, display = c("sites", "species"), shrink = FALSE, choices, ...)
 {
-    display <- match.arg(display)
+    display <- match.arg(display, c("sites","species"), several.ok = TRUE)
     if (missing(choices))
-        choices <- 1:x$ndim
+        choices <- seq_len(x$ndim)
     else
         choices <- choices[choices <= x$ndim]
-    if (display == "sites")
-        X <- x$points
-    else if (display == "species") {
-        X <- x$species
+    sites <- NULL
+    species <- NULL
+    if ("sites" %in% display) {
+        sites <- x$points[, choices, drop=FALSE]
+        colnames(sites) <- paste0("NMDS", choices)
+    }
+    if ("species" %in% display && !is.null(x$species)) {
+        species <- x$species[, choices, drop=FALSE]
+        colnames(species) <- paste0("NMDS", choices)
         if (shrink) {
             mul <- sqrt(attr(X, "shrinkage"))
             if (is.null(mul))
                 warning("species cannot be shrunken, because they were not expanded")
             else {
+                mul <- mul[choices]
                 cnt <- attr(X, "centre")
                 X <- sweep(X, 2, cnt, "-")
                 X <- sweep(X, 2, mul, "*")
@@ -22,7 +28,10 @@
             }
         }
     }
-    colnames(X) <- paste("NMDS", 1:ncol(X), sep="")
-    X[, choices, drop = FALSE]
+    out <- list()
+    out$sites <- sites
+    out$species <- species
+    if (length(out) == 1)
+        out <- out[[1]]
+    out
 }
-
