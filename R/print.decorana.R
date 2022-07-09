c59a91f60e22f513fb5558d553f4dc50308b47b0
Jari Oksanen <jhoksane@gmail.com>
2022-07-09 11:21:02 +0300

    print additive (orthogonalized) eigenvalues in decorana

--- R/print.decorana.R
+++ R/print.decorana.R
@@ -1,18 +1,18 @@
-"print.decorana" <-
-function (x, digits = max(3, getOption("digits") - 3), ...) 
+`print.decorana` <-
+    function (x, digits = max(3, getOption("digits") - 3), ...)
 {
     cat("\nCall:\n")
     cat(deparse(x$call), "\n\n")
     cat(ifelse(x$ira, "Orthogonal", "Detrended"), "correspondence analysis")
-    cat(ifelse(!x$ira, paste(" with ", x$mk, " segments.\n", 
+    cat(ifelse(!x$ira, paste(" with ", x$mk, " segments.\n",
         sep = ""), ".\n"))
     if (x$iresc) {
         cat("Rescaling of axes with", x$iresc, "iterations")
-        if (x$short) 
+        if (x$short)
             cat(", and shortest axis rescaled", x$short)
         cat(".\n")
     }
-    if (!is.null(x$v)) 
+    if (!is.null(x$v))
         cat("Downweighting of rare species from fraction 1/", x$fraction, ".\n", sep="")
     if (!is.null(x$before)) {
         cat("Piecewise transformation of above-zero abundances:\n")
@@ -23,8 +23,10 @@ function (x, digits = max(3, getOption("digits") - 3), ...)
         axlen <- apply(x$rproj, 2, max)
     }
     cat("\n")
-    print(rbind(Eigenvalues = x$evals, "Decorana values" = x$evals.decorana, 
-        "Axis lengths" = axlen), digits = digits)
+    print(rbind("Eigenvalues" = x$evals,
+                "Additive Eigenvals" = x$evals.ortho,
+                "Decorana values" = x$evals.decorana,
+                "Axis lengths" = axlen), digits = digits)
     cat("\n")
     invisible(x)
 }
