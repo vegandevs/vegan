34f4ec8e9a05e2ed45adb5d6da73312f3435a39e
Jari Oksanen <jhoksane@gmail.com>
2022-08-02 16:44:11 +0300

    shrinking species scores failed and missing scores gave trouble

--- R/scores.metaMDS.R
+++ R/scores.metaMDS.R
@@ -17,15 +17,17 @@
         species <- x$species[, choices, drop=FALSE]
         colnames(species) <- paste0("NMDS", choices)
         if (shrink) {
-            mul <- sqrt(attr(X, "shrinkage"))
+            ## [,choices] drops attributes
+            mul <- sqrt(attr(x$species, "shrinkage"))
+            cnt <- attr(x$species, "centre")
             if (is.null(mul))
-                warning("species cannot be shrunken, because they were not expanded")
+                message("species are not shrunken, because they were not expanded")
             else {
                 mul <- mul[choices]
-                cnt <- attr(X, "centre")
-                X <- sweep(X, 2, cnt, "-")
-                X <- sweep(X, 2, mul, "*")
-                X <- sweep(X, 2, cnt, "+")
+                cnt <- cnt[choices]
+                species <- sweep(species, 2, cnt, "-")
+                species <- sweep(species, 2, mul, "*")
+                species <- sweep(species, 2, cnt, "+")
             }
         }
         out$species <- species
