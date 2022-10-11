89c48abc94fd3080f27cfecd9bd5213535e2facf
Jari Oksanen <jhoksane@gmail.com>
2022-10-11 18:23:47 +0300

    adonis is finally deprecated & six packages still fail (issue #523)
    The failing packages still using adonis after release of adonis2 on
    June 15, 2016 (vegan 2.4-0) are betaper, GUniFrac, microbial,
    RVAideMemoire, SigTree and smartsnp.

--- R/adonis-deprecated.R
+++ R/adonis-deprecated.R
@@ -5,9 +5,7 @@
              contr.unordered="contr.sum", contr.ordered="contr.poly",
              parallel = getOption("mc.cores"), ...)
 {
-    ## nobodody seems to believe that adonis is deprecated: give up
-    ## .Deprecated("adonis2", package="vegan")
-    message("'adonis' will be deprecated: use 'adonis2' instead")
+    .Deprecated("adonis2", package="vegan")
     EPS <- sqrt(.Machine$double.eps) ## use with >= in permutation P-values
     ## formula is model formula such as Y ~ A + B*C where Y is a data
     ## frame or a matrix, and A, B, and C may be factors or continuous
