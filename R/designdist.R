9f778c046c343fc158ab3e0df4ddcfa818a5c0e6
Jari Oksanen <jhoksane@gmail.com>
2023-05-12 16:11:18 +0300

    designdist2: dissimilarities between rows of two matrices
    Returns a rectangular matrix of dissimilarities between two
    matrices with similar interface as designdist.
    
    terms = "quadratic" is not implemented (yet).

--- R/designdist.R
+++ R/designdist.R
@@ -53,6 +53,72 @@
     dis
 }
 
+### Variant of designdist that calculates dissimilarities between two
+### data sets. Returns a rectangular matrix where rows are the rows of
+### matrix 'x', and columns are the rows of matrix 'y'.
+
+`designdist2` <-
+    function (x, y, method = "(A+B-2*J)/(A+B)",
+              terms = c("binary", "quadratic", "minimum"),
+              abcd = FALSE, alphagamma = FALSE, name, maxdist)
+{
+    terms <- match.arg(terms)
+    if (terms == "minimum")
+        .NotYetUsed("terms = 'minimum'")
+    if ((abcd || alphagamma) && terms != "binary")
+        warning("perhaps terms should be 'binary' with 'abcd' or 'alphagamma'?")
+    x <- as.matrix(x)
+    y <- as.matrix(y)
+    ## only do numeric data for which "pa", minimum and quadratic make sense
+    if (!(is.numeric(x) || is.logical(x)))
+        stop("input data must be numeric")
+    Nx <- nrow(x)
+    Ny <- nrow(y)
+    P <- ncol(x)
+    if (ncol(x) != P)
+        stop("number of columns do not match in 'x' and 'y'")
+    if (terms == "binary") {
+        x <- ifelse(x > 0, 1, 0)
+        y <- ifelse(y > 0, 1, 0)
+    }
+    if (terms == "binary" || terms == "quadratic")
+        J <- tcrossprod(y, x)
+    ## terms = "minimum" not yet implemented
+    if (terms == "minimum")
+        x <- .Call(do_minterms, as.matrix(x))
+    A <- diag(tcrossprod(x))[col(J)]
+    B <- diag(tcrossprod(y))[row(J)]
+    Jattr <- attributes(J)
+    J <- as.vector(J)
+    ## 2x2 contingency table notation
+    if (abcd) {
+        a <- J
+        b <- A - J
+        c <- B - J
+        d <- P - A - B + J
+    }
+    ## beta diversity notation
+    if (alphagamma) {
+        alpha <- (A + B)/2
+        gamma <- A + B - J
+        delta <- abs(A - B)/2
+    }
+    dis <- eval(parse(text = method))
+    attributes(dis) <- Jattr
+    attr(dis, "call") <- match.call()
+    if (missing(name))
+        attr(dis, "method") <- paste(terms, method)
+    else attr(dis, "method") <- name
+    if (!missing(maxdist)) {
+        if (!is.na(maxdist) && any(dis > maxdist)) {
+            warning("'maxdist' was lower than some distances: setting to NA")
+            maxdist <- NA
+        }
+        attr(dis, "maxdist") <- maxdist
+    }
+    dis
+}
+
 ## similar to designdist, but uses Chao's terms U & V instead of J, A,
 ## B (or their derived terms) in designdist. I considered having this
 ## as an option 'terms = "chao"' in designdist, but there really is so
