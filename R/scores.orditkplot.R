e64779cd1ca50f7f0de712efa5707592c2583c65
Jari Oksanen <jhoksane@gmail.com>
2024-03-21 12:26:16 +0200

    deprecate orditkplot support functions
    These may be used with previously saved orditkplot result objects

--- R/scores.orditkplot.R
+++ R/scores.orditkplot.R
@@ -1,6 +1,7 @@
 `scores.orditkplot` <-
     function(x, display, ...)
 {
+    .Deprecated(msg = "function was moved to CRAN package vegan3d")
     if (!missing(display) && !is.na(pmatch(display, "labels")))
         x$labels
     else
