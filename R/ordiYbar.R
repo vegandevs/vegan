92c8fd575c642e46e65eb7a8c823d852510c8c07
Jari Oksanen <jhoksane@gmail.com>
2024-04-19 15:06:34 +0300

    remove support for vegan cca objects < 2.5 (2016 or older)

--- R/ordiYbar.R
+++ R/ordiYbar.R
@@ -15,8 +15,9 @@
     isDB <- inherits(x, "dbrda")
     if (!is.null(x$Ybar))
         Ybar <- x$Ybar
-    else
-        return(vegan24Xbar(x, model))
+    else ## vegan < 2.5 from 2016 did not have Ybar
+        stop(gettextf("outdated object from 2016 or before: use update(%s)",
+                      noquote(deparse(substitute(x)))))
     if (model == "initial")
         return(Ybar)
     ## return NULL for missing elements
@@ -63,61 +64,3 @@
     Ybar
 }
 
-#' Extract internal working matrices of cca objects for vegan 2.4
-
-`vegan24Xbar` <-
-    function(x, model = c("CCA", "CA", "pCCA", "partial", "initial"))
-{
-    model <- match.arg(model)
-    ## return NULL for missing elements
-    if (model %in% c("CCA", "CA", "pCCA"))
-        if(is.null(x[[model]]))
-            return(NULL)
-    Ybar <- NULL
-    ## initial working data is not saved and must be reconstructed in
-    ## partial models, but this cannot be done in all cases.
-    if (model == "initial") {
-        if (inherits(x, "dbrda")) {
-            ## NULL in partial models
-            if (is.null(x$pCCA)) {
-                Ybar <- x$CCA$Xbar
-                if (is.null(Ybar))
-                    Ybar <- x$CA$Xbar
-            }
-        } else {
-            ## this will ignore imaginary components in capscale
-            if (is.null(x$CCA))
-                Ybar <- x$CA$Xbar
-            else
-                Ybar <- x$CCA$Xbar
-            if (!is.null(x$pCCA))
-                Ybar <- Ybar + x$pCCA$Fit
-        }
-        ## vegan 2.5 compatibility: returns Ybar divided with d.o.f.
-        if (x$inertia == "variance")
-            Ybar <- Ybar/sqrt(nobs(x)-1)
-        return(Ybar)
-    }
-    ## several components are already stored in the result object and
-    ## we just fetch those (only CCA needs work)
-    switch(model,
-    "pCCA" =
-        Ybar <- x$pCCA$Fit,
-    "partial" = {
-        Ybar <- x$CCA$Xbar
-        if (is.null(Ybar))
-            Ybar <- x$CA$Xbar
-    },
-    "CCA" = {
-        Ybar <- qr.fitted(x$CCA$QR, x$CCA$Xbar)
-        if (inherits(x, "dbrda"))
-            Ybar <- qr.fitted(x$CCA$QR, t(Ybar))
-    },
-    "CA" =
-        Ybar <- x$CA$Xbar
-    )
-    ## vegan 2.5 divides Ybar with sqrt(n-1) in rda for variance
-    if (x$inertia == "variance")
-        Ybar <- Ybar/sqrt(nobs(x)-1)
-    Ybar
-}
