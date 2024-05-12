9bf8ffcb98c1e5eae2c54c79db6a04bd40bb0c53
Jari Oksanen <jhoksane@gmail.com>
2024-05-12 12:29:16 +0300

    summary.isomamp no longer returns scores and prints net more neatly
    (cherry picked from commit cf94f090ad41a403c175dce0867e4c95093678f7)

--- R/summary.isomap.R
+++ R/summary.isomap.R
@@ -1,10 +1,9 @@
 `summary.isomap` <-
-function (object, axes=4, ...) 
+function (object, ...)
 {
 	axes <- min(axes, ncol(object$points))
 	out <- list()
 	out$call <- object$call
-	out$points <- object$points[,1:axes]
 	out$net <- object$net
 	n <- nrow(object$points)
 	out$ndis <- n * (n-1) / 2
