84705cc937a1fae2f3411eeaa8c3da011e896915
Jari Oksanen <jhoksane@gmail.com>
2024-06-06 13:09:49 +0300

    text/points.ordiplot gained arg  for arrow length multiplier
    This makes ordiplot support functions compatible with
    text/points.cca.

--- R/points.ordiplot.R
+++ R/points.ordiplot.R
@@ -1,14 +1,20 @@
 `points.ordiplot`  <-
-    function (x, what, select, arrows = FALSE, length = 0.05, ...)
+    function (x, what, select, arrows = FALSE, length = 0.05,
+              arr.mul, ...)
 {
     sco <- scores(x, what)
     if (!missing(select))
         sco <- .checkSelect(select, sco)
-    ## draw adjusted arrows automatically for biplot scores
-    scoatt <- attr(sco, "score")
-    if (!is.null(scoatt) && scoatt %in% c("biplot", "regression")) {
-        arrows = TRUE
-        sco <- sco * ordiArrowMul(sco)
+    if (!missing(arr.mul)) {
+        arrows <- TRUE
+        sco <- sco * arr.mul
+    } else {
+        ## draw adjusted arrows automatically for biplot scores
+        scoatt <- attr(sco, "score")
+        if (!is.null(scoatt) && scoatt %in% c("biplot", "regression")) {
+            arrows = TRUE
+            sco <- sco * ordiArrowMul(sco)
+        }
     }
     ## draw arrows when requested, also for "species" etc
     if (arrows) {
