vegandevs/vegan

commit fe927136185e15f68eb8a8e4c50c876dc3b58cf8

scores.ordiplot sets attr also when input has single matrix

R/scores.ordiplot.R
@@ -1,8 +1,10 @@
 `scores.ordiplot`  <-
     function (x, display = "sites", ...)
 {
-    if (length(x) == 1)
+    if (length(x) == 1) {
+        attr(x[[1]], "score") <- display
         return(x[[1]])
+    }
     items <- names(x)
     items <- items[!is.na(items)]
     display <- match.arg(display, items)

commit 3e42ce4d4649c027b3baaa1fc78a67809e7175c6

scores.ordiplot can fail if input was a list of one type of scores
If ordiplot object is a list of one type of scores (list of one
matrix), arg display can be missing, but then scores and hence
points and text failed. Regression was introduced in fe927136 (June 8, 2024)
and broke ulrb package.

R/scores.ordiplot.R
@@ -2,7 +2,7 @@
     function (x, display = "sites", ...)
 {
     if (length(x) == 1) {
-        attr(x[[1]], "score") <- display
+        attr(x[[1]], "score") <- names(x)
         return(x[[1]])
     }
     items <- names(x)