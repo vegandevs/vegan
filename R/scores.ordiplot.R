25bbf9f154cea717bf795c2c759e24f5c815772c
Jari Oksanen <jhoksane@gmail.com>
2024-06-13 11:58:42 +0300

    scores.ordiplot can fail if input was a list of one type of scores
    If ordiplot object is a list of one type of scores (list of one
    matrix), arg display can be missing, but then scores and hence
    points and text failed. Regression was introduced in fe927136 (June 8, 2024)
    and broke ulrb package.
    
    (cherry picked from commit 3e42ce4d4649c027b3baaa1fc78a67809e7175c6)

--- R/scores.ordiplot.R
+++ R/scores.ordiplot.R
@@ -2,7 +2,7 @@
     function (x, display = "sites", ...)
 {
     if (length(x) == 1) {
-        attr(x[[1]], "score") <- display
+        attr(x[[1]], "score") <- names(x)
         return(x[[1]])
     }
     items <- names(x)
