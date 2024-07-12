9871aae893c7e8222415f183a583af85be4dfdea
Jari Oksanen <jhoksane@gmail.com>
2024-07-12 18:09:51 +0300

    make.cepnames: allow user set minlengths instead hard-coded 4+4

--- R/make.cepnames.R
+++ R/make.cepnames.R
@@ -1,5 +1,6 @@
 `make.cepnames` <-
-    function (names, seconditem = FALSE, uniqgenera = FALSE)
+    function (names, minlengths = c(4,4), seconditem = FALSE,
+              uniqgenera = FALSE)
 {
     ## do not split by hyphens, but collapse hyphened names
     names <- gsub("-", "", names)
@@ -14,14 +15,17 @@
     epi <- sapply(names,
                   function(x) {if (seconditem) x[2]
                                else if (length(x) > 1) x[length(x)] else ""})
-    ## strict=TRUE always takes 4 even if this generates duplicates
+    ## strict=TRUE always takes given minlength even if these are duplicates
+    glen <- minlengths[1]
+    nmlen <- sum(minlengths)
     gen <- ifelse(epi != "",
-                  abbreviate(gen, 4, use.classes = FALSE, strict = !uniqgenera),
+                  abbreviate(gen, glen, use.classes = FALSE,
+                             strict = !uniqgenera),
                   gen)
     if (uniqgenera)
-        gen <- abbreviate(gen, 4, use.classes = TRUE)
-    names <- abbreviate(paste0(gen, epi), 8, use.classes = FALSE)
-    ## try to remove wovels if names > 8
-    names <- abbreviate(names, 8, use.classes = TRUE, named = FALSE)
+        gen <- abbreviate(gen, glen, use.classes = TRUE)
+    names <- abbreviate(paste0(gen, epi), nmlen, use.classes = FALSE)
+    ## try to remove wovels if names > glel
+    names <- abbreviate(names, nmlen, use.classes = TRUE, named = FALSE)
     names
 }
