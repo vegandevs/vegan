f79162a928701651ad1a9cf2dddffa9de3939d3e
Jari Oksanen <jhoksane@gmail.com>
2024-07-12 18:00:27 +0300

    make.cepnames gained arg to avoid duplicates of genus part
    With duplicated generic abbreviations the genus part can take > 4
    letters leaving fewer letters for the epithet.

--- R/make.cepnames.R
+++ R/make.cepnames.R
@@ -1,5 +1,5 @@
 `make.cepnames` <-
-    function (names, seconditem = FALSE)
+    function (names, seconditem = FALSE, uniqgenera = FALSE)
 {
     ## do not split by hyphens, but collapse hyphened names
     names <- gsub("-", "", names)
@@ -16,8 +16,10 @@
                                else if (length(x) > 1) x[length(x)] else ""})
     ## strict=TRUE always takes 4 even if this generates duplicates
     gen <- ifelse(epi != "",
-                  abbreviate(gen, 4, use.classes = FALSE, strict = TRUE),
+                  abbreviate(gen, 4, use.classes = FALSE, strict = !uniqgenera),
                   gen)
+    if (uniqgenera)
+        gen <- abbreviate(gen, 4, use.classes = TRUE)
     names <- abbreviate(paste0(gen, epi), 8, use.classes = FALSE)
     ## try to remove wovels if names > 8
     names <- abbreviate(names, 8, use.classes = TRUE, named = FALSE)
