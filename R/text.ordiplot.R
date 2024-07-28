fcf32997ab988426e0d541c1f9dca84c47dc26f4
Jari Oksanen <jhoksane@gmail.com>
2024-07-28 12:08:54 +0300

    text.ordiplot and hence plot.cca handle 'bg' with 'optimize'

--- R/text.ordiplot.R
+++ R/text.ordiplot.R
@@ -21,9 +21,12 @@
         arrows(0, 0, sco[,1], sco[,2], length = length, ...)
         sco <- ordiArrowTextXY(sco, rownames(sco), rescale = FALSE, ...)
     }
-    if (optimize)
-        ordipointlabel(sco, display="sites", bg = bg, add = TRUE, ...)
-    else if (missing(bg))
+    if (optimize) {
+        if (missing(bg))
+            ordipointlabel(sco, display = what, add = TRUE, ...)
+        else
+            ordipointlabel(sco, display = what, bg = bg, add = TRUE, ...)
+    } else if (missing(bg))
         text(sco, labels = rownames(sco), ...)
     else
         ordilabel(sco, labels = rownames(sco), fill = bg, ...)
