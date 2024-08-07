ad3d5a7cb602b988e999b9f5ed4f7e6a7a437f2e
Jari Oksanen <jhoksane@gmail.com>
2024-08-07 12:20:54 +0300

    more cases of careless use of diplay as positional arg in scores
    (cherry picked from commit 0ee31c0f2d117c55a2b419f828df603c085b4c89)

--- R/identify.ordiplot.R
+++ R/identify.ordiplot.R
@@ -1,7 +1,7 @@
 `identify.ordiplot` <-
     function (x, what, labels, ...)
 {
-    x <- scores(x, what)
+    x <- scores(x, display = what)
     if (missing(labels))
         labels <- rownames(x)
     identify(x, labels = labels, ...)
