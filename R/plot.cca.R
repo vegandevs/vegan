d6fe8c2283f0c8cf63bbd658366c1aa19590fef4
Gavin Simpson <ucfagls@gmail.com>
2024-08-28 15:48:49 +0200

    call NextMethod with no arguments; seems to fix #685 and allo1 optimize to work from very limited testing
    !

--- R/plot.cca.R
+++ R/plot.cca.R
@@ -152,5 +152,5 @@
     if (!("CA" %in% names(x)))
         stop(gettextf("%s is not a vegan rda object",
                       sQuote(deparse(substitute(x)))))
-    plot.cca(x, ...)
+    NextMethod() # no need to pass ..., happens automagically
 }
