cbcb04929ce8ca3d2ba458cb3e3c26967c2a52f3
Jari Oksanen <jhoksane@gmail.com>
2024-09-03 16:33:43 +0300

    edit from betadistances -> centredist.betadisper

--- R/centredist.R
+++ R/centredist.R
@@ -1,3 +1,7 @@
+### Function to find distances of points to centroids
+
+`centredist` <- function(x) UseMethod("centredist")
+
 ### Function to find distances from each sampling unit to each centroid
 ### for vegan::betadisper result.
 ### x (input): result object from vegan::betadisper
@@ -6,18 +10,29 @@
 ### https://stackoverflow.com/questions/77391007/ and in github issue
 ### #606
 
-`betadistances` <-
+`centredist.betadisper` <-
     function(x)
- {
-     cnt <- x$centroids
-     coord <- x$vectors
-     pos <- which(x$eig >= 0)
-     neg <- which(x$eig < 0)
-     d <- apply(cnt[,pos], 1,
-                function(z) rowSums(sweep(coord[,pos], 2, z)^2))
-     if (length(neg))
-         d <- d - apply(cnt[, neg], 1,
-                        function(z) rowSums(sweep(coord[,neg], 2, z)^2))
-     d <- as.data.frame(sqrt(d))
-     cbind("group" = x$group, d)
- }
+{
+    cnt <- x$centroids
+    coord <- x$vectors
+    pos <- which(x$eig >= 0)
+    neg <- which(x$eig < 0)
+    d <- apply(cnt[,pos], 1,
+               function(z) rowSums(sweep(coord[,pos], 2, z)^2))
+    if (length(neg))
+        d <- d - apply(cnt[, neg], 1,
+                       function(z) rowSums(sweep(coord[,neg], 2, z)^2))
+    d <- as.data.frame(sqrt(d))
+    nearest <- levels(x$group)[apply(d, 1, which.min)]
+    out <- list("centre" = x$group, "nearest" = nearest, "distances" = d)
+    class(out) <- "centredist"
+    out
+}
+
+`print.centredist`<-
+    function(x, ...)
+{
+     print(data.frame("centre" = x$centre, "nearest" =  x$nearest, x$distances))
+     invisible(x)
+}
+
