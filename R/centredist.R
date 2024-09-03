4bf71107550c62e4b648db22677498bb248aaa6a
Jari Oksanen <jhoksane@gmail.com>
2024-09-03 17:13:10 +0300

    implement centredist.default: still work in progress

--- R/centredist.R
+++ R/centredist.R
@@ -1,6 +1,6 @@
 ### Function to find distances of points to centroids
 
-`centredist` <- function(x) UseMethod("centredist")
+`centredist` <- function(x, ...) UseMethod("centredist")
 
 ### Function to find distances from each sampling unit to each centroid
 ### for vegan::betadisper result.
@@ -11,7 +11,7 @@
 ### #606
 
 `centredist.betadisper` <-
-    function(x)
+    function(x, ...)
 {
     cnt <- x$centroids
     coord <- x$vectors
@@ -25,6 +25,33 @@
     d <- as.data.frame(sqrt(d))
     nearest <- levels(x$group)[apply(d, 1, which.min)]
     out <- list("centre" = x$group, "nearest" = nearest, "distances" = d)
+    attr(out, "distance") <- attr(x, "method")
+    class(out) <- "centredist"
+    out
+}
+
+`centredist.default` <-
+    function(x, centres, distance = c("euclidean", "mahalanobis"), ...)
+{
+    ## assume so far that x is a matrix that can be handled as such
+    centres <- factor(centres)
+    distance <- match.arg(distance)
+    cv <- list()
+    for(cl in levels(centres)) {
+        cv[[cl]] <- cov.wt(x[centres == cl,, drop=FALSE])
+    }
+    if (distance == "mahalanobis") {
+        dis <- sapply(levels(centres),
+                      function (cl) mahalanobis(x, cv[[cl]]$center,
+                                                cv[[cl]]$cov))
+    } else { # euclidean
+        cnt <- sapply(cv, function(z) z$center)
+        dis <- apply(cnt, 2, function(z) rowSums(sweep(x, 2, z)^2))
+        dis <- sqrt(dis)
+    }
+    nearest <- levels(centres)[apply(dis, 1, which.min)]
+    out <- list("centre" = centres, "nearest" = nearest, "distances" = dis)
+    attr(out, "distance") <- distance
     class(out) <- "centredist"
     out
 }
@@ -32,7 +59,8 @@
 `print.centredist`<-
     function(x, ...)
 {
-     print(data.frame("centre" = x$centre, "nearest" =  x$nearest, x$distances))
-     invisible(x)
+    cat("distances: ", attr(x, "distance"), "\n\n")
+    print(data.frame("centre" = x$centre, "nearest" =  x$nearest, x$distances))
+    invisible(x)
 }
 
