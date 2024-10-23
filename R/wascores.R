01f204c9d649ff5cc882d3e92b303d3388c42888
Jari Oksanen <jhoksane@gmail.com>
2024-10-23 18:41:21 +0300

    wascores: implement SD of weighted averages via cov.wt

--- R/wascores.R
+++ R/wascores.R
@@ -1,5 +1,5 @@
 `wascores` <-
-    function (x, w, expand = FALSE) 
+    function (x, w, expand = FALSE, stdev = FALSE)
 {
     if(any(w < 0) || sum(w) == 0)
         stop("weights must be non-negative and not all zero")
@@ -7,11 +7,12 @@
     w <- as.matrix(w)
     nc <- ncol(x)
     nr <- ncol(w)
-    wa <- matrix(NA, nrow = nr, ncol = nc)
-    colnames(wa) <- colnames(x)
-    rownames(wa) <- colnames(w)
-    for (i in 1:nr) {
-        wa[i, ] <- apply(x, 2, weighted.mean, w = w[, i])
+    wa <- t(sapply(colnames(w),
+                   function(m) apply(x, 2, weighted.mean, w = w[,m])))
+    if (stdev) {
+        sdwa <- sqrt(sapply(colnames(x), function(k)
+            sapply(colnames(w), function(m)
+                cov.wt(x[,k,drop=FALSE], w[,m])$cov)))
     }
     if (expand) {
         i <- complete.cases(wa)
@@ -25,8 +26,14 @@
         ewa <- sweep(ewa, 2, mul, "*")
         ewa <- sweep(ewa, 2, wa.cov$center, "+")
         wa[i,] <- ewa
+        if (stdev)
+            sdwa <- sweep(sdwa, 2, mul, "*")
         attr(wa, "shrinkage") <- 1/mul^2
         attr(wa, "centre") <- wa.cov$center
+    }
+    if (stdev) {
+        wa <- list("wa" = wa, "stdev" = sdwa,
+                   "N2" = diversity(w, "invsimpson", MARGIN=2))
     }
     wa
 }
