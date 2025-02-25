944017f8610c465af557c4c1c9de9409302e7daf
Jari Oksanen <jhoksane@gmail.com>
2025-02-25 13:19:06 +0200

    points/text.cca is based on ordiplot methods: answers issue of PR #729
    point/text.cca were old functions that had accumulated a lot of cruft.
    They could not be used in many cases, and the results diverged with
       plot(m, type="n"); text(m,"species" select=, labels=, ...) and
       plot(m, type="n") |> text("species", select=, labels=, ...)
    Now these functions are based on text/points.ordiplot with
    streamlined and maintainable code.

--- R/points.cca.R
+++ R/points.cca.R
@@ -1,44 +1,17 @@
 `points.cca` <-
     function (x, display = "sites", choices = c(1, 2), scaling = "species",
-              arrow.mul, head.arrow = 0.05, select, const, axis.bp = FALSE,
+              arrow.mul, head.arrow = 0.05, select, const,
               correlation = FALSE, hill = FALSE, ...)
 {
     if (length(display) > 1)
         stop("only one 'display' item can be added in one command")
     pts <- scores(x, choices = choices, display = display, scaling = scaling,
                   const, correlation = correlation, hill = hill, tidy=FALSE,
-                  droplist = TRUE)
+                  droplist = FALSE)
+    class(pts) <- "ordiplot"
     if (!missing(select))
-        pts <- .checkSelect(select, pts)
-    if (display == "cn") {
-        cnam <- rownames(pts)
-        points(pts, ...)
-        pts <- scores(x, choices = choices, display = "bp", scaling = scaling,
-                      const, correlation = correlation, hill = hill,
-                      droplist = TRUE)
-        bnam <- rownames(pts)
-        pts <- pts[!(bnam %in% cnam), , drop = FALSE]
-        if (nrow(pts) == 0)
-            return(invisible())
-        else display <- "bp"
-    }
-    if (display %in% c("bp", "reg", "re", "r")) {
-    	if (missing(arrow.mul)) {
-            arrow.mul <- ordiArrowMul(pts)
-    	}
-        pts <- pts * arrow.mul
-        arrows(0, 0, pts[, 1], pts[, 2], length = head.arrow,
-               ...)
-        pts <- pts * 1.1
-        if (axis.bp) {
-            axis(3, at = c(-arrow.mul, 0, arrow.mul),
-                 labels = rep("", 3))
-            axis(4, at = c(-arrow.mul, 0, arrow.mul),
-                 labels = c(-1, 0, 1))
-        }
-        return(invisible())
-    }
-    points(pts, ...)
+        pts[[1]] <- .checkSelect(select, pts[[1]])
+    points.ordiplot(pts, what = names(pts), ...)
     invisible()
 }
 
