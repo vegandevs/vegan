00f0c1d5f732013e0135e852ffddee903755b3c3
Jari Oksanen <jhoksane@gmail.com>
2025-07-16 18:47:49 +0300

    plot.varpart did not pass graphical args (...) to text under the box
    (cherry picked from commit 8fce2d85c82bbbd8057d44029058fa108f994934)

--- R/plot.varpart234.R
+++ R/plot.varpart234.R
@@ -8,8 +8,10 @@ function(x, cutoff = 0, digits = 1,  ...)
     labs <-  format(vals, digits=digits, nsmall=digits+1)
     labs <- gsub("NA", "", labs)
     showvarparts(x$nsets, labs, ...)
-    if (any(is.na(vals)))
-        mtext(paste("Values <", cutoff," not shown", sep=""), 1)
+    if (any(is.na(vals))) {
+        localMtext <- function(..., Xnames, cutoff) mtext(...)
+        localMtext(paste("Values <", cutoff," not shown", sep=""), side=1, ...)
+    }
     invisible()
 }
 
