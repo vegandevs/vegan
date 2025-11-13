aed3358aede689f58f91584ea8bd27d28bcea779
Jari Oksanen <jhoksane@gmail.com>
2025-11-13 12:38:49 +0200

    decostand: allow invalid input produce invalid result (NaN)
    Since CRAN release 1.6-10 we have used .Machine$double.eps to
    avoid dividing with zero and NaN result. This works smoothly, but
    can give wrong and misleading results in further analysis since
    invalid input was regarded as valid. Remove 0-avoidance code
    completely (it was made non-functional already in b0df88e7f50).

--- R/decostand.R
+++ R/decostand.R
@@ -8,30 +8,28 @@
                  "log", "clr", "rclr", "alr")
     method <- match.arg(method, METHODS)
     if (any(x < 0, na.rm = TRUE)) {
-        k <- min(x, na.rm = TRUE)
         if (method %in% c("total", "frequency", "pa", "chi.square", "rank",
                           "rrank", "rclr")) {
             warning("input data contains negative entries: result may be non-sense")
         }
     }
-    else k <- 0 # all pmax.int(k,...) should be dropped but test first
     attr <- NULL
     switch(method, total = {
         if (missing(MARGIN))
             MARGIN <- 1
-        tmp <- pmax.int(k, apply(x, MARGIN, sum, na.rm = na.rm))
+        tmp <- apply(x, MARGIN, sum, na.rm = na.rm)
         x <- sweep(x, MARGIN, tmp, "/")
         attr <- list("total" = tmp, "margin" = MARGIN)
     }, max = {
         if (missing(MARGIN))
             MARGIN <- 2
-        tmp <- pmax.int(k, apply(x, MARGIN, max, na.rm = na.rm))
+        tmp <- apply(x, MARGIN, max, na.rm = na.rm)
         x <- sweep(x, MARGIN, tmp, "/")
         attr <- list("max" = tmp, "margin" = MARGIN)
     }, frequency = {
         if (missing(MARGIN))
             MARGIN <- 2
-        tmp <- pmax.int(k, apply(x, MARGIN, sum, na.rm = na.rm))
+        tmp <- apply(x, MARGIN, sum, na.rm = na.rm)
         fre <- apply(x > 0, MARGIN, sum, na.rm = na.rm)
         tmp <- fre/tmp
         x <- sweep(x, MARGIN, tmp, "*")
@@ -56,7 +54,6 @@
         tmp <- apply(xtmp, MARGIN, min, na.rm = na.rm)
         ran <- apply(xtmp, MARGIN, max, na.rm = na.rm)
         ran <- ran - tmp
-        ran <- pmax.int(k, ran, na.rm = na.rm)
         x <- sweep(x, MARGIN, tmp, "-")
         x <- sweep(x, MARGIN, ran, "/")
         attr <- list("min" = tmp, "range" = ran, "margin" = MARGIN)
@@ -101,8 +98,8 @@
         ## MARGIN 2 transposes the result!
         if (MARGIN == 2)
             x <- t(x)
-        rs <- pmax.int(k, rowSums(x, na.rm = na.rm))
-        cs <- pmax.int(k, colSums(x, na.rm = na.rm))
+        rs <- rowSums(x, na.rm = na.rm)
+        cs <- colSums(x, na.rm = na.rm)
         tot <- sum(x, na.rm = na.rm)
         x <- sqrt(tot) * x/outer(rs, sqrt(cs))
         attr <- list("tot" = tot, "rsum" = rs, "csum" = cs, margin = MARGIN)
