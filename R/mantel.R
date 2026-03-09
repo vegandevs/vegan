028eca5dd70bae375bb3429cee76e0425c8be631
Jari Oksanen <jhoksane@gmail.com>
2026-03-09 13:40:22 +0200

    mantel[.partial] always return permutations as a vector: fixes #773
    (cherry picked from commit 699231a3e81de2cbd46815de98192f9d3837be7f)

--- R/mantel.R
+++ R/mantel.R
@@ -63,6 +63,7 @@
         } else {
             perm <- sapply(1:permutations, function(i, ...) ptest(permat[i,], ...))
         }
+        perm <- drop(perm)
         signif <- (sum(perm >= statistic - EPS) + 1)/(permutations + 1)
     }
     else {
