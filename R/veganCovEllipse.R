f325667126863c59a39a972b80638c93a2019969
O957 <127630341+O957@users.noreply.github.com>
2026-04-18 22:51:44 -0400

    address review comments

--- R/veganCovEllipse.R
+++ R/veganCovEllipse.R
@@ -1,7 +1,7 @@
 `veganCovEllipse` <-
     function(cov, center = c(0,0), scale = 1, npoints = 100)
 {
-    ## Basically taken from the 'car' package: The Circle
+    ## Basically taken from the 'car' package: The circle
     theta <- (0:npoints) * 2 * pi/npoints
     Circle <- cbind(cos(theta), sin(theta))
     ## scale, center and cov must be calculated separately
