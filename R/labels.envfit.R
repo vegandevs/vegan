ab1a284de16452307a3d89db057ee3c8858a5363
Jari Oksanen <jhoksane@gmail.com>
2026-04-28 16:22:38 +0300

    add labels<-.envfit, but fails because labels<- does not exist in R
    The purpose was to add a function to permanently replace the variable
    names in envfit object. We had (since 2012) labels.envfit to query
    those labels. However, labels<- is not a generic function in R and
    we surely won't add that in vegan. We need to have some other name
    – either a vegan-specific or names<-.envfit or row.names<-.envfit
    (rownames<- is not generic)

--- R/labels.envfit.R
+++ R/labels.envfit.R
@@ -1,3 +1,5 @@
+## Query labels
+
 `labels.envfit` <-
     function(object, ...)
 {
@@ -6,4 +8,26 @@
     if (is.null(out$vectors) || is.null(out$factors))
         out <- unlist(out, use.names = FALSE)
     out
+}
+
+## Change labels
+## THIS WON'T WORK: labels<- IS NOT A GENERIC FUNCTION IN R
+
+`labels<-.envfit` <-
+    function(object, value)
+{
+    if (is.list(value)) { # list of vectors & factors
+        if(!is.null(object$vectors) && !is.null(value$vectors))
+            rownames(object$vectors$arrows) <- value$vectors
+        if (!is.null(object$factors) && !is.null(value$factors))
+            rownames(object$factors$centroids) <- value$factors
+    } else {
+        if(!is.null(object$factors) && !is.null(object$vectors))
+            stop("needs a list with both 'vectors' and 'factors' labels")
+        if (!is.null(object$factors))
+            rownames(object$factors$centroids) <- value
+        else
+            rownames(object$vectors$arrows) <- value
+    }
+    object
 }
