b886b20c3f2e37bb91205e58b17160d5c2f46454
Jari Oksanen <jhoksane@gmail.com>
2026-05-23 07:58:37 +0300

    remove start-up message from CRAN releases: still in devel versions

--- R/zzz.R
+++
@@ -1,7 +0,0 @@
-.onAttach <- function(lib, pkg)  {
-    if(interactive())
-        packageStartupMessage(
-            "This is vegan ",
-            utils::packageDescription("vegan", fields="Version"),
-            appendLF = TRUE)
-}
