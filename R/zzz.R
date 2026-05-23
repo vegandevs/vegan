4ab2891a584db4a26040e477697105309cf07aa2
Jari Oksanen <jhoksane@gmail.com>
2026-05-23 08:02:50 +0300

    start-up message says this is devel version (and release is mute)
    CRAN release candidate 2.7-4 drops start-up message and loads
    silently, and that is the plan for future releases. Devel version
    is loud.

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onAttach <- function(lib, pkg)  {
     if(interactive())
         packageStartupMessage(
-            "This is vegan ",
+            "This is vegan devel version ",
             utils::packageDescription("vegan", fields="Version"),
             appendLF = TRUE)
 }
