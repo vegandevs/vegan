37ed6906d606e0f413583cd3b9531d62ab0d2137
Jari Oksanen <jhoksane@gmail.com>
2026-08-06 16:40:21 +0300

    Fix very old code for CRAN NOTE on deprecated features
    Check: R code for possible problems
    Result: NOTE
      Found calls to structure() using deprecated special names:
        vegan/R/clamtest.R (.Names: 1)
        vegan/R/print.summary.clamtest.R (.Names: 1)
        vegan/R/showvarparts.R (.Dim: 1)
      '.Names' should be changed to 'names'.
      '.Dim' should be changed to 'dim'.

--- R/summary.clamtest.R
+++ R/summary.clamtest.R
@@ -1,6 +1,8 @@
-summary.clamtest <- function(object, ...) {
-    structure(c(attr(object, "settings"), 
-        list(summary=cbind(Species=table(object$Classes), 
+`summary.clamtest` <-
+    function(object, ...)
+{
+    structure(c(attr(object, "settings"),
+        list(summary=cbind(Species=table(object$Classes),
             Proportion=table(object$Classes)/nrow(object)),
         minv=attr(object, "minv"),
         coverage=attr(object, "coverage"))), class="summary.clamtest")
