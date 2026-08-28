2cf7f8b192e1e017f510d18827733d81b64b57ae
Jari Oksanen <jhoksane@gmail.com>
2026-08-28 12:37:19 +0300

    modernize text.decorana to use text.ordiplot: allows optimize & bg
    Funnily plot.decorana as well as calling text in pipe after
    plot.decorana allowed optimize & bg.
    
    points.decorana is still old: points.ordiplot would add only
    arrows=FALSE that we should not need (???) with decorana.
    
    (cherry picked from commit 76c9a64818bf8dc33fa1eb0cb9623ed8329410c2)

--- R/text.decorana.R
+++ R/text.decorana.R
@@ -2,15 +2,19 @@
     function (x, display = c("sites", "species"), labels, choices = 1:2,
               origin = TRUE, select, ...)
 {
+    ## do we need localText???
     localText <- function(..., shrink, origin, scaling, triangular)
-        text(...)
+        text.ordiplot(...)
     display <- match.arg(display)
     x <- scores(x, display = display, choices = choices, origin = origin,
                 ...)
+    ## text.ordiplot expects a list of one matrix
+    x <- list(x)
+    names(x) <- display
     if (!missing(select))
         x <- .checkSelect(select, x)
     if (!missing(labels))
         rownames(x) <- labels
-    localText(x, rownames(x), ...)
+    localText(x, what = display, ...)
     invisible()
 }
