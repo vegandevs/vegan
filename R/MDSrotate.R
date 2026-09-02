vegandevs/vegan

commit 3ab61ad0b0dc9494afb10deac60262b4e8a18f44

use x %*% svd(x)$v instead of prcomp(x)$x: saves some microseconds

R/MDSrotate.R
@@ -61,10 +61,10 @@
     }
     ## Two or more free axes are (optionally) rotated to PCs
     if (N - NV > 1 && attr(object$points, "pc")) {
-        pc <- prcomp(x[,-seq_len(NV)])
-        x[,-seq_len(NV)] <- pc$x
+        rot <- svd(x[,-seq_len(NV)], nu = 0)$v
+        x[,-seq_len(NV)] <- x[, seq_len(NV)] %*% rot
         if (!all(is.na(sp)))
-            sp[,-seq_len(NV)] <- sp[,-seq_len(NV)] %*% pc$rotation
+            sp[,-seq_len(NV)] <- sp[,-seq_len(NV)] %*% rot
     }
     ## '[] <-' retains attributes
     object$points[] <- x

NAMESPACE
@@ -86,7 +86,7 @@ importFrom(stats, .getXlevels, add.scope, aggregate, aov, approx,
            influence, is.ts, kmeans, lm, median, na.action, na.fail,
            na.pass, napredict, naprint, nlm, nls, optim, optimize, p.adjust,
            p.adjust.methods, pchisq, pf, plogis, pnorm, poisson,
-           ppoints, prcomp, printCoefmat, pt, qchisq, qf, qlogis,
+           ppoints, printCoefmat, pt, qchisq, qf, qlogis,
            qnorm, qt, quantile, quasipoisson, r2dtable, reformulate,
            rmultinom, rnorm, runif, sd, spline, summary.lm, terms, ts,
            uniroot, update.formula, var, weighted.mean)

R/monoMDS.R
@@ -125,7 +125,7 @@
     sol$model <- model
     sol$points <- matrix(sol$points, nobj, k)
     if (pc)
-        sol$points <- prcomp(sol$points)$x
+        sol$points <- sol$points %*% svd(sol$points, nu = 0)$v
     attr(sol$points, "pc") <- pc
     rownames(sol$points) <- nm
     colnames(sol$points) <- paste("MDS", 1:k, sep="")

R/postMDS.R
@@ -10,7 +10,7 @@
         x <- scale(x, scale = FALSE)
     if (pc) {
         dn <- dimnames(x)
-        x <- prcomp(x, center = center)$x
+        x <- x %*% svd(x, nu = 0)$v
         dimnames(x) <- dn
     }
     ## Check halfchange scaling, if not explicitly defined

commit be22a15d8e52a4edc54b27e7e9a4a22300e9a79a

MDSrotate failed if there were 2 or more free axes rotated to PCs
Bug introduced in commit 3ab61ad0 (not ported to CRAN releases)

R/MDSrotate.R
@@ -62,7 +62,7 @@
     ## Two or more free axes are (optionally) rotated to PCs
     if (N - NV > 1 && attr(object$points, "pc")) {
         rot <- svd(x[,-seq_len(NV)], nu = 0)$v
-        x[,-seq_len(NV)] <- x[, seq_len(NV)] %*% rot
+        x[,-seq_len(NV)] <- x[, -seq_len(NV)] %*% rot
         if (!all(is.na(sp)))
             sp[,-seq_len(NV)] <- sp[,-seq_len(NV)] %*% rot
     }